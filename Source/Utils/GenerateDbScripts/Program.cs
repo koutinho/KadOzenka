ac319a7a1b8c5f8354b42b55a9cda26d3fb5d4c5
Терешечкин Михаил Юрьевич <tereshechkin@genix.pro>
2019-09-02 14:18:04 +0000

    git-tfs-id: [http://192.168.3.86:8080/tfs/defaultcollection]$/CIPJS/KadOzenka;C49269

---
+++ Source/Utils/GenerateDbScripts/Program.cs
@@ -0,0 +1,40 @@
+﻿using Core.Shared.Extensions;
+using Microsoft.Practices.EnterpriseLibrary.Data;
+using Platform.Configurator;
+using Platform.Configurator.ExportProfile;
+using System;
+using System.Configuration;
+using System.IO;
+
+namespace GenerateDbScripts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			ExportDb();
+		}
+
+		private static void ExportDb()
+		{
+			string exportProfileFilename = ConfigurationManager.AppSettings["DbExporterProfile"];
+			string connectionString = ConfigurationManager.AppSettings["DbExporterConnectionString"];
+			string providerName = ConfigurationManager.AppSettings["DbExporterProviderName"];
+
+			string baseFolder = ConfigurationManager.AppSettings["DbExporterBaseFolder"];
+
+
+			//var command = DBMngr.Realty.GetStoredProcCommand("Core_Register_PKG.CorrectSystemLayoutsAndFilters");
+			//DBMngr.Realty.ExecuteNonQuery(command);
+
+
+			ExportProfile exportProfile = File.ReadAllText(exportProfileFilename).DeserializeFromXml<ExportProfile>();
+
+			DbExporter dbExporter = new DbExporter(connectionString, providerName);
+			dbExporter.GenerateExportScripts(baseFolder, exportProfile);
+
+			Console.WriteLine("Done");
+			Console.ReadLine();
+		}
+    }
+}
