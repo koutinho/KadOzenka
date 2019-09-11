cd24c7daca14b7afde1c28c8a2626e1cd83db5b5
Силанов Ярослав Олегович <INECAS\silanov>
2019-09-11 08:21:50 +0000

    Фикс рабочей версии
    git-tfs-id: [http://192.168.3.86:8080/tfs/defaultcollection]$/CIPJS/KadOzenka;C49523

--- Source/KadOzenka.Dal/Launcher/OuterMarketParser.cs
+++ Source/KadOzenka.Dal/Launcher/OuterMarketParser.cs
@@ -9,6 +9,7 @@ using Core.Register.LongProcessManagment;
 using OuterMarketParser.LinksGenerator;
 using OuterMarketParser.Model.DatabaseOperations;
 using OuterMarketParser.Parser.Cian;
+using OuterMarketParser.DatabaseReader;
 
 namespace OuterMarketParser.Launcher
 {
@@ -17,9 +18,11 @@ namespace OuterMarketParser.Launcher
 
         public void StartProcess(OMProcessType processType, OMQueue processQueue, CancellationToken cancellationToken)
         {
-            List<string> links = new LinkGenerator().GenerateCianLinks();
+            OuterMarketSettings settings = new OuterMarketSettings();
+            List<string> links = new LinkGenerator().GenerateCianLinks(settings);
             Parser.Cian.Client client = new Parser.Cian.Client(new CianDataParser(links));
             Model.DatabaseOperations.Client dbCient = new Model.DatabaseOperations.Client(new DataToPostgreSQL(client.Parser.GetProperty()));
+            settings.UpdateLastSuccesfulUpdateDate();
         }
 
         public void LogError(long? objectId, Exception ex, long? errorId = null)
