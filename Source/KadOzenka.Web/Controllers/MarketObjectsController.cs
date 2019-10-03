a249f449e3f531fd891a0ece77c4347d91dd271b
Симбирсофт Данила <INECAS\simbirsoft_danila>
2019-10-03 15:00:33 +0000

    CIPJSKO-11: Добавил контроллер и страницу
    git-tfs-id: [http://192.168.3.86:8080/tfs/defaultcollection]$/CIPJS/KadOzenka;C50223

---
+++ Source/KadOzenka.Web/Controllers/MarketObjectsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ObjectModel.Market;
+
+namespace KadOzenka.Web.Controllers
+{
+	public class MarketObjectsController : Controller
+	{
+		[HttpGet]
+		public IActionResult ObjectCard(long id)
+		{
+			var analogItem = OMCoreObject
+				.Where(x => x.Id == id)
+				.SelectAll()
+				.ExecuteFirstOrDefault();
+
+			return View(analogItem);
+		}
+	}
+}
+
+//10003800
