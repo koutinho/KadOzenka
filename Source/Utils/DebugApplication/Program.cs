3c8b91d1e07711598507d59f598be5fe9f680a7e
Силанов Ярослав Олегович <INECAS\silanov>
2019-10-03 11:17:43 +0000

    Выгрузка из excele файла росреестра в БД при помощи ORM и IronXL;
    git-tfs-id: [http://192.168.3.86:8080/tfs/defaultcollection]$/CIPJS/KadOzenka;C50198

--- Source/Utils/DebugApplication/Program.cs
+++ Source/Utils/DebugApplication/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 
 using Core.Register.LongProcessManagment;
+using IronXL;
+using ObjectModel.Directory;
+using ObjectModel.Market;
 
 namespace DebugApplication
 {
@@ -10,9 +13,58 @@ namespace DebugApplication
     {
         static void Main(string[] args)
         {
-            LongProcessManagementService service = new LongProcessManagementService();
-            service.Start();
-            return;
+            LoadExcel();
+            //LongProcessManagementService service = new LongProcessManagementService();
+            //service.Start();
+            //return;
         }
+
+        static void LoadExcel()
+        {
+            string log = String.Empty;
+
+            WorkBook workbook = WorkBook.Load(@"C:\Users\silanov\Desktop\Лист Microsoft Excel.xlsx");
+            WorkSheet sheet = workbook.WorkSheets.First();
+            int iterator = 0;
+            foreach (RangeRow row in sheet.Rows)
+            {
+
+                try
+                {
+                    var analogObject = new OMCoreObject
+                    {
+                        Market_Code = MarketTypes.Rosreestr,
+                        CadastralNumber = row.ElementAt(0).ToString(),
+                        PropertyType_Code = GetPropertyType(row.ElementAt(12).ToString()),
+                        DealType_Code =  DealType.SaleDeal
+                    };
+                    analogObject.Save();
+                    log += $"{sheet.Rows.IndexOf(row)};true;\n";
+                }
+                catch(Exception ex)
+                {
+                    log += $"Error===>{sheet.Rows.IndexOf(row)};{row.ElementAt(0)};false;{ex.Message}\n";
+                }
+                if (iterator >= 10) break;
+                iterator++;
+            }
+            Console.WriteLine(log);
+        }
+
+        static PropertyTypes GetPropertyType(string data)
+        {
+            switch (data)
+            {
+                case "нежилое здание":
+                case "жилой дом": return PropertyTypes.Building;
+                case "комната":
+                case "нежилое помещение":
+                case "квартира": return PropertyTypes.Pllacement;
+                case "машино-место": return PropertyTypes.Parking;
+                case "сооружение": return PropertyTypes.Construction;
+            }
+            return PropertyTypes.None;
+        }
+
     }
 }
