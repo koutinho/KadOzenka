e4eb4348a2f786f5ecc9f6a274a23f8fc47ad9c3
Еремин Вячеслав Валерьевич <INECAS\eremin>
2019-10-21 15:23:31 +0000

    CIPJSKO-17 Сделать блок "Скриншоты" в карточке объекта аналога
    git-tfs-id: [http://192.168.3.86:8080/tfs/defaultcollection]$/CIPJS/KadOzenka;C50746

--- Source/KadOzenka.Web/Controllers/MarketObjectsController.cs
+++ Source/KadOzenka.Web/Controllers/MarketObjectsController.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Core.Main.FileStorages;
+using Core.Shared.Extensions;
+using Microsoft.AspNetCore.Mvc;
 using ObjectModel.Market;
-using System.Diagnostics;
+using System.Linq;
 
 namespace KadOzenka.Web.Controllers
 {
@@ -13,9 +15,45 @@ namespace KadOzenka.Web.Controllers
 				.Where(x => x.Id == id)
 				.SelectAll()
 				.ExecuteFirstOrDefault();
+
+            if (analogItem != null)
+            {
+                var screenList = OMScreenshots.Where(x => x.InitialId == id)
+                    .SelectAll().Execute()
+                    .Select(x => (x.Id, x.CreationDate)).ToList();
+
+                if(screenList.IsNotEmpty())
+                    ViewBag.ScreenShots = screenList;
+            }
+
             return View(analogItem);
 		}
-	}
+
+        /*private List<(DateTime, string)> GetScreenShots(long id)
+        {
+            var result = new List<(DateTime, string)>();
+
+            var screenList =  OMScreenshots.Where(x => x.InitialId == id).SelectAll().Execute();
+
+            foreach (OMScreenshots screen in screenList)
+            {
+                var filePath = FileStorageManager.GetPathForFileFolder("MarketObjectScreenShot", screen.CreationDate.Value);
+                string fullFileName = Path.Combine(filePath, screen.Id.ToString());
+
+                result.Add((screen.CreationDate.Value, fullFileName));
+            }
+
+            return result;
+        }*/
+
+        [HttpGet]
+        public FileResult ShowFile(long id)
+        {
+            var screen = OMScreenshots.Where(x => x.Id == id).SelectAll().ExecuteFirstOrDefault();
+            var fileStream = FileStorageManager.GetFileStream("MarketObjectScreenShot", screen.CreationDate.Value, id.ToString());
+            return File(fileStream, screen.Type, id.ToString());
+        }
+    }
 }
 
 //10003800
