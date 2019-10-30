ddca5575ed5e3037d0cfff8d3f690e71227933d5
Терешечкин Михаил Юрьевич <tereshechkin@genix.pro>
2019-10-30 10:14:32 +0000

    Заготовка для импортера
    git-tfs-id: [http://192.168.3.86:8080/tfs/defaultcollection]$/CIPJS/KadOzenka;C51097

---
+++ Source/KadOzenka.Dal/DataImport/DataImporter.cs
@@ -0,0 +1,50 @@
+﻿using Core.Register;
+using GemBox.Spreadsheet;
+using KadOzenka.Dal.DataExport;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace KadOzenka.Dal.DataImport
+{
+	public class DataImporter
+	{
+		public static void ImportDataFromExcel(int mainRegisterId, ExcelFile excelFile, List<DataExportColumn> columns)
+		{
+			var mainWorkSheet = excelFile.Worksheets[0];
+
+			CancellationTokenSource cancelTokenSource = new CancellationTokenSource();
+			ParallelOptions options = new ParallelOptions
+			{
+				CancellationToken = cancelTokenSource.Token,
+				MaxDegreeOfParallelism = 10
+			};
+
+			Parallel.ForEach(mainWorkSheet.Rows, options, x =>
+			{
+				try
+				{
+					// Найти ИД объекта по ключевым полям
+					long objectId = 0;
+
+					RegisterObject registerObject = new RegisterObject((int)mainRegisterId, (int)objectId);
+
+					foreach(var column in columns.Where(y => !y.IsKey))
+					{
+						object value = null;
+						registerObject.SetAttributeValue((int)column.AttributrId, value);
+
+						RegisterStorage.Save(registerObject);
+					}
+				}
+				catch (Exception ex)
+				{
+					
+				}
+			});
+		}
+	}
+}
