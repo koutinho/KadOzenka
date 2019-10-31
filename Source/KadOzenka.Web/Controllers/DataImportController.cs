9909c969adccd4da5d55773996177fa8c380aa88
Митрюшина Наталья Николаевна <INECAS\cp_mitryushina>
2019-10-31 16:23:16 +0000

    CIPJSKO-20 Add data import
    git-tfs-id: [http://192.168.3.86:8080/tfs/defaultcollection]$/CIPJS/KadOzenka;C51165

---
+++ Source/KadOzenka.Web/Controllers/DataImportController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Core.UI.Registers.Controllers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using GemBox.Spreadsheet;
+using KadOzenka.Web.Models.DataUpload;
+using KadOzenka.Dal.DataExport;
+using KadOzenka.Dal.DataImport;
+
+namespace KadOzenka.Web.Controllers
+{
+	public class DataImportController : BaseController
+	{
+		[HttpGet]
+		public IActionResult DataImport(string registerViewId, long? mainRegisterId)
+		{
+			if (!string.IsNullOrEmpty(registerViewId))
+			{
+				ViewBag.RegisterViewId = registerViewId;
+			}
+			if (mainRegisterId.HasValue)
+			{
+				ViewBag.MainRegisterId = mainRegisterId;
+			}
+
+			return View();
+		}
+
+		[HttpPost]
+		public ActionResult ParseFileColumns(List<IFormFile> files)
+		{
+			if (files == null || files.Count == 0)
+			{
+				return EmptyResponse();
+			}
+
+			try
+			{
+				var file = files.FirstOrDefault();
+				var columnsNames = new List<object>();
+				using (var stream = file.OpenReadStream())
+				{
+					var excelFile = ExcelFile.Load(stream, new XlsxLoadOptions());
+					var ws = excelFile.Worksheets[0];
+					var headerRow = ws.Rows[0];
+					columnsNames
+						.AddRange(headerRow.AllocatedCells.Where(x => x.Value != null)
+							.Select((x, index) => new { Id = index, Name = x.Value.ToString() }));
+					if (columnsNames.Count == 0)
+					{
+						throw new Exception("Выбран пустой файл");
+					}
+				}
+				return Content(JsonConvert.SerializeObject(columnsNames), "application/json");
+			}
+			catch (Exception ex)
+			{
+				return Content(ex.Message);
+			}
+		}
+
+		[HttpPost]
+		public IActionResult ImportDataFromExcel(int mainRegisterId, IFormFile file, List<DataColumnDto> columns)
+		{
+			ExcelFile excelFile;
+			using (var stream = file.OpenReadStream())
+			{
+				excelFile = ExcelFile.Load(stream, new XlsxLoadOptions());
+			}
+
+			DataImporter.ImportDataFromExcel(mainRegisterId, excelFile, columns.Select(x => new DataExportColumn
+				{ AttributrId = x.AttributeId, ColumnName = x.ColumnName, IsKey = x.IsKey }).ToList());
+
+			return NoContent();
+		}
+	}
+}
