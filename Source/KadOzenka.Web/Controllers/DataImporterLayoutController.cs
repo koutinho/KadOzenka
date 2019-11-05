41e0f65d854ed6a877ab6dac9b35eeb9749d17c0
Митрюшина Наталья Николаевна <INECAS\cp_mitryushina>
2019-11-05 07:07:25 +0000

    CIPJSKO-24 intermediate commit (add main infrastracture)
    git-tfs-id: [http://192.168.3.86:8080/tfs/defaultcollection]$/CIPJS/KadOzenka;C51216

---
+++ Source/KadOzenka.Web/Controllers/DataImporterLayoutController.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using Core.UI.Registers.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using ObjectModel.Common;
+
+namespace KadOzenka.Web.Controllers
+{
+	public class DataImporterLayoutController : BaseController
+	{
+		[HttpGet]
+		public ActionResult MainData(long importId)
+		{
+			var import = OMImportFromTemplates
+				.Where(x => x.Id == importId)
+				.SelectAll()
+				.ExecuteFirstOrDefault();
+
+			return View(importId);
+		}
+
+		[HttpGet]
+		public ActionResult Download(long importId)
+		{
+			return EmptyResponse();
+		}
+
+		[HttpGet]
+		public ActionResult ImportReStart(long importId)
+		{
+			return EmptyResponse();
+		}
+	}
+}
