d000927fa18be9f4ec92fd86e3dc0369a4f006b2
Терешечкин Михаил Юрьевич <tereshechkin@genix.pro>
2019-11-08 09:19:36 +0000

    Sud - заготовка
    git-tfs-id: [http://192.168.3.86:8080/tfs/defaultcollection]$/CIPJS/KadOzenka;C51397

---
+++ Source/KadOzenka.Web/Controllers/SudController.cs
@@ -0,0 +1,13 @@
+﻿using Core.UI.Registers.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KadOzenka.Web.Controllers
+{
+    public class SudController : BaseController
+	{
+
+    }
+}
