54a8d6fc5e133abf72362f769ccdba936fec9158
Божко Александр Сергеевич(CorePartners) <INECAS\cp_bozhko>
2019-11-08 14:06:17 +0000

    CIPJSKO-29 создание модели, формы, экшенов
    git-tfs-id: [http://192.168.3.86:8080/tfs/defaultcollection]$/CIPJS/KadOzenka;C51428

--- Source/KadOzenka.Web/Controllers/SudController.cs
+++ Source/KadOzenka.Web/Controllers/SudController.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
 using Core.UI.Registers.Controllers;
+using DevExpress.DataProcessing.InMemoryDataProcessor;
 using KadOzenka.Web.Models.Sud;
+using Kendo.Mvc.Extensions;
 using ObjectModel.Sud;
 
 namespace KadOzenka.Web.Controllers
@@ -22,6 +25,41 @@ namespace KadOzenka.Web.Controllers
 		public ActionResult EditObjectCard(ObjectCardModel data)
 		{
 			return EmptyResponse();
+		}
+
+		[HttpGet]
+		public ActionResult ReportLink(int reportLinkId)
+		{
+			OMOtchetLink reportLink;
+
+			reportLink = OMOtchetLink
+				.Where(x => x.Id == reportLinkId)
+				.Execute().FirstOrDefault();
+
+			ReportLinkModel model = reportLinkId != 0 && reportLink != null ? ReportLinkModel.FromEntity(reportLink) : new ReportLinkModel();
+
+
+			return View(model);
+		}
+
+		[HttpPost]
+		public ActionResult ReportLink(ReportLinkModel reportLinkViewModel)
+		{
+			var errros = ModelState.Where(x => x.Value.Errors.Count > 0).Select(x => new
+			{
+				Control = x.Key,
+				Message = string.Join("\n", x.Value.Errors.Select(e =>
+				{
+					if (e.ErrorMessage == "The value '' is invalid.")
+					{
+						return $"{e.ErrorMessage} Поле {x.Key}";
+					}
+
+					return e.ErrorMessage;
+				}))
+			});
+
+			return Json(new { });
 		}
 	}
 }
---
+++ Source/KadOzenka.Web/Models/Sud/ReportLinkModel.cs
@@ -0,0 +1,50 @@
+﻿using System.ComponentModel.DataAnnotations;
+using ObjectModel.Sud;
+
+namespace KadOzenka.Web.Models.Sud
+{
+	public class ReportLinkModel
+	{
+		/// <summary>
+		/// Ид отчета
+		/// </summary>
+		public long Id { get; set; }
+
+		/// <summary>
+		/// Рыночная стоимость
+		/// </summary>
+		[Required(ErrorMessage = "Поле Рыночная стоимост обязательное")]
+		public decimal? Rs { get; set; }
+
+		/// <summary>
+		/// Удельная стоимость
+		/// </summary>
+		[Required(ErrorMessage = "Поле Удельная стоимост обязательное")]
+		public decimal? Uprs { get; set; }
+
+		/// <summary>
+		/// Текущее использование
+		/// </summary>
+		[Required(ErrorMessage = "Поле Текущее использование обязательное")]
+		public string Use { get; set; }
+
+		/// <summary>
+		/// Примечание
+		/// </summary>
+		public string Descr { get; set; }
+
+		public static ReportLinkModel FromEntity(OMOtchetLink entity)
+		{
+			var res = new ReportLinkModel();
+			res.Id = entity.Id;
+			res.Descr = entity.Descr;
+			res.Rs = entity.Rs.GetValueOrDefault();
+			res.Uprs = entity.Uprs.GetValueOrDefault();
+			res.Use = entity.Use;
+
+			return res;
+
+		}
+	}
+
+}
