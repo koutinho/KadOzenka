412788855da220b7d77fab663ceeabcaed9f5357
Митрюшина Наталья Николаевна <INECAS\cp_mitryushina>
2019-11-08 12:10:37 +0000

    CIPJSKO-30 started to implement objectCard
    git-tfs-id: [http://192.168.3.86:8080/tfs/defaultcollection]$/CIPJS/KadOzenka;C51414

--- Source/KadOzenka.Web/Controllers/SudController.cs
+++ Source/KadOzenka.Web/Controllers/SudController.cs
@@ -1,13 +1,27 @@
-﻿using Core.UI.Registers.Controllers;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Mvc;
+using Core.UI.Registers.Controllers;
+using KadOzenka.Web.Models.Sud;
+using ObjectModel.Sud;
 
 namespace KadOzenka.Web.Controllers
 {
-    public class SudController : BaseController
+	public class SudController : BaseController
 	{
+		[HttpGet]
+		public IActionResult ObjectCard(long id)
+		{
+			var obj = OMObject
+				.Where(x => x.Id == id)
+				.SelectAll()
+				.ExecuteFirstOrDefault();
 
-    }
+			return View(ObjectCardModel.FromOMObject(obj));
+		}
+
+		[HttpPost]
+		public ActionResult EditObjectCard(ObjectCardModel data)
+		{
+			return EmptyResponse();
+		}
+	}
 }
---
+++ Source/KadOzenka.Web/Models/Sud/ObjectCardModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using ObjectModel.Sud;
+
+namespace KadOzenka.Web.Models.Sud
+{
+	public class ObjectCardModel
+	{
+		public long Id { get; set; }
+		[Display(Name = "Кадастровый номер")]
+		public string Kn { get; set; }
+		[Display(Name = "Дата определения стоимости")]
+		public DateTime? Date { get; set; }
+		[Display(Name = "Площадь")]
+		public decimal? Square { get; set; }
+		[Display(Name = "Оспариваемая стоимость")]
+		public decimal? Kc { get; set; }
+		[Display(Name = "Тип объекта")]
+		public long? ObjectType { get; set; }
+		[Display(Name = "Адрес")]
+		public string Address { get; set; }
+		[Display(Name = "Наименование (ТЦ, БЦ)")]
+		public string NameCenter { get; set; }
+		[Display(Name = "Внесено в статистику ДГИ")]
+		public string StatDgi { get; set; }
+		[Display(Name = "Заказчик / Истец")]
+		public string Owner { get; set; }
+		[Display(Name = "Требуется дополнительный анализ")]
+		public bool? AdditionalAnalysisRequired { get; set; }
+
+		public static ObjectCardModel FromOMObject(OMObject omObject)
+		{
+			return new ObjectCardModel
+			{
+				Kn = omObject.Kn,
+				Date = omObject.Date,
+				Square = omObject.Square,
+				Kc = omObject.Kc,
+				ObjectType = omObject.Typeobj,
+				Address = omObject.Adres,
+				NameCenter = omObject.NameCenter,
+				StatDgi = omObject.StatDgi,
+				Owner = omObject.Owner,
+				AdditionalAnalysisRequired = omObject.Workstat != null && Convert.ToBoolean(omObject.Workstat.Value)
+			};
+		}
+	}
+}
