e87e1d644d2a2ea8b9db50e1c85c5aa86c9a8cf2
Божко Александр Сергеевич(CorePartners) <INECAS\cp_bozhko>
2019-11-21 12:13:16 +0000

    CIPJSKO-41 добавил создание/редактирование
    git-tfs-id: [http://192.168.3.86:8080/tfs/defaultcollection]$/CIPJS/KadOzenka;C51960

---
+++ Source/KadOzenka.Web/Controllers/CommissionController.cs
@@ -0,0 +1,95 @@
+﻿
+using System;
+using System.Linq;
+using System.Transactions;
+using CIPJS.Models.Commission;
+using Core.UI.Registers.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using ObjectModel.Commission;
+
+namespace KadOzenka.Web.Controllers
+{
+	public class CommissionController : BaseController
+	{
+		[HttpGet]
+		public ActionResult EditCommission(long commissionId)
+		{
+			OMCost commission = OMCost
+				.Where(x => x.Id == commissionId)
+				.SelectAll()
+				.ExecuteFirstOrDefault();
+
+			var model = commission != null
+				? CommissionModel.FromEntity(commission)
+				: CommissionModel.FromEntity(new OMCost());
+
+			return View(model);
+		}
+
+		[HttpPost]
+		public ActionResult EditCommission(CommissionModel commissionViewModel)
+		{
+			if (!ModelState.IsValid)
+			{
+				return Json(new
+				{
+					Errors = ModelState.Where(x => x.Value.Errors.Count > 0).Select(x => new
+					{
+						Control = x.Key,
+						Message = string.Join("\n", x.Value.Errors.Select(e =>
+						{
+							if (e.ErrorMessage == "The value '' is invalid.")
+							{
+								return $"{e.ErrorMessage} Поле {x.Key}";
+							}
+
+							return e.ErrorMessage;
+						}))
+					})
+				});
+			}
+
+			OMCost commission = OMCost
+				.Where(x => x.Id == commissionViewModel.Id)
+				.SelectAll()
+				.Execute().FirstOrDefault();
+
+			if (commissionViewModel.Id != -1 && commission == null)
+			{
+				return NotFound();
+			}
+
+			if (commission == null)
+			{
+				commission = new OMCost();
+			}
+
+			CommissionModel.ToEntity(commissionViewModel, ref commission);
+			long id;
+			using (var ts = new TransactionScope())
+			{
+				try
+				{
+					id = commission.Save();
+					ts.Complete();
+				}
+				catch (Exception e)
+				{
+					return Json(new
+					{
+						Errors =
+							new
+							{
+								Control = string.Empty,
+								e.Message
+							}
+					});
+				}
+			}
+
+			commissionViewModel.Id = id;
+			return Json(new { Success = "Сохранено успешно", data = commissionViewModel });
+		}
+
+	}
+}
---
+++ Source/KadOzenka.Web/Models/Commission/CommissionModel.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using DevExpress.CodeParser;
+using ObjectModel.Commission;
+
+namespace CIPJS.Models.Commission
+{
+	public class CommissionModel
+	{
+		/// <summary>
+		/// Идентификатор (ID)
+		/// </summary>
+		[Display(Name = "Идентификатор")]
+		public long Id { get; set; }
+		/// <summary>
+		/// Тип комиссии 
+		/// </summary>
+		[Display(Name = "Тип комиссии")]
+		public long CommissionType { get; set; }
+		/// <summary>
+		/// Кадастровый номер объекта
+		/// </summary>
+		[Display(Name = "Кадастровый номер объекта")]
+		public string Kn { get; set; }
+		/// <summary>
+		/// Оспариваемая кадастровая стоимость
+		/// </summary>
+		[Display(Name = "Оспариваемая кадастровая стоимость")]
+		public decimal? Kc { get; set; }
+		/// <summary>
+		/// Дата определения кадастровой стоимости
+		/// </summary>
+		[Display(Name = "Дата определения кадастровой стоимости")]
+		public DateTime? DateKc { get; set; }
+		/// <summary>
+		/// Номер заявления
+		/// </summary>
+		[Display(Name = "Номер заявления")]
+		public string StatementNumber { get; set; }
+		/// <summary>
+		/// Дата заявления
+		/// </summary>
+		[Display(Name = "Дата заявления")]
+		public DateTime? StatementDate { get; set; }
+		/// <summary>
+		/// Статус заявителя
+		/// </summary>
+		[Display(Name = "Статус заявителя")]
+		public long? ApplicantStatus { get; set; }
+		/// <summary>
+		/// Номер решения
+		/// </summary>
+		[Display(Name = "Номер решения")]
+		public string DecisionNumber { get; set; }
+		/// <summary>
+		/// Дата решения
+		/// </summary>
+		[Display(Name = "Дата решения")]
+		public DateTime? DecisionDate { get; set; }
+		/// <summary>
+		/// Решение комиссии
+		/// </summary>
+		[Display(Name = "Решение комиссии")]
+		public long? DecisionResult { get; set; }
+		/// <summary>
+		/// Рыночная стоимость после оспаривания
+		/// </summary>
+		[Display(Name = "Рыночная стоимость после оспаривания")]
+		public decimal? MarketValue { get; set; }
+		/// <summary>
+		/// Кадастровая стоимость по решению 
+		/// </summary>
+		[Display(Name = "Кадастровая стоимость по решению")]
+		public decimal? CommissionKc { get; set; }
+		/// <summary>
+		/// Группа после оспаривания
+		/// </summary>
+		[Display(Name = "Группа после оспаривания")]
+		public string CommissionGroup { get; set; }
+		/// <summary>
+		/// Изменения характеристик
+		/// </summary>
+		[Display(Name = "Изменения характеристик")]
+		public string CommissionChange { get; set; }
+
+
+		public static CommissionModel FromEntity(OMCost entity)
+		{
+			if (entity == null) return new CommissionModel();
+
+			return new CommissionModel()
+			{
+				Id = entity.Id,
+				CommissionType = entity.CommissionType,
+				CommissionKc = entity.CommissionKc,
+				CommissionChange = entity.CommissionChange,
+				CommissionGroup = entity.CommissionGroup,
+				MarketValue = entity.MarketValue,
+				DecisionNumber = entity.DecisionNumber,
+				DecisionDate = entity.DecisionDate,
+				DateKc = entity.DateKc,
+				DecisionResult = entity.DecisionResult,
+				StatementDate = entity.StatementDate,
+				ApplicantStatus = entity.ApplicantStatus,
+				Kn = entity.Kn,
+				Kc = entity.Kc,
+				StatementNumber = entity.StatementNumber
+
+			};
+		}
+
+		public static void ToEntity(CommissionModel commissionViewModel, ref OMCost entity)
+		{
+			entity.MarketValue = commissionViewModel.MarketValue;
+			entity.CommissionType = commissionViewModel.CommissionType;
+			entity.CommissionKc = commissionViewModel.CommissionKc;
+			entity.StatementDate = commissionViewModel.StatementDate;
+			entity.DecisionNumber = commissionViewModel.DecisionNumber;
+			entity.DateKc = commissionViewModel.DateKc;
+			entity.Kn = commissionViewModel.Kn;
+			entity.Kc = commissionViewModel.Kc;
+			entity.StatementNumber = commissionViewModel.StatementNumber;
+			entity.DecisionDate = commissionViewModel.DecisionDate;
+			entity.DecisionResult = commissionViewModel.DecisionResult;
+			entity.ApplicantStatus = commissionViewModel.ApplicantStatus;
+			entity.CommissionChange = commissionViewModel.CommissionChange;
+			entity.CommissionGroup = commissionViewModel.CommissionGroup;
+		}
+	}
+
+}
