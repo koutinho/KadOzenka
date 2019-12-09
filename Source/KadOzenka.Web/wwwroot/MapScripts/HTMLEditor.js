6fbd1e186a34eda9d2bb1d5981a9d882889aeaa7
Митрюшина Наталья Николаевна <INECAS\cp_mitryushina>
2019-12-09 12:29:38 +0000

    добавила галлерею на карточку объекта на карте
    git-tfs-id: [http://192.168.3.86:8080/tfs/defaultcollection]$/CIPJS/KadOzenka;C52847

--- Source/KadOzenka.Web/wwwroot/MapScripts/HTMLEditor.js
+++ Source/KadOzenka.Web/wwwroot/MapScripts/HTMLEditor.js
@@ -20,6 +20,9 @@
                 </div>
             </div>
             <div class="Container">
+                    ${getImageGallery(cartData)}
+            </div>
+            <div class="Container">
                 <div class="Name">Тип сделки</div>
                 <div class="Value">${cartData.dealType}</div>
             </div>
@@ -84,4 +87,124 @@ function showCardContainer() {
     document.getElementById("data").classList.remove("Hidden");
     document.getElementById("map").style.width = "calc(100% - 350px)";
     map.container.fitToViewport();
+};
+
+function getValidImageUrlArray(source) {
+    let imageUrlArray = [];
+    if (source) {
+        imageUrlArray = source.split(',');
+    }
+
+    return imageUrlArray;
+};
+
+function checkImageUrl(url) {
+    return new Promise((resolve, reject) => {
+        let img = new Image();
+        img.addEventListener('load', e => {
+            resolve(url);
+        });
+        img.addEventListener('error', () => {
+            reject(new Error(`Failed to load image's URL: ${url}`));
+        });
+        img.src = url;
+    });
+}
+
+function getImageGallery(cartData) {
+    let imgUrs = getValidImageUrlArray(cartData.images);
+    let promises = imgUrs.map(url => {
+        return checkImageUrl(url);
+    });
+    if (!Promise.allSettled) {
+        Promise.allSettled = function (promises) {
+            return Promise.all(promises.map(p => Promise.resolve(p).then(value => ({
+                state: 'fulfilled',
+                value: value
+            }), error => ({
+                state: 'rejected',
+                reason: error
+            }))));
+        };
+    }
+    Promise.allSettled(promises)
+        .then(results => {
+            let existingImages = [];
+            results.forEach((result, num) => {
+                if (result.status == "fulfilled") {
+                    existingImages.push(result.value);
+                }
+            });
+            if (existingImages.length > 0) {
+                document.getElementById("sliderGallery" + cartData.id).innerHTML =
+                    `
+                        <div class='imageGallery visibleImageGallery' order="0" onclick="onImageClick(${cartData.id});" style="background-image:url(${existingImages[0]});"></div>`;
+                for (let i = 1; i < existingImages.length; i++) {
+                    document.getElementById("sliderGallery" + cartData.id).innerHTML +=
+                        `<div class='imageGallery' order="${i}" onclick="onImageClick(${cartData.id});" style="background-image:url(${existingImages[i]});"></div>`;
+                }
+                if (existingImages.length > 1) {
+                    document.getElementById("sliderGallery" + cartData.id).innerHTML +=
+                        `<div class="arrowsContainer">
+                             <div class="arrowLeftContainer" onclick="onLeftButtonClick(${cartData.id},${existingImages.length});">
+                                 <span class="fas fa-arrow-left"></span>
+                                 
+                             </div>
+                             <div class="arrowRightContainer" onclick="onRightButtonClick(${cartData.id},${existingImages.length});">
+                                 <span class="fas fa-arrow-right"></span>
+                             </div>
+                         </div>`;
+                }
+                document.getElementById("sliderGallery" + cartData.id).innerHTML +=
+                    `<div class="imageCountsContainer" onclick="onImageClick(${cartData.id});">
+                        <div class="imageCounts">1/${existingImages.length}</div>
+                     </div>`;
+            } else {
+                document.getElementById("sliderGallery" + cartData.id).innerHTML = `Нет фото`;
+            }
+        });
+
+    return `${
+            `<div class="sliderGalleryContainer">
+                <div class="sliderGallery" id ="sliderGallery${cartData.id}" >
+                    <div class="preloaderImg"></div>
+                </div>
+             </div>
+            `}`;
+};
+
+function onLeftButtonClick(cardId, imgCount) {
+    let parent = document.getElementById("sliderGallery" + cardId);
+    let imageCount = $(parent).find(".imageCounts")[0];
+    let visibleImage = $(parent).find(".visibleImageGallery")[0];
+    $(visibleImage).removeClass("visibleImageGallery");
+    if (visibleImage.getAttribute("order") == 0) {
+        let newVisibleImage = $(parent).find(".imageGallery")[imgCount - 1];
+        $(newVisibleImage).addClass("visibleImageGallery");
+        imageCount.innerHTML = imgCount + "/" + imgCount;
+    } else {
+        let newVisibleImage = visibleImage.previousSibling;
+        $(newVisibleImage).addClass("visibleImageGallery");
+        imageCount.innerHTML = (parseInt(newVisibleImage.getAttribute("order")) + 1)  + "/" + imgCount;
+    }
+}
+
+function onRightButtonClick(cardId, imgCount) {
+    let parent = document.getElementById("sliderGallery" + cardId);
+    let imageCount = $(parent).find(".imageCounts")[0];
+    let visibleImage = $(parent).find(".visibleImageGallery")[0];
+    $(visibleImage).removeClass("visibleImageGallery");
+    if (visibleImage.getAttribute("order") == imgCount - 1) {
+        let newVisibleImage = $(parent).find(".imageGallery")[0];
+        $(newVisibleImage).addClass("visibleImageGallery");
+        imageCount.innerHTML = 1 + "/" + imgCount;
+    } else {
+        let newVisibleImage = visibleImage.nextSibling;
+        $(newVisibleImage).addClass("visibleImageGallery");
+        imageCount.innerHTML = (parseInt(newVisibleImage.getAttribute("order")) + 1) + "/" + imgCount;
+    }
+}
+
+function onImageClick(cardId) {
+   //TODO: future handler for image click event
 };
