bed1fcb6b5a86395e644b533ef73e6dfca254141
Божко Александр Сергеевич(CorePartners) <INECAS\cp_bozhko>
2019-12-11 18:21:13 +0000

    CIPJSKO-51 полная реализация певых 2 карточек
    git-tfs-id: [http://192.168.3.86:8080/tfs/defaultcollection]$/CIPJS/KadOzenka;C53000

---
+++ Source/KadOzenka.Web/Controllers/TaskController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Transactions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ObjectModel.KO;
+
+namespace KadOzenka.Web.Controllers
+{
+	public class TaskController : Controller
+    {
+	    [HttpGet]
+		public IActionResult TourEstimates()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult TourEstimates([FromForm]int id, [FromForm]string year)
+        {
+	        if (string.IsNullOrEmpty(year))
+	        {
+		        return Json(new {Error = "Поле не должно быть пустым"});
+	        }
+
+	        var tour = OMTour.Where(x => x.Id == id).SelectAll().ExecuteFirstOrDefault();
+
+	        if (tour == null)
+	        {
+		        tour = new OMTour();
+	        }
+
+	        tour.Year = int.Parse(year);
+
+	        int idSave; 
+			using (var ts = new TransactionScope())
+	        {
+		        idSave = tour.Save();
+		        ts.Complete();
+	        }
+
+			return Json(new {Success = "Сохранение выполненно", Id = idSave });
+        }
+
+
+		public JsonResult GetTourEstimations()
+        {
+            var tours = OMTour.Where(x => x).SelectAll().Execute()
+                .Select(x => new
+                {
+                    x.Id,
+                    Text = x.Year
+                });
+
+           return Json(tours);
+        }
+    }
+}
