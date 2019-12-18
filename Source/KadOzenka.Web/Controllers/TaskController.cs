cc4c45eaabbb57bba9c3771cf8676abce025bed4
simbirsoft_lidiya <INECAS\simbirsoft_lidiya>
2019-12-18 11:47:24 +0000

    CIPJSKO-51 fix
    git-tfs-id: [http://192.168.3.86:8080/tfs/defaultcollection]$/CIPJS/KadOzenka;C53329

--- Source/KadOzenka.Web/Controllers/TaskController.cs
+++ Source/KadOzenka.Web/Controllers/TaskController.cs
@@ -115,19 +115,53 @@ namespace KadOzenka.Web.Controllers
 			};
 			
 			query.AddColumn(OMGroup.GetColumn(x => x.ParentId, "ParentId"));
+			query.AddColumn(OMGroup.GetColumn(x => x.GroupAlgoritm_Code, "GroupAlgoritm"));
 			query.AddColumn(OMGroup.GetColumn(x => x.GroupName, "GroupName"));
 			query.AddColumn(OMTourGroup.GetColumn(x => x.TourId, "TourId"));
 
 			DataTable table = query.ExecuteQuery();
 			List<GroupTreeDto> groups = new List<GroupTreeDto>();
 
+			GroupTreeDto oks = new GroupTreeDto
+			{
+				Id = (long)KoGroupAlgoritm.MainOKS,
+				ParentId = null,
+				GroupName = "Основная группа ОКС"
+			};
+			groups.Add(oks);
+
+			GroupTreeDto parcel = new GroupTreeDto
+			{
+				Id = (long)KoGroupAlgoritm.MainParcel,
+				ParentId = null,
+				GroupName = "Основная группа Участки"
+			};
+			groups.Add(parcel);
+
 			foreach (DataRow row in table.Rows)
 			{
 				GroupTreeDto str = new GroupTreeDto();
 
 				str.Id = long.Parse(row["Id"].ToString());
+
 				long? parent = long.Parse(row["ParentId"].ToString());
-				str.ParentId = (parent == -1) ? null : parent;
+				long groupAlgoritm = long.Parse(row["GroupAlgoritm"].ToString());
+				if (parent == -1)
+				{
+					if (groupAlgoritm == (long)KoGroupAlgoritm.MainOKS)
+					{
+						str.ParentId = oks.Id;
+					}
+					else if (groupAlgoritm == (long)KoGroupAlgoritm.MainParcel)
+					{
+						str.ParentId = parcel.Id;
+					}
+				}
+				else
+				{
+					str.ParentId = parent;
+				}
+								
 				str.GroupName = row["GroupName"].ToString();
 				str.TourId = long.Parse(row["TourId"].ToString());
 
