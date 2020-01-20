d5ffe70f332ca57afa4f408728ba092e5f85ccd3
Митрюшина Наталья Николаевна <INECAS\cp_mitryushina>
2020-01-20 14:20:09 +0000

    CIPJSKO-88 добавление в ОМ реестра Подписанты, добавление соотв. параметра в отчеты уведомлений, небольшой рефакторинг отчетов уведомлений
    git-tfs-id: [http://192.168.3.86:8080/tfs/defaultcollection]$/CIPJS/KadOzenka;C54252

--- Source/KadOzenka.Dal/FastReports/AdoptionOfDeclarationNotificationReport.cs
+++ Source/KadOzenka.Dal/FastReports/AdoptionOfDeclarationNotificationReport.cs
@@ -1,15 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.Specialized;
-using System.Data;
-using System.Linq;
+﻿using System.Collections.Specialized;
 using Core.Shared.Extensions;
-using Core.SRD;
-using NPetrovich;
-using ObjectModel.Core.SRD;
 using ObjectModel.Declarations;
 using ObjectModel.Directory.Declarations;
-using Platform.Reports;
 
 namespace KadOzenka.Dal.FastReports
 {
@@ -25,83 +17,8 @@ namespace KadOzenka.Dal.FastReports
 			return ReportType.Title;
 		}
 
-		protected override DataSet GetData(NameValueCollection query, HashSet<long> objectList = null)
+		public override string GetMainData(OMDeclaration declaration, OMUved notification)
 		{
-			DataSet dataSet = new DataSet();
-			dataSet.Tables.Add("Data");
-			dataSet.Tables[0].Columns.Add("OwnerName");
-			dataSet.Tables[0].Columns.Add("OwnerAddress");
-			dataSet.Tables[0].Columns.Add("DeclarationDateIn");
-			dataSet.Tables[0].Columns.Add("DeclarationNumber");
-			dataSet.Tables[0].Columns.Add("UserIspName");
-			dataSet.Tables[0].Columns.Add("MainData");
-
-			var notification = OMUved
-				.Where(x => x.Id == ObjectId)
-				.SelectAll()
-				.Execute().FirstOrDefault();
-			if (notification == null)
-			{
-				throw new Exception($"Уведомление с ИД {ObjectId} не найдено");
-			}
-
-			var declaration = OMDeclaration
-				.Where(x => x.Id == notification.Declaration_Id)
-				.SelectAll()
-				.Execute().FirstOrDefault();
-			if (declaration == null)
-			{
-				throw new Exception($"Декларация не найдена");
-			}
-
-			var owner = OMSubject
-				.Where(x => x.Id == declaration.Owner_Id)
-				.SelectAll()
-				.Execute().FirstOrDefault();
-			if (owner == null)
-			{
-				throw new Exception($"Заявитель не найден");
-			}
-
-			var ownerName = string.Empty;
-			if (owner.Type_Code == SubjectType.Fl)
-			{
-				var petrovich = new Petrovich
-				{
-					FirstName = owner.I_Name,
-					LastName = owner.F_Name,
-					MiddleName = owner.O_Name,
-					AutoDetectGender = true
-				};
-				var inflected = petrovich.InflectTo(Case.Dative);
-				ownerName = inflected.LastName;
-				if (!string.IsNullOrWhiteSpace(inflected.FirstName) &&
-				    !string.IsNullOrWhiteSpace(inflected.MiddleName))
-				{
-					ownerName += $" {inflected.FirstName.Trim()[0]}.{inflected.MiddleName.Trim()[0]}.";
-				}
-			}
-			else
-			{
-				ownerName = owner.Name;
-			}
-
-			var book = OMBook
-				.Where(x => x.Id == declaration.Book_Id)
-				.SelectAll()
-				.Execute().FirstOrDefault();
-
-			var userIsp = OMUser
-				.Where(x => x.Id == SRDSession.GetCurrentUserId())
-				.SelectAll()
-				.ExecuteFirstOrDefault();
-
-			var userIspName = userIsp.Name;
-			if (!string.IsNullOrWhiteSpace(userIsp.Surname) && !string.IsNullOrWhiteSpace(userIsp.Patronymic))
-			{
-				userIspName += $" {userIsp.Surname.Trim()[0]}.{userIsp.Patronymic.Trim()[0]}.";
-			}
-
 			var mainData =
 				"\u0009Настоящим уведомлением сообщаем, что ГБУ «Центр имущественных платежей и\u00A0жилищного страхования» " +
 				"на\u00A0основании приказа Минэкономразвития от\u00A004.06.2019 №\u00A0318 «Об\u00A0утверждении Порядка рассмотрения декларации о\u00A0характеристиках объекта " +
@@ -119,15 +36,7 @@ namespace KadOzenka.Dal.FastReports
 			}
 			mainData = mainData.Replace(" ", "\u0020");
 
-			dataSet.Tables[0].Rows.Add(
-				ownerName,
-				FormAddress(owner),
-				declaration.DateIn?.ToString("dd.MM.yyyy"),
-				$"{declaration.NumIn}/{book?.Prefics}",
-				userIspName,
-				mainData);
-
-			return dataSet;
+			return mainData;
 		}
 	}
 }
--- Source/KadOzenka.Dal/FastReports/DeclarationNotificationReport.cs
+++ Source/KadOzenka.Dal/FastReports/DeclarationNotificationReport.cs
@@ -1,9 +1,16 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Data;
 using System.Linq;
 using System.Text;
 using Core.Shared.Extensions;
+using Core.SRD;
+using Core.UI.Registers.Reports.Model;
 using DeepMorphy;
-using DocumentFormat.OpenXml.Office2010.ExcelAc;
+using NPetrovich;
+using ObjectModel.Core.SRD;
+using ObjectModel.Declaration;
 using ObjectModel.Declarations;
 using ObjectModel.Directory.Declarations;
 using Platform.Reports;
@@ -18,6 +25,121 @@ namespace KadOzenka.Dal.FastReports
 		{
 			MorphAnalyzer = new MorphAnalyzer();
 		}
+
+		public override void InitializeFilterValues(long objId, string senderName, bool initialisation, List<FilterValue> filterValues)
+		{
+			if (initialisation)
+			{
+				var podpisantFilterValue = filterValues.FirstOrDefault(f => f.ParamName == "Podpisant");
+				if (podpisantFilterValue != null)
+				{
+					podpisantFilterValue.ReportParameters = new List<ReportParameter>();
+					var signatories = OMSignatory.Where(x => true).SelectAll().Execute();
+					podpisantFilterValue.ReportParameters.AddRange(signatories.Select(s => new ReportParameter { Value = $"{s.FullName}, {s.Position}", Key = s.Id.ToString() }));
+				}
+			}
+		}
+
+		protected override DataSet GetData(NameValueCollection query, HashSet<long> objectList = null)
+		{
+			DataSet dataSet = new DataSet();
+			dataSet.Tables.Add("Data");
+			dataSet.Tables[0].Columns.Add("OwnerName");
+			dataSet.Tables[0].Columns.Add("OwnerAddress");
+			dataSet.Tables[0].Columns.Add("DeclarationDateIn");
+			dataSet.Tables[0].Columns.Add("DeclarationNumber");
+			dataSet.Tables[0].Columns.Add("UserIspName");
+			dataSet.Tables[0].Columns.Add("MainData");
+			dataSet.Tables[0].Columns.Add("SignatoryPosition");
+			dataSet.Tables[0].Columns.Add("SignatoryName");
+
+			var notification = OMUved
+				.Where(x => x.Id == ObjectId)
+				.SelectAll()
+				.Execute().FirstOrDefault();
+			if (notification == null)
+			{
+				throw new Exception($"Уведомление с ИД {ObjectId} не найдено");
+			}
+
+			var declaration = OMDeclaration
+				.Where(x => x.Id == notification.Declaration_Id)
+				.SelectAll()
+				.Execute().FirstOrDefault();
+			if (declaration == null)
+			{
+				throw new Exception($"Декларация не найдена");
+			}
+
+			var owner = OMSubject
+				.Where(x => x.Id == declaration.Owner_Id)
+				.SelectAll()
+				.Execute().FirstOrDefault();
+			if (owner == null)
+			{
+				throw new Exception($"Заявитель не найден");
+			}
+
+			var ownerName = string.Empty;
+			if (owner.Type_Code == SubjectType.Fl)
+			{
+				var petrovich = new Petrovich
+				{
+					FirstName = owner.I_Name,
+					LastName = owner.F_Name,
+					MiddleName = owner.O_Name,
+					AutoDetectGender = true
+				};
+				var inflected = petrovich.InflectTo(Case.Dative);
+				ownerName = inflected.LastName;
+				if (!string.IsNullOrWhiteSpace(inflected.FirstName) &&
+					!string.IsNullOrWhiteSpace(inflected.MiddleName))
+				{
+					ownerName += $" {inflected.FirstName.Trim()[0]}.{inflected.MiddleName.Trim()[0]}.";
+				}
+			}
+			else
+			{
+				ownerName = owner.Name;
+			}
+
+			var book = OMBook
+				.Where(x => x.Id == declaration.Book_Id)
+				.SelectAll()
+				.Execute().FirstOrDefault();
+
+			var userIsp = OMUser
+				.Where(x => x.Id == SRDSession.GetCurrentUserId())
+				.SelectAll()
+				.ExecuteFirstOrDefault();
+
+			var userIspName = userIsp.Name;
+			if (!string.IsNullOrWhiteSpace(userIsp.Surname) && !string.IsNullOrWhiteSpace(userIsp.Patronymic))
+			{
+				userIspName += $" {userIsp.Surname.Trim()[0]}.{userIsp.Patronymic.Trim()[0]}.";
+			}
+
+			var signatoryId = GetQueryParam<long>("Podpisant", query);
+			var signatory = OMSignatory.Where(x => x.Id == signatoryId).SelectAll().Execute().FirstOrDefault();
+			if (signatory == null)
+			{
+				throw new Exception($"Подписант не найден");
+			}
+
+			dataSet.Tables[0].Rows.Add(
+				ownerName,
+				FormAddress(owner),
+				declaration.DateIn?.ToString("dd.MM.yyyy"),
+				$"{declaration.NumIn}/{book?.Prefics}",
+				userIspName,
+				GetMainData(declaration, notification),
+				signatory.Position,
+				signatory.FullName);
+
+			return dataSet;
+		}
+
+		public abstract string GetMainData(OMDeclaration declaration, OMUved notification);
 
 		public string GetObjectTypeString(ObjectType objectType)
 		{
