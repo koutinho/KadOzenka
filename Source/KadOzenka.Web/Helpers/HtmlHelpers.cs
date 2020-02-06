b7b961590560056a69c48777288046f07c46819f
Митрюшина Наталья Николаевна <INECAS\cp_mitryushina>
2020-02-06 10:50:16 +0000

    CIPJSKO-113 изменение хелпера
    git-tfs-id: [http://192.168.3.86:8080/tfs/defaultcollection]$/CIPJS/KadOzenka;C54996

--- Source/KadOzenka.Web/Helpers/HtmlHelpers.cs
+++ Source/KadOzenka.Web/Helpers/HtmlHelpers.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
+using System.Dynamic;
 using System.IO;
 using System.Linq;
 using System.Linq.Expressions;
@@ -9,6 +11,7 @@ using Core.Shared.Misc;
 using Microsoft.AspNetCore.Html;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Kendo.Mvc.UI;
+using Kendo.Mvc.UI.Fluent;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.AspNetCore.Mvc.ViewFeatures.Internal;
 using Microsoft.AspNetCore.Routing;
@@ -26,16 +29,110 @@ namespace KadOzenka.Web.Helpers
 
 			var htmlFieldName = ExpressionHelper.GetExpressionText(expression);
 			var name = html.ViewContext.ViewData.TemplateInfo.GetFullHtmlFieldName(htmlFieldName);
+
+			var script = "<script>" +
+			             $"function clearField{name}() {{ $('#{name}').data('kendoDropDownList').value('');}}" +
+							$"$(document).ready(function(){{$('.clear-button-{name}').on('click', clearField{name});}});" +
+							"</script>";
+
+			var dataList = data.Cast<object>().ToList();
+			var dataSource = dataList.Select(x => new SelectListItem(x.GetPropertyValue(dataTextField)?.ToString(),
+				x.GetPropertyValue(dataValueField)?.ToString())).ToList();
+			var emptyItem = new SelectListItem("", null);
+			dataSource.Insert(0, emptyItem);
+
+			var clearTag = new TagBuilder("a");
+			clearTag.AddCssClass("k-button");
+			clearTag.AddCssClass("k-button-icon");
+			clearTag.AddCssClass($"clear-button-{name}");
+			clearTag.InnerHtml.AppendHtml("<span class='k-icon k-i-close'></span>");
+
+			DropDownListBuilder dropDownBuilder = html.Kendo().DropDownList()
+				.Name(name)
+				.Filter(filter)
+				.BindTo(dataSource)
+				.OptionLabel(null)
+				.Value(modelExplorer.Model?.ToString());
+
+			//TODO: need to fix the problem with showing
+			var tooltip = html.Kendo().Tooltip()
+				.Name($"#{name}Tooltip")
+				.For($"#{name}Wrapper")
+				//.Filter(".k-dropdown")
+				//.ShowOn(TooltipShowOnEvent.Click)
+				.Width(200)
+				.Height(200)
+				.Iframe(true)
+				//.Position(TooltipPosition.Top)
+				.Content("<div>content</div>")
+				.AutoHide(true);
+
+			var dropDownBuilderHtmlAttributes = new RouteValueDictionary
+			{
+				{ "style", "width: 100%;" },
+			};
+
+			if (modelExplorer.Model != null)
+			{
+				var selectedDataItem = dataList.FirstOrDefault(x =>
+					x.HasProperty(dataValueField) &&
+					x.GetPropertyValue(dataValueField).ToString() == modelExplorer.Model.ToString());
+				if (selectedDataItem != null)
+				{
+					tooltip = tooltip.Content(selectedDataItem.GetPropertyValue(dataTextField)?.ToString());
+				}
+			}
+
+			if (modelExplorer.Metadata != null && modelExplorer.Metadata.IsRequired)
+			{
+				dropDownBuilderHtmlAttributes.Add("required", "required");
+			}
+			dropDownBuilder.HtmlAttributes(dropDownBuilderHtmlAttributes);
+
+			var autocompleteDiv = new TagBuilder("div");
+			autocompleteDiv.MergeAttribute("id", $"#{name}Wrapper");
+			autocompleteDiv.AddCssClass("col-sm-11");
+			autocompleteDiv.InnerHtml.AppendHtml(dropDownBuilder);
+
+			var wrapButtonDiv = new TagBuilder("div");
+			wrapButtonDiv.AddCssClass("col-sm-1");
+			wrapButtonDiv.InnerHtml.AppendHtml(clearTag);
+
+			var container = new TagBuilder("div");
+			container.AddCssClass("row");
+			//container.MergeAttribute("style", "display: flex;");
+			container.InnerHtml.AppendHtml(autocompleteDiv);
+			container.InnerHtml.AppendHtml(wrapButtonDiv);
+			//container.InnerHtml.AppendHtml(tooltip);
+
+			var writer = new StringWriter();
+			container.WriteTo(writer, HtmlEncoder.Default);
+			StringBuilder content = new StringBuilder();
+			content.AppendLine(script);
+			content.AppendLine(writer.ToString());
+
+			return new HtmlString(content.ToString());
+		}
+
+		public static IHtmlContent KendoAutocompleteWithClearButtonFor<TModel, TValue>(this IHtmlHelper<TModel> html,
+			Expression<Func<TModel, TValue>> expression, IEnumerable data, string dataTextField = "Text",
+			string dataValueField = "Value", string filter = "contains", double minLength = 3)
+		{
+			ModelExplorer modelExplorer =
+				ExpressionMetadataProvider.FromLambdaExpression(expression, html.ViewData, html.MetadataProvider);
+
+			var htmlFieldName = ExpressionHelper.GetExpressionText(expression);
+			var name = html.ViewContext.ViewData.TemplateInfo.GetFullHtmlFieldName(htmlFieldName);
 			var textName = html.ViewContext.ViewData.TemplateInfo.GetFullHtmlFieldName(htmlFieldName) + "Text";
 
 			var script = "<script>" +
-			                $"var valid{textName}; var id{textName} = '{textName}';" +
-							$"function onOpen{textName}() {{if(this.element.attr('id') === id{textName}) valid{textName} = false;}}"+
-			                $"function onClose{textName}() {{if(this.element.attr('id') === id{textName} && !valid{textName}) {{this.value(''); $('#{textName}').prop('title', ''); $('#{name}').val(null);}}}}" +
-			                $"function onSelect{textName}(e) {{if(this.element.attr('id') === id{textName}) {{valid{textName} = true; $('#{textName}').prop('title', e.dataItem.{dataTextField}); $('#{name}').val(e.dataItem.{dataValueField});}}}}" +
-			                $"function clearField{textName}() {{ $('#{textName}').val(''); $('#{textName}').prop('title', ''); $('#{name}').val(null);}}" +
+							$"var valid{textName}; var id{textName} = '{textName}';" +
+							$"function onOpen{textName}() {{if(this.element.attr('id') === id{textName}) valid{textName} = false;}}" +
+							$"function onClose{textName}() {{if(this.element.attr('id') === id{textName} && !valid{textName}) {{this.value(''); $('#{textName}').prop('title', ''); $('#{name}').val(null);}}}}" +
+							$"function onSelect{textName}(e) {{if(this.element.attr('id') === id{textName}) {{valid{textName} = true; $('#{textName}').prop('title', e.dataItem.{dataTextField}); $('#{name}').val(e.dataItem.{dataValueField});}}}}" +
+							$"function clearField{textName}() {{ $('#{textName}').val(''); $('#{textName}').prop('title', ''); $('#{name}').val(null);}}" +
 							$"$(document).ready(function(){{$('.clear-button-{textName}').on('click', clearField{textName});}});" +
-			                "</script>";
+							"</script>";
 
 			var dataList = data.Cast<object>().ToList();
 
