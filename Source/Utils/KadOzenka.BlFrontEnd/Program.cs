2ff9536daabd7d8ec64e701237c7eb2ef5cb97e3
Силанов Ярослав Олегович <INECAS\silanov>
2020-02-07 11:23:55 +0000

    -Изменения в вызовах команд в BlFrontend;
    git-tfs-id: [http://192.168.3.86:8080/tfs/defaultcollection]$/CIPJS/KadOzenka;C55065

--- Source/Utils/KadOzenka.BlFrontEnd/Program.cs
+++ Source/Utils/KadOzenka.BlFrontEnd/Program.cs
@@ -48,13 +48,12 @@ namespace KadOzenka.BlFrontEnd
 			});
 
 			/*Загрузка информации по сделкам росреестра из excel*/
-			consoleHelper.AddCommand("1001", "Загрузка объектов росреестра из Excel",
-				ObjectReplicationExcelProcess.UploadRosreestrObjectsToDatabase);
-			consoleHelper.AddCommand("1002", "Присвоение координат объектам росреестра из базы данных",
-				() => { ObjectReplicationExcelProcess.SetRRFDBCoordinatesByYandex(); });
+			consoleHelper.AddCommand("1001", "Загрузка объектов росреестра из Excel", ObjectReplicationExcelProcess.UploadRosreestrObjectsToDatabase);
+			consoleHelper.AddCommand("1002", "Присвоение координат объектам росреестра из базы данных", () => { ObjectReplicationExcelProcess.SetRRFDBCoordinatesByYandex(); });
 
             /*Загрузка информации по предложениям из ЦИАН-а через RestApp*/
             consoleHelper.AddCommand("1101", "Запуск выгрузки объявлений объектов-аналогов из RestApp", () => { new Data().Detect(); });
+            consoleHelper.AddCommand("1107", "Запуск выгрузки объявлений объектов-аналогов с сайта Яндекс-Недвижимость", () => { new YandexChecker().FormMarketObjects(); });
             consoleHelper.AddCommand("1102", "Присвоение адресов не обработанным объектам сторонних маркетов", () => { new Addresses().Detect(); });
             consoleHelper.AddCommand("1103", "Присвоение кадастровых номеров объектам сторонних маркетов", () => { new KadNumbers().Detect(); });
             consoleHelper.AddCommand("1104", "Процедура обновления цен объектов-аналогов с ЦИАН-а", () => { new Cian().RefreshAllData(15000, true); });
@@ -62,13 +61,10 @@ namespace KadOzenka.BlFrontEnd
             consoleHelper.AddCommand("1105", "Процедура проверки данных на дублирование", () => { new Duplicates().Detect(); });
 
 			/*Генерация тайлов для карт яндекс*/
-			consoleHelper.AddCommand("1901", "Генерация JSON файлов с пиксельными координатами",
-				() => { new CoordinatesConverter().GenerateInitialCoordinates(); });
-			consoleHelper.AddCommand("1902", "Генерация тайлов для карты",
-				() => { new CoordinatesConverter().GenerateInitialImages(); });
+			consoleHelper.AddCommand("1901", "Генерация JSON файлов с пиксельными координатами", () => { new CoordinatesConverter().GenerateInitialCoordinates(); });
+			consoleHelper.AddCommand("1902", "Генерация тайлов для карты", () => { new CoordinatesConverter().GenerateInitialImages(); });
 
-			consoleHelper.AddCommand("5", "Загрузка словаря с кадастровыми номерами из Excel",
-				ObjectReplicationExcelProcess.StartImport);
+			consoleHelper.AddCommand("5", "Загрузка словаря с кадастровыми номерами из Excel", ObjectReplicationExcelProcess.StartImport);
 
 			consoleHelper.AddCommand("10", "Экспорт данных в Excel на основе шаблона", DataExportConsole.ExportData);
 			consoleHelper.AddCommand("11", "Импорт данных в Excel из шаблона", DataImportConsole.ImportData);
@@ -81,68 +77,36 @@ namespace KadOzenka.BlFrontEnd
 						ConfigurationManager.AppSettings["InitialAddressFile"],
 						ConfigurationManager.AppSettings["DefaultExceleValue"]);
 				});
-			consoleHelper.AddCommand("17", "Сформировать файл с выгрузкой адресов росреестра",
-				() =>
-				{
-					ObjectReplicationExcelProcess.FormFile(ConfigurationManager.AppSettings["GroupedAddressesFile"]);
-				});
-			consoleHelper.AddCommand("18", "Присвоение координат объектам росреестра из файла",
-				() =>
-				{
-					ObjectReplicationExcelProcess.SetRRCoordinatesByYandex(
-						ConfigurationManager.AppSettings["GroupedAddressesFile"]);
-				});
+			consoleHelper.AddCommand("17", "Сформировать файл с выгрузкой адресов росреестра", () => { ObjectReplicationExcelProcess.FormFile(ConfigurationManager.AppSettings["GroupedAddressesFile"]); });
+			consoleHelper.AddCommand("18", "Присвоение координат объектам росреестра из файла", () => { ObjectReplicationExcelProcess.SetRRCoordinatesByYandex(ConfigurationManager.AppSettings["GroupedAddressesFile"]); });
 			consoleHelper.AddCommand("19", "Парсинг XML файла", () => { XMLToJSPolyLine.parseDistricts(); });
-			consoleHelper.AddCommand("20", "Тест конвертации из Postgres в Mongo",
-				() => { ConvertToMongo.Convert(20000); });
+			consoleHelper.AddCommand("20", "Тест конвертации из Postgres в Mongo", () => { ConvertToMongo.Convert(20000); });
 
 			consoleHelper.AddCommand("30", "Тест получения значения атрибутов ГБУ", GbuTests.TestGetDataFromAllpri);
 
-			consoleHelper.AddCommand("100",
-				"Тест Парсинга объектов-аналогов с сайта Яндекс-Недвижимость (тестовая категория - Покупка офисного помещения)",
-				() => { new YandexChecker().FormMarketObjects(); });
-
 			consoleHelper.AddCommand("200", "Импорт данных KO (БД) Модель 2016", MSExporter.DoLoadBd2016Model);
-			consoleHelper.AddCommand("201", "Импорт данных KO (БД) Объекты и факторы 2016 ОНС",
-				MSExporter.DoLoadBd2016Unit_Uncomplited);
-			consoleHelper.AddCommand("202", "Импорт данных KO (БД) Объекты и факторы 2016 Сооружения",
-				MSExporter.DoLoadBd2016Unit_Construction);
-			consoleHelper.AddCommand("203", "Импорт данных KO (БД) Объекты и факторы 2016 Здания",
-				MSExporter.DoLoadBd2016Unit_Building);
-			consoleHelper.AddCommand("204", "Импорт данных KO (БД) Объекты и факторы 2016 Помещения",
-				MSExporter.DoLoadBd2016Unit_Flat);
-			consoleHelper.AddCommand("205", "Импорт данных KO (БД) Объекты и факторы 2016 Участки",
-				MSExporter.DoLoadBd2016Unit_Parcel);
+			consoleHelper.AddCommand("201", "Импорт данных KO (БД) Объекты и факторы 2016 ОНС", MSExporter.DoLoadBd2016Unit_Uncomplited);
+			consoleHelper.AddCommand("202", "Импорт данных KO (БД) Объекты и факторы 2016 Сооружения", MSExporter.DoLoadBd2016Unit_Construction);
+			consoleHelper.AddCommand("203", "Импорт данных KO (БД) Объекты и факторы 2016 Здания", MSExporter.DoLoadBd2016Unit_Building);
+			consoleHelper.AddCommand("204", "Импорт данных KO (БД) Объекты и факторы 2016 Помещения", MSExporter.DoLoadBd2016Unit_Flat);
+			consoleHelper.AddCommand("205", "Импорт данных KO (БД) Объекты и факторы 2016 Участки", MSExporter.DoLoadBd2016Unit_Parcel);
 			consoleHelper.AddCommand("206", "Импорт данных KO (XML) 2016", MSExporter.DoLoadXml2016);
-			consoleHelper.AddCommand("207", "Импорт данных KO (БД) Объекты и факторы 2016 Помещения (отсутствующие)",
-				MSExporter.DoLoadBd2016Unit_FlatSkip);
+			consoleHelper.AddCommand("207", "Импорт данных KO (БД) Объекты и факторы 2016 Помещения (отсутствующие)", MSExporter.DoLoadBd2016Unit_FlatSkip);
 
 			consoleHelper.AddCommand("210", "Импорт данных KO (БД) Модель 2018", MSExporter.DoLoadBd2018Model);
-			consoleHelper.AddCommand("211", "Импорт данных KO (БД) Объекты и факторы 2018 ОНС",
-				MSExporter.DoLoadBd2018Unit_Uncomplited);
-			consoleHelper.AddCommand("212", "Импорт данных KO (БД) Объекты и факторы 2018 Сооружения",
-				MSExporter.DoLoadBd2018Unit_Construction);
-			consoleHelper.AddCommand("213", "Импорт данных KO (БД) Объекты и факторы 2018 Здания",
-				MSExporter.DoLoadBd2018Unit_Building);
-			consoleHelper.AddCommand("214", "Импорт данных KO (БД) Объекты и факторы 2018 Помещения",
-				MSExporter.DoLoadBd2018Unit_Flat);
-			consoleHelper.AddCommand("215", "Импорт данных KO (БД) Объекты и факторы 2018 Участки",
-				MSExporter.DoLoadBd2018Unit_Parcel);
+			consoleHelper.AddCommand("211", "Импорт данных KO (БД) Объекты и факторы 2018 ОНС", MSExporter.DoLoadBd2018Unit_Uncomplited);
+			consoleHelper.AddCommand("212", "Импорт данных KO (БД) Объекты и факторы 2018 Сооружения", MSExporter.DoLoadBd2018Unit_Construction);
+			consoleHelper.AddCommand("213", "Импорт данных KO (БД) Объекты и факторы 2018 Здания", MSExporter.DoLoadBd2018Unit_Building);
+			consoleHelper.AddCommand("214", "Импорт данных KO (БД) Объекты и факторы 2018 Помещения", MSExporter.DoLoadBd2018Unit_Flat);
+			consoleHelper.AddCommand("215", "Импорт данных KO (БД) Объекты и факторы 2018 Участки", MSExporter.DoLoadBd2018Unit_Parcel);
 			consoleHelper.AddCommand("216", "Импорт данных KO (XML) 2018", MSExporter.DoLoadXml2018);
-			consoleHelper.AddCommand("217", "Импорт данных KO (БД) Модель 2018 (метки, модель)",
-				MSExporter.DoLoadBd2018ModelAdd);
-			consoleHelper.AddCommand("218", "Импорт данных KO (БД) Эталонные объекты",
-				MSExporter.LoadGroupEtalonParcel_2018);
-			consoleHelper.AddCommand("219", "Импорт данных KO (БД) ВУОН Земля",
-				MSExporter.DoLoadBd2018Unit_Parcel_VUON);
-			consoleHelper.AddCommand("220", "Импорт данных KO (БД) ВУОН Здания",
-				MSExporter.DoLoadBd2018Unit_Build_VUON);
-			consoleHelper.AddCommand("221", "Импорт данных KO (БД) ВУОН Сооружения",
-				MSExporter.DoLoadBd2018Unit_Construction_VUON);
-			consoleHelper.AddCommand("222", "Импорт данных KO (БД) ВУОН Помещения",
-				MSExporter.DoLoadBd2018Unit_Flat_VUON);
-			consoleHelper.AddCommand("223", "Импорт данных KO (БД) ВУОН ОНС",
-				MSExporter.DoLoadBd2018Unit_Uncomplited_VUON);
+			consoleHelper.AddCommand("217", "Импорт данных KO (БД) Модель 2018 (метки, модель)", MSExporter.DoLoadBd2018ModelAdd);
+			consoleHelper.AddCommand("218", "Импорт данных KO (БД) Эталонные объекты", MSExporter.LoadGroupEtalonParcel_2018);
+			consoleHelper.AddCommand("219", "Импорт данных KO (БД) ВУОН Земля", MSExporter.DoLoadBd2018Unit_Parcel_VUON);
+			consoleHelper.AddCommand("220", "Импорт данных KO (БД) ВУОН Здания", MSExporter.DoLoadBd2018Unit_Build_VUON);
+			consoleHelper.AddCommand("221", "Импорт данных KO (БД) ВУОН Сооружения", MSExporter.DoLoadBd2018Unit_Construction_VUON);
+			consoleHelper.AddCommand("222", "Импорт данных KO (БД) ВУОН Помещения", MSExporter.DoLoadBd2018Unit_Flat_VUON);
+			consoleHelper.AddCommand("223", "Импорт данных KO (БД) ВУОН ОНС", MSExporter.DoLoadBd2018Unit_Uncomplited_VUON);
 
 			consoleHelper.AddCommand("250", "Формула 2016", MSExporter.GetFormulaText);
 			consoleHelper.AddCommand("251", "Рассчет", MSExporter.GetCalcGroup);
@@ -157,10 +121,8 @@ namespace KadOzenka.BlFrontEnd
 			consoleHelper.AddCommand("302", "Экспорт данных судебной подсистемы в Xml", SudExporter.ExportXml);
 			consoleHelper.AddCommand("303", "Экспорт данных судебной подсистемы в Excel", SudExporter.ExportExcel);
 			consoleHelper.AddCommand("304", "Статистика сводная в Excel", SudExporter.ExportStatExcel);
-			consoleHelper.AddCommand("305", "Статискика по объектам недвидимости в Excel",
-				SudExporter.ExportStatObjectExcel);
-			consoleHelper.AddCommand("306", "Статистика по положительным судебным решениям в Excel",
-				SudExporter.ExportStatCheckExcel);
+			consoleHelper.AddCommand("305", "Статискика по объектам недвидимости в Excel", SudExporter.ExportStatObjectExcel);
+			consoleHelper.AddCommand("306", "Статистика по положительным судебным решениям в Excel", SudExporter.ExportStatCheckExcel);
 			consoleHelper.AddCommand("350", "Импорт данных решений комиссий (БД)", CommissionExporter.DoLoadBd);
 			consoleHelper.AddCommand("351", "Импорт данных решений комиссий (Excel)", CommissionExporter.DoLoadExcel);
 			consoleHelper.AddCommand("390", "Тест API судебной подсистемы", SudTestApi.TestAll);
@@ -182,8 +144,9 @@ namespace KadOzenka.BlFrontEnd
 				xml.Load("c:\\WORK\\cod2.xml");
 				KadOzenka.Dal.DataImport.DataImporterCod.ImportDataCodFromXml(xml, 2, true);
 			});
-			consoleHelper.AddCommand("501", "Импорт данных деклараций (Excel)",
-				() => { new DataImporterDeclarationsTest().ImportData(); });
+			consoleHelper.AddCommand("501", "Импорт данных деклараций (Excel)", () => { new DataImporterDeclarationsTest().ImportData(); });
 		}
+
 	}
+
 }
