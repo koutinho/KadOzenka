cbccc47982d134274714277da311aca4be26bac7
Митрюшина Наталья Николаевна <INECAS\cp_mitryushina>
2020-02-11 14:13:03 +0000

    CIPJSKO-116 внесение изменений для формы гармонизации по кл. ЦОД
    git-tfs-id: [http://192.168.3.86:8080/tfs/defaultcollection]$/CIPJS/KadOzenka;C55199

--- Source/KadOzenka.Web/Models/GbuObject/HarmonizationCODViewModel.cs
+++ Source/KadOzenka.Web/Models/GbuObject/HarmonizationCODViewModel.cs
@@ -1,96 +1,19 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using ObjectModel.Directory;
 using ObjectModel.Gbu.Harmonization;
 
 namespace KadOzenka.Web.Models.GbuObject
 {
-	public class HarmonizationCODViewModel
+	public class HarmonizationCODViewModel : HarmonizationViewModel
 	{
 		/// <summary>
 		/// Идентификатор задания ЦОД
 		/// </summary>
 		[Display(Name = "Задание ЦОД")]
+		[Required(ErrorMessage = "Выберете Задание ЦОД")]
 		public long? IdCodJob { get; set; }
-
-		/// <summary>
-		/// Идентификатор атрибута, куда будет записан результат 
-		/// </summary>
-		[Display(Name = "Характеристика")]
-		public long? IdAttributeResult { get; set; }
-
-		/// <summary>
-		/// Тип объекта 
-		/// </summary>
-		[Required(ErrorMessage = "Выберете Тип объекта")]
-		public long? PropertyType { get; set; }
-
-		/// <summary>
-		/// Выборка по всем объектам
-		/// </summary>
-		public bool SelectAllObject { get; set; }
-
-		/// <summary>
-		/// Идентификатор аттрибута - фильтра
-		/// </summary>
-		[Display(Name = "Характеристика")]
-		public long? IdAttributeFilter { get; set; }
-
-		/// <summary>
-		/// Список значений фильтра
-		/// </summary>
-		[Display(Name = "Значение")]
-		public List<string> ValuesFilter { get; set; }
-
-		/// <summary>
-		/// Фактор 1 уровня 
-		/// </summary>
-		public long? Level1Attribute { get; set; }
-
-		/// <summary>
-		/// Фактор 2 уровня 
-		/// </summary>
-		public long? Level2Attribute { get; set; }
-
-		/// <summary>
-		/// Фактор 3 уровня 
-		/// </summary>
-		public long? Level3Attribute { get; set; }
-
-		/// <summary>
-		/// Фактор 4 уровня 
-		/// </summary>
-		public long? Level4Attribute { get; set; }
-
-		/// <summary>
-		/// Фактор 5 уровня 
-		/// </summary>
-		public long? Level5Attribute { get; set; }
-
-		/// <summary>
-		/// Фактор 6 уровня 
-		/// </summary>
-		public long? Level6Attribute { get; set; }
-
-		/// <summary>
-		/// Фактор 7 уровня 
-		/// </summary>
-		public long? Level7Attribute { get; set; }
-
-		/// <summary>
-		/// Фактор 8 уровня 
-		/// </summary>
-		public long? Level8Attribute { get; set; }
-
-		/// <summary>
-		/// Фактор 9 уровня 
-		/// </summary>
-		public long? Level9Attribute{get; set; }
-
-		/// <summary>
-		/// Фактор 10 уровня 
-		/// </summary>
-		public long? Level10Attribute { get; set; }
 
 		/// <summary>
 		/// Значение по умолчанию 
@@ -125,7 +48,9 @@ namespace KadOzenka.Web.Models.GbuObject
 				Level9Attribute = Level9Attribute,
 				Level10Attribute = Level10Attribute,
 				DefaultValue = DefaultValue,
-				IdDocument = IdDocument
+				IdDocument = IdDocument,
+				TaskFilter = IsDataActualUsed ? null : TaskFilter,
+				DateActual = IsDataActualUsed ? DataActual : null
 			};
 
 			return settings;
