9cae6d518e3dddb4bbf86cce25d98d6be4a0ffc4
Хомяков Евгений Иванович <INECAS\cipjs_homyakov>
2020-02-12 13:30:11 +0000

    Добавил метод переноса атрибутов из ГБУ в КО
    git-tfs-id: [http://192.168.3.86:8080/tfs/defaultcollection]$/CIPJS/KadOzenka;C55248

---
+++ Source/KadOzenka.Dal/GbuObject/GbuObjectExportAttribute.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Text;
+using System.Data;
+using System.Globalization;
+using System.Data.Common;
+using System.Collections.Generic;
+using Microsoft.Practices.EnterpriseLibrary.Data;
+
+using Core.Register;
+using Core.Numerator;
+using Core.Shared.Misc;
+using Core.Shared.Extensions;
+using Core.Register.RegisterEntities;
+using System.Threading;
+using System.Threading.Tasks;
+using Core.SRD;
+using ObjectModel.Gbu.ExportAttribute;
+
+namespace KadOzenka.Dal.GbuObject
+{
+    /// <summary>
+    /// Перенос атрибутов из ГБУ в КО
+    /// </summary>
+    public class ExportAttributeToKO
+    {
+        /// <summary>
+        /// Выполнение операции переноса атрибутов
+        /// </summary>
+        public static void Run(GbuExportAttributeSettings setting)
+        {
+            CancellationTokenSource cancelTokenSource = new CancellationTokenSource();
+            ParallelOptions options = new ParallelOptions
+            {
+                CancellationToken = cancelTokenSource.Token,
+                MaxDegreeOfParallelism = 20
+            };
+
+
+            if (setting.TaskFilter.Count > 0)
+            {
+                foreach(long taskId in setting.TaskFilter)
+                {
+                    List<ObjectModel.KO.OMUnit> Objs = ObjectModel.KO.OMUnit.Where(x => x.TaskId == taskId).SelectAll().Execute();
+                    //Parallel.ForEach(Objs, options, item => { RunOneUnit(item, setting); });
+                }
+            }
+        }
+    }
+
+}
--- Source/KadOzenka.ObjectModel/OMGBUPartial.cs
+++ Source/KadOzenka.ObjectModel/OMGBUPartial.cs
@@ -532,3 +532,40 @@ namespace ObjectModel.Gbu.CodSelection
         }
     }
 }
+
+namespace ObjectModel.Gbu.ExportAttribute
+{
+    /// <summary>
+    /// Соответствие атрибутов ГБУ и КО
+    /// </summary>
+    public struct ExportAttributeItem
+    {
+        /// <summary>
+        /// Идентификатор фактора КО
+        /// </summary>
+        public long IdAttributeKO;
+        /// <summary>
+        /// Идентификатор фактора ГБУ
+        /// </summary>
+        public long IdAttributeGBU;
+    }
+
+    /// <summary>
+    /// Настройки переноса атрибутов из ГБУшной части в КОшную
+    /// </summary>
+    public struct GbuExportAttributeSettings
+    {
+        /// <summary>
+        /// Список заданий на оценку
+        /// </summary>
+        public List<long> TaskFilter;
+
+        /// <summary>
+        /// Список сопоставленных атрибутов
+        /// </summary>
+        public List<ExportAttributeItem> Attributes;
+
+
+    }
+}
+
