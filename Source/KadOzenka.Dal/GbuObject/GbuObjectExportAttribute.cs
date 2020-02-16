971651dcfff4f92a40ed29e5c2ce991e38710ed6
Хомяков Евгений Иванович <INECAS\cipjs_homyakov>
2020-02-16 18:11:08 +0000

    git-tfs-id: [http://192.168.3.86:8080/tfs/defaultcollection]$/CIPJS/KadOzenka;C55361

--- Source/KadOzenka.Dal/GbuObject/GbuObjectExportAttribute.cs
+++ Source/KadOzenka.Dal/GbuObject/GbuObjectExportAttribute.cs
@@ -15,6 +15,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using Core.SRD;
 using ObjectModel.Gbu.ExportAttribute;
+using ObjectModel.Core.Shared;
 
 namespace KadOzenka.Dal.GbuObject
 {
@@ -35,16 +36,73 @@ namespace KadOzenka.Dal.GbuObject
                 MaxDegreeOfParallelism = 20
             };
 
+            List<long> lstIds = new List<long>();
+            foreach (ExportAttributeItem item in setting.Attributes)
+            {
+                lstIds.Add(item.IdAttributeGBU);
+            }
+
 
             if (setting.TaskFilter.Count > 0)
             {
                 foreach(long taskId in setting.TaskFilter)
                 {
                     List<ObjectModel.KO.OMUnit> Objs = ObjectModel.KO.OMUnit.Where(x => x.TaskId == taskId).SelectAll().Execute();
-                    //Parallel.ForEach(Objs, options, item => { RunOneUnit(item, setting); });
+                    Parallel.ForEach(Objs, options, item => { RunOneUnit(item, setting, lstIds); });
                 }
             }
         }
+
+        public static void RunOneUnit(ObjectModel.KO.OMUnit unit, GbuExportAttributeSettings setting, List<long> lstIds)
+        {
+            List<GbuObjectAttribute> attribs = new GbuObjectService().GetAllAttributes(unit.ObjectId.Value, null, lstIds, unit.CreationDate);
+
+            foreach (GbuObjectAttribute attrib in attribs)
+            {
+                ExportAttributeItem current = setting.Attributes.Find(x => x.IdAttributeGBU == attrib.AttributeId);
+                //if (current != null)
+                {
+                    long id_factor = current.IdAttributeKO;
+                    long RegId = id_factor / 100000;
+                    object value = attrib.StringValue;
+
+
+                    RegisterObject registerObject = new RegisterObject((int)RegId, (int)unit.Id);
+                    var attributeData = RegisterCache.GetAttributeData((int)id_factor);
+                    int referenceItemId = -1;
+                    if (attributeData.CodeField.IsNotEmpty() && attributeData.ReferenceId > 0)
+                    {
+                        OMReferenceItem item = OMReferenceItem.Where(x => x.ReferenceId == attributeData.ReferenceId && x.Value == value.ToString()).ExecuteFirstOrDefault();
+                        if (item != null) referenceItemId = (int)item.ItemId;
+                    }
+
+                    switch (attributeData.Type)
+                    {
+                        case RegisterAttributeType.INTEGER:
+                            value = value.ParseToLongNullable();
+                            break;
+                        case RegisterAttributeType.DECIMAL:
+                            value = value.ParseToDecimalNullable();
+                            break;
+                        case RegisterAttributeType.BOOLEAN:
+                            value = value.ParseToBooleanNullable();
+                            break;
+                        case RegisterAttributeType.STRING:
+                            value = value.ToString();
+                            break;
+                        case RegisterAttributeType.DATE:
+                            value = value.ParseToDateTimeNullable();
+                            break;
+                    }
+                    registerObject.SetAttributeValue((int)id_factor, value, referenceItemId);
+                    RegisterStorage.Save(registerObject);
+                }
+            }
+
+
+
+        }
+
     }
 
 }
