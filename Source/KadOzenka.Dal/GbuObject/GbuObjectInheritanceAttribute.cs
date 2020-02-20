db9e59ce61db47f86edcdbb3916b7dd81e0d69d4
Хомяков Евгений Иванович <INECAS\cipjs_homyakov>
2020-02-20 12:19:21 +0000

    Добавлен метод наследования атрибутов ГБУ по объектам
    git-tfs-id: [http://192.168.3.86:8080/tfs/defaultcollection]$/CIPJS/KadOzenka;C55577

---
+++ Source/KadOzenka.Dal/GbuObject/GbuObjectInheritanceAttribute.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Text;
+using System.Data;
+using System.Globalization;
+using System.Data.Common;
+using System.Collections.Generic;
+using Microsoft.Practices.EnterpriseLibrary.Data;
+
+using Core.Register;
+using Core.Numerator;
+using Core.Shared.Misc;
+using Core.Shared.Extensions;
+using Core.Register.RegisterEntities;
+using System.Threading;
+using System.Threading.Tasks;
+using Core.SRD;
+using ObjectModel.Gbu.InheritanceAttribute;
+using ObjectModel.Core.Shared;
+
+namespace KadOzenka.Dal.GbuObject
+{
+    /// <summary>
+    /// Наследование атрибутов ГБУ по объектам
+    /// </summary>
+    public class GbuObjectInheritanceAttribute
+    {
+        /// <summary>
+        /// Объект для блокировки счетчика в многопоточке
+        /// </summary>
+        private static object locked;
+        /// <summary>
+        /// Общее число объектов
+        /// </summary>
+        public static int MaxCount = 0;
+        /// <summary>
+        /// Индекс текущего объекта
+        /// </summary>
+        public static int CurrentCount = 0;
+        /// <summary>
+        /// Выполнение операции наследования атрибутов
+        /// </summary>
+        public static void Run(GbuInheritanceAttributeSettings setting)
+        {
+            locked = new object();
+            CancellationTokenSource cancelTokenSource = new CancellationTokenSource();
+            ParallelOptions options = new ParallelOptions
+            {
+                CancellationToken = cancelTokenSource.Token,
+                MaxDegreeOfParallelism = 20
+            };
+
+            if (setting.TaskFilter.Count > 0)
+            {
+                List<ObjectModel.KO.OMUnit> Objs = new List<ObjectModel.KO.OMUnit>();
+                foreach (long taskId in setting.TaskFilter)
+                {
+                    Objs.AddRange(ObjectModel.KO.OMUnit.Where(x => x.TaskId == taskId).SelectAll().Execute());
+                }
+                MaxCount = Objs.Count;
+                CurrentCount = 0;
+                Parallel.ForEach(Objs, options, item => { RunOneUnit(item, setting); });
+                CurrentCount = 0;
+                MaxCount = 0;
+            }
+        }
+
+        public static void RunOneUnit(ObjectModel.KO.OMUnit unit, GbuInheritanceAttributeSettings setting)
+        {
+            lock (locked)
+            {
+                CurrentCount++;
+            }
+
+            //TO DO: Со старого комплекса переносить смысла нет, проще написать с нуля!!!
+        }
+
+    }
+
+}
--- Source/KadOzenka.ObjectModel/OMGBUPartial.cs
+++ Source/KadOzenka.ObjectModel/OMGBUPartial.cs
@@ -569,3 +569,65 @@ namespace ObjectModel.Gbu.ExportAttribute
     }
 }
 
+namespace ObjectModel.Gbu.InheritanceAttribute
+{
+    /// <summary>
+    /// Настройки наследования атрибутов
+    /// </summary>
+    public struct GbuInheritanceAttributeSettings
+    {
+        /// <summary>
+        /// Список заданий на оценку
+        /// </summary>
+        public List<long> TaskFilter;
+
+        /// <summary>
+        /// Тип наследования: Кадастровый квартал -> Земельный участок
+        /// </summary>
+        public bool CadastralBlockToParcel;
+        /// <summary>
+        /// Тип наследования: Кадастровый квартал -> Здание
+        /// </summary>
+        public bool CadastralBlockToBuilding;
+        /// <summary>
+        /// Тип наследования: Кадастровый квартал -> Сооружение
+        /// </summary>
+        public bool CadastralBlockToConstruction;
+        /// <summary>
+        /// Тип наследования: Кадастровый квартал -> Объект незавершенного строительства
+        /// </summary>
+        public bool CadastralBlockToUncomplited;
+        /// <summary>
+        /// Тип наследования: Земельный участок -> Здание
+        /// </summary>
+        public bool ParcelToBuilding;
+        /// <summary>
+        /// Тип наследования: Земельный участок -> Сооружение
+        /// </summary>
+        public bool ParcelToConstruction;
+        /// <summary>
+        /// Тип наследования: Земельный участок -> Объект незавершенного строительства
+        /// </summary>
+        public bool ParcelToUncomplited;
+        /// <summary>
+        /// Тип наследования: Здание -> Помещение
+        /// </summary>
+        public bool BuildToFlat;
+
+
+
+        /// <summary>
+        /// Фактор, содержащий родительский кадастровый номер, по которому осуществляется сопоставление с родительским объектом
+        /// </summary>
+        public long ParentCadastralNumberAttribute;
+
+        /// <summary>
+        /// Список выбранных атрибутов
+        /// </summary>
+        public List<long> Attributes;
+
+
+    }
+}
+
+
