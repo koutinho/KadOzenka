0907069d02da349966cc72b046deced7c81efcc2
Короткова Екатерина Владимировна <INECAS\cp_korotkova>
2020-03-13 11:35:44 +0000

    CIPJSKO-150: рефакторинг js-скриптов
    git-tfs-id: [http://192.168.3.86:8080/tfs/defaultcollection]$/CIPJS/KadOzenka;C56485

---
+++ Source/KadOzenka.Web/wwwroot/js/custom-validation.js
@@ -0,0 +1,13 @@
+﻿function distinctErrors(errors) {
+    var result = [];
+    $.each(errors, function (index, event) {
+        var events = $.grep(result, function (e) {
+            return event.Message === e.Message;
+        });
+        if (events.length === 0) {
+            result.push(event);
+        }
+    });
+
+    return result;
+}
