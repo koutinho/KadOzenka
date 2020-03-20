ea49df3bfef5ee0cfeb15d2f8b0d3b510dacd1f1
Митрюшина Наталья Николаевна <INECAS\cp_mitryushina>
2020-03-20 12:40:20 +0000

    добавление основной структуры для отчета корректировки по торгу
    git-tfs-id: [http://192.168.3.86:8080/tfs/defaultcollection]$/CIPJS/KadOzenka;C56861

---
+++ Source/KadOzenka.Dal/FastReports/CorrectionByBargainReport.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Data;
+using System.Linq;
+using Core.UI.Registers.Reports.Model;
+using ObjectModel.Market;
+using Platform.Reports;
+
+namespace KadOzenka.Dal.FastReports
+{
+    public class CorrectionByBargainReport : FastReportBase
+    {
+        protected override string TemplateName(NameValueCollection query)
+        {
+            return nameof(CorrectionByBargainReport);
+        }
+
+        public override string GetTitle(long? objectId)
+        {
+            return ReportType.Title;
+        }
+
+        public override void InitializeFilterValues(long objId, string senderName, bool initialisation, List<FilterValue> filterValues)
+        {
+            if (initialisation)
+            {
+                var cadastralQuarterFilterValue = filterValues.FirstOrDefault(f => f.ParamName == "CadastralQuarter");
+                if (cadastralQuarterFilterValue != null)
+                {
+                    cadastralQuarterFilterValue.ReportParameters = new List<ReportParameter>();
+                    var omQuarters = OMQuartalDictionary.Where(x => true)
+                        .Select(x => x.CadastralQuartal)
+                        .Execute().Select(x => x.CadastralQuartal);
+
+                    cadastralQuarterFilterValue.ReportParameters.Add(new ReportParameter { Value = string.Empty, Key = string.Empty });
+                    cadastralQuarterFilterValue.ReportParameters.AddRange(omQuarters.Select(x => new ReportParameter { Value = x, Key = x }));
+                }
+            }
+        }
+
+        protected override DataSet GetData(NameValueCollection query, HashSet<long> objectList = null)
+        {
+            var dataSet = new DataSet();
+            return dataSet;
+        }
+    }
+}
