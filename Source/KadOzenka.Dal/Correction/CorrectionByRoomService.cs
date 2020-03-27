b2846ee37f844be472242211ac4394c7c35a7168
Короткова Екатерина Владимировна <INECAS\cp_korotkova>
2020-03-27 07:22:44 +0000

    CIPJSKO-206: добавлена служба для рассчета цены с учетом корректировки на комнатность
    git-tfs-id: [http://192.168.3.86:8080/tfs/defaultcollection]$/CIPJS/KadOzenka;C57115

---
+++ Source/KadOzenka.Dal/Correction/CorrectionByRoomService.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ObjectModel.Directory;
+using ObjectModel.Market;
+
+namespace KadOzenka.Dal.Correction
+{
+    public class CorrectionByRoomService
+    {
+        private const int PrecisionForPrice = 2;
+        private const int PrecisionForCoefficients = 4;
+
+        public void UpdateMarketObjectsPrice()
+        {
+            var numberOfRooms = new long?[] {1, 2, 3};
+
+            var groupedObjects = OMCoreObject.Where(x =>
+                    x.BuildingCadastralNumber != null &&
+                    x.RoomsCount != null && numberOfRooms.Contains(x.RoomsCount) &&
+                    x.DealType_Code == DealType.SaleSuggestion || x.DealType_Code == DealType.SaleDeal)
+                .SelectAll(false)
+                //TODO remove
+                .SetPackageSize(10000).SetPackageIndex(0)
+                .Execute()
+                .GroupBy(x => new {x.BuildingCadastralNumber, x.PropertyMarketSegment_Code}).ToList();
+            
+            groupedObjects.ForEach(group =>
+            {
+                var objectsInBuilding = group.ToList();
+
+                if (IsBuildingContainAllRoomsTypes(objectsInBuilding))
+                {
+                    var oneRoomAveragePricePerMeter = GetAveragePricePerMeter(objectsInBuilding, 1);
+                    var twoRoomsAveragePricePerMeter = GetAveragePricePerMeter(objectsInBuilding, 2);
+                    var threeRoomAveragePricePerMeter = GetAveragePricePerMeter(objectsInBuilding, 3);
+
+                    var oneRoomCoefficient = Math.Round(twoRoomsAveragePricePerMeter / oneRoomAveragePricePerMeter, PrecisionForCoefficients);
+                    var twoRoomsCoefficient = Math.Round(twoRoomsAveragePricePerMeter / threeRoomAveragePricePerMeter, PrecisionForCoefficients);
+
+                    SaveHistory(group.Key.BuildingCadastralNumber, oneRoomCoefficient, twoRoomsCoefficient);
+
+                    CalculatePriceAfterCorrectionByRooms(objectsInBuilding, oneRoomCoefficient, twoRoomsCoefficient);
+                }
+            });
+        }
+
+
+        #region Support Methods
+
+        private bool IsBuildingContainAllRoomsTypes(List<OMCoreObject> objectsInBuilding)
+        {
+            bool haveOneRoomApartment = false, haveTwoRoomsApartment = false, haveThreeRoomsApartment = false;
+
+            objectsInBuilding.ForEach(obj =>
+            {
+                switch (obj.RoomsCount)
+                {
+                    case 1:
+                        haveOneRoomApartment = true;
+                        break;
+                    case 2:
+                        haveTwoRoomsApartment = true;
+                        break;
+                    case 3:
+                        haveThreeRoomsApartment = true;
+                        break;
+                }
+            });
+
+            return haveOneRoomApartment && haveTwoRoomsApartment && haveThreeRoomsApartment;
+        }
+
+        private void CalculatePriceAfterCorrectionByRooms(List<OMCoreObject> objects, decimal oneRoomCoefficient, decimal twoRoomsCoefficient)
+        {
+            objects.ForEach(x =>
+            {
+                if (x.RoomsCount == 1 || x.RoomsCount == 3)
+                {
+                    var coefficient = 0m;
+                    switch (x.RoomsCount)
+                    {
+                        case 1:
+                            coefficient = oneRoomCoefficient;
+                            break;
+                        case 3:
+                            coefficient = twoRoomsCoefficient;
+                            break;
+                    }
+
+                    x.PriceAfterCorrectionByRooms = Math.Round(x.Price.GetValueOrDefault() * coefficient, PrecisionForPrice);
+                    x.Save();
+                }
+            });
+        }
+
+        private decimal GetAveragePricePerMeter(IEnumerable<OMCoreObject> objects, int numberOfRooms)
+        {
+            return objects.Where(x => x.RoomsCount == numberOfRooms).Select(x => x.PricePerMeter.GetValueOrDefault()).Average();
+        }
+
+        private void SaveHistory(string buildingCadastralNumber, decimal oneRoomCoefficient, decimal twoRoomsCoefficient)
+        {
+            new OMPriceCorrectionByRoomsHistory
+            {
+                BuildingCadastralNumber = buildingCadastralNumber,
+                ChangingDate = new DateTime(DateTime.Today.Year, DateTime.Today.Month, 1),
+                OneRoomCoefficient = oneRoomCoefficient,
+                TwoRoomsCoefficient = twoRoomsCoefficient
+            }.Save();
+        }
+
+        #endregion
+    }
+}
---
+++ Source/KadOzenka.Dal/LongProcess/CorrectionByRoomForMarketObjectsLongProcess.cs
@@ -0,0 +1,24 @@
+﻿using Core.Register.LongProcessManagment;
+using ObjectModel.Core.LongProcess;
+using System.Threading;
+using KadOzenka.Dal.Correction;
+using ObjectModel.Market;
+
+namespace KadOzenka.Dal.LongProcess
+{
+    public class CorrectionByRoomForMarketObjectsLongProcess : LongProcess
+    {
+        public const string LongProcessName = nameof(CorrectionByRoomForMarketObjectsLongProcess);
+
+        public static void AddProcessToQueue()
+        {
+            LongProcessManager.AddTaskToQueue(LongProcessName, registerId: OMCoreObject.GetRegisterId());
+        }
+
+        public override void StartProcess(OMProcessType processType, OMQueue processQueue, CancellationToken cancellationToken)
+        {
+            var correctionByRoomService = new CorrectionByRoomService();
+            correctionByRoomService.UpdateMarketObjectsPrice();
+        }
+    }
+}
