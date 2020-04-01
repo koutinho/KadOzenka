7e5b0c24918b4fafa5621ba8d37315e6e3df5d03
Божко Александр Сергеевич(CorePartners) <INECAS\cp_bozhko>
2020-04-01 18:23:15 +0000

    CIPJSKO-212 форма плюс карта
    git-tfs-id: [http://192.168.3.86:8080/tfs/defaultcollection]$/CIPJS/KadOzenka;C57296

---
+++ Source/KadOzenka.Web/Controllers/ExpressScoreController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KadOzenka.Web.Controllers
+{
+	public class ExpressScoreController : Controller
+	{
+		public ActionResult Index()
+		{
+			return View();
+		}
+	}
+}
