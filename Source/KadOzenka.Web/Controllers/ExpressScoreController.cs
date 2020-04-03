2876491472b8d2ae3ca600aa0c7d9dab927c7f93
Божко Александр Сергеевич(CorePartners) <INECAS\cp_bozhko>
2020-04-03 12:22:55 +0000

    CIPJSKO-212 подгрузка кад номера и выбор адреса по кад номеру
    git-tfs-id: [http://192.168.3.86:8080/tfs/defaultcollection]$/CIPJS/KadOzenka;C57373

--- Source/KadOzenka.Web/Controllers/ExpressScoreController.cs
+++ Source/KadOzenka.Web/Controllers/ExpressScoreController.cs
@@ -1,12 +1,35 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ObjectModel.Market;
 
 namespace KadOzenka.Web.Controllers
 {
-	public class ExpressScoreController : Controller
+	public class ExpressScoreController : KoBaseController
 	{
 		public ActionResult Index()
 		{
 			return View();
+		}
+
+		public JsonResult GetKadNumber(string address)
+		{
+			var yandexAddress = OMYandexAddress.Where(x => x.FormalizedAddress.Contains(address)).Select(x => x.CadastralNumber).ExecuteFirstOrDefault();
+
+			if (yandexAddress == null)
+			{
+				return SendErrorMessage("Кадастровый номер для объекта не найден");
+			}
+			return Json(new { response = new {kadNumber = yandexAddress.CadastralNumber} });
+		}
+
+		public JsonResult GetAddressByKadNumber(string kadNumber)
+		{
+			var yandexAddress = OMYandexAddress.Where(x => x.CadastralNumber == kadNumber).Select(x => x.FormalizedAddress).ExecuteFirstOrDefault();
+
+			if (yandexAddress == null)
+			{
+				return SendErrorMessage("Адрес для объекта не найден");
+			}
+			return Json(new { response = new { address = yandexAddress.FormalizedAddress } });
 		}
 	}
 }
