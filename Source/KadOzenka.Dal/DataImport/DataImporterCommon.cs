150303bf7b8991e1583298367d9b62da988af084
simbirsoft_lidiya <INECAS\simbirsoft_lidiya>
2020-04-08 18:45:57 +0000

    CIPJSKO-216 добавлена загрузка данных по списку в таблицы allpri
    git-tfs-id: [http://192.168.3.86:8080/tfs/defaultcollection]$/CIPJS/KadOzenka;C57582

--- Source/KadOzenka.Dal/DataImport/DataImporterCommon.cs
+++ Source/KadOzenka.Dal/DataImport/DataImporterCommon.cs
@@ -19,6 +19,8 @@ using Core.SRD;
 using Core.ErrorManagment;
 using ObjectModel.Core.Shared;
 using Core.Messages;
+using ObjectModel.Core.Register;
+using ObjectModel.Gbu;
 
 namespace KadOzenka.Dal.DataImport
 {
@@ -114,75 +116,98 @@ namespace KadOzenka.Dal.DataImport
 			mainWorkSheet.Rows[0].Cells[maxColumns].SetValue($"Результат сохранения");
 			List<string> columnNames = new List<string>();
 			for (int i = 0; i < maxColumns; i++) columnNames.Add(mainWorkSheet.Rows[0].Cells[i].Value.ToString());
+
+			var register = OMRegister.Where(x => x.RegisterId == mainRegisterId).Select(x => x.AllpriTable).ExecuteFirstOrDefault();
+			bool isAllpri = !string.IsNullOrEmpty(register.AllpriTable);
+
+			if (isAllpri)
+				ProcessAllpri(columns, mainWorkSheet, options, maxColumns, columnNames);
+			else
+				ProcessNotAllpri(columns, mainWorkSheet, options, maxColumns, columnNames, mainRegisterId);
+					   			 		  
+			MemoryStream stream = new MemoryStream();
+			excelFile.Save(stream, SaveOptions.XlsxDefault);
+			stream.Seek(0, SeekOrigin.Begin);
+			return stream;
+		}
+
+		private static void ProcessAllpri(List<DataExportColumn> columns, ExcelWorksheet mainWorkSheet, 
+			ParallelOptions options, int maxColumns, List<string> columnNames)
+		{
 			Parallel.ForEach(mainWorkSheet.Rows, options, row =>
 			{
 				try
 				{
 					if (row.Index != 0) //все, кроме заголовков
 					{
-						// Найти ИД объекта по ключевым полям					
-						List<QSCondition> conditions = new List<QSCondition>();
-                        long objectId = -1;
-                        if(columns.Any(x => x.IsKey))
-                        {
-                            foreach (var keyColumn in columns.Where(x => x.IsKey))
-                            {
-                                int index = columnNames.IndexOf(keyColumn.ColumnName);
-                                object rowValue = row.Cells[index].Value.ToString();
-                                QSCondition con1 = new QSConditionSimple
-                                {
-                                    ConditionType = QSConditionType.Equal,
-                                    LeftOperand = new QSColumnSimple((int)keyColumn.AttributrId),
-                                    RightOperand = new QSColumnConstant(rowValue)
-                                };
-                                conditions.Add(con1);
-                            }
-                            QSQuery query = new QSQuery
-                            {
-                                MainRegisterID = mainRegisterId,
-                                Condition = new QSConditionGroup
-                                {
-                                    Type = QSConditionGroupType.And,
-                                    Conditions = conditions
-                                }
-                            };
-                            DataTable dt = query.ExecuteQuery();
-                            if (dt.Rows.Count >= 1) objectId = dt.Rows[0]["ID"].ParseToLong();
-                        }
-                        RegisterObject registerObject = new RegisterObject((int)mainRegisterId, (int)objectId);
-                        foreach (var column in columns.Where(y => !y.IsKey))
-					    {
-                            int cell = columnNames.IndexOf(column.ColumnName);
-                            object value = mainWorkSheet.Rows[row.Index].Cells[cell].Value;
-						    var attributeData = RegisterCache.GetAttributeData((int)column.AttributrId);
-						    int referenceItemId = -1;
-                            if(attributeData.CodeField.IsNotEmpty() && attributeData.ReferenceId > 0)
-                            {
-                                OMReferenceItem item = OMReferenceItem.Where(x => x.ReferenceId == attributeData.ReferenceId && x.Value == (string)value).ExecuteFirstOrDefault();
-                                if (item != null) referenceItemId = (int)item.ItemId;
-                            }
-                            switch(attributeData.Type)
-                            {
-                                case RegisterAttributeType.INTEGER:
-								    value = value.ParseToLongNullable();
-								    break;
-							    case RegisterAttributeType.DECIMAL:
-								    value = value.ParseToDecimalNullable();
-								    break;
-							    case RegisterAttributeType.BOOLEAN:
-								    value = value.ParseToBooleanNullable();
-								    break;
-							    case RegisterAttributeType.STRING:
-									value = value == null ? "" : value.ToString();
-								    break;
-							    case RegisterAttributeType.DATE:
-									value = value.ParseToDateTimeNullable();
-									break;
-						    }
-						    registerObject.SetAttributeValue((int)column.AttributrId, value, referenceItemId);
-					    }
-					    RegisterStorage.Save(registerObject);
-					    mainWorkSheet.Rows[row.Index].Cells[maxColumns].SetValue("Успешно");
+						long objectId = -1;
+
+						//ключ - кадастровый номер, колонка №2						
+						string cadastralNumber = row.Cells[1].Value.ToString();
+						OMMainObject mainObject = OMMainObject.Where(x => x.CadastralNumber == cadastralNumber)
+							.Select(x => x.Id).ExecuteFirstOrDefault();
+
+						if (mainObject == null)
+						{
+							mainObject = new OMMainObject
+							{
+								CadastralNumber = cadastralNumber,
+								ObjectType_Code = ObjectModel.Directory.PropertyTypes.Building
+							};
+							mainObject.Save();
+						}
+						objectId = mainObject.Id;
+
+						var loadColumns = columns.Where(y => !y.IsKey).ToList();
+						List<long> loadColumnIds = columns.Where(y => !y.IsKey).Select(x => x.AttributrId).ToList();
+
+						var registers = OMAttribute.Where(x => loadColumnIds.Contains(x.Id))
+							.Select(x => x.RegisterId)
+							.Execute();
+
+						var registerGroups = registers.GroupBy(x => x.RegisterId, y => y.Id).ToList();
+
+						foreach (var group in registerGroups)
+						{
+							long registerId = group.Key;
+							List<long> attributeIds = group.Select(x => x).ToList();
+
+							RegisterObject registerObject = new RegisterObject((int)registerId, (int)objectId);
+
+							foreach (long attribute in attributeIds)
+							{
+								DataExportColumn column = columns.Where(x => x.AttributrId == attribute).First();
+
+								int cell = columnNames.IndexOf(column.ColumnName);
+								object value = mainWorkSheet.Rows[row.Index].Cells[cell].Value;
+
+								var attributeData = RegisterCache.GetAttributeData((int)column.AttributrId);
+
+								switch (attributeData.Type)
+								{
+									case RegisterAttributeType.INTEGER:
+										value = value.ParseToLongNullable();
+										break;
+									case RegisterAttributeType.DECIMAL:
+										value = value.ParseToDecimalNullable();
+										break;
+									case RegisterAttributeType.BOOLEAN:
+										value = value.ParseToBooleanNullable();
+										break;
+									case RegisterAttributeType.STRING:
+										value = value == null ? "" : value.ToString();
+										break;
+									case RegisterAttributeType.DATE:
+										value = value.ParseToDateTimeNullable();
+										break;
+								}
+
+								registerObject.SetAttributeValue((int)column.AttributrId, value);
+							}
+
+							RegisterStorage.Save(registerObject);
+							mainWorkSheet.Rows[row.Index].Cells[maxColumns].SetValue("Успешно");
+						}
 					}
 				}
 				catch (Exception ex)
@@ -191,10 +216,88 @@ namespace KadOzenka.Dal.DataImport
 					mainWorkSheet.Rows[row.Index].Cells[maxColumns].SetValue($"{ex.Message} (подробно в журнале №{errorId})");
 				}
 			});
-			MemoryStream stream = new MemoryStream();
-			excelFile.Save(stream, SaveOptions.XlsxDefault);
-			stream.Seek(0, SeekOrigin.Begin);
-			return stream;
 		}
-    }
+
+		private static void ProcessNotAllpri(List<DataExportColumn> columns, ExcelWorksheet mainWorkSheet, 
+			ParallelOptions options, int maxColumns, List<string> columnNames, int mainRegisterId)
+		{
+			Parallel.ForEach(mainWorkSheet.Rows, options, row =>
+			{
+				try
+				{
+					if (row.Index != 0) //все, кроме заголовков
+					{
+						// Найти ИД объекта по ключевым полям					
+						List<QSCondition> conditions = new List<QSCondition>();
+						long objectId = -1;
+						if (columns.Any(x => x.IsKey))
+						{
+							foreach (var keyColumn in columns.Where(x => x.IsKey))
+							{
+								int index = columnNames.IndexOf(keyColumn.ColumnName);
+								object rowValue = row.Cells[index].Value.ToString();
+								QSCondition con1 = new QSConditionSimple
+								{
+									ConditionType = QSConditionType.Equal,
+									LeftOperand = new QSColumnSimple((int)keyColumn.AttributrId),
+									RightOperand = new QSColumnConstant(rowValue)
+								};
+								conditions.Add(con1);
+							}
+							QSQuery query = new QSQuery
+							{
+								MainRegisterID = mainRegisterId,
+								Condition = new QSConditionGroup
+								{
+									Type = QSConditionGroupType.And,
+									Conditions = conditions
+								}
+							};
+							DataTable dt = query.ExecuteQuery();
+							if (dt.Rows.Count >= 1) objectId = dt.Rows[0]["ID"].ParseToLong();
+						}
+						RegisterObject registerObject = new RegisterObject((int)mainRegisterId, (int)objectId); 
+						foreach (var column in columns.Where(y => !y.IsKey))
+						{
+							int cell = columnNames.IndexOf(column.ColumnName);
+							object value = mainWorkSheet.Rows[row.Index].Cells[cell].Value;
+							var attributeData = RegisterCache.GetAttributeData((int)column.AttributrId);
+							int referenceItemId = -1;
+							if (attributeData.CodeField.IsNotEmpty() && attributeData.ReferenceId > 0)
+							{
+								OMReferenceItem item = OMReferenceItem.Where(x => x.ReferenceId == attributeData.ReferenceId && x.Value == (string)value).ExecuteFirstOrDefault();
+								if (item != null) referenceItemId = (int)item.ItemId;
+							}
+							switch (attributeData.Type)
+							{
+								case RegisterAttributeType.INTEGER:
+									value = value.ParseToLongNullable();
+									break;
+								case RegisterAttributeType.DECIMAL:
+									value = value.ParseToDecimalNullable();
+									break;
+								case RegisterAttributeType.BOOLEAN:
+									value = value.ParseToBooleanNullable();
+									break;
+								case RegisterAttributeType.STRING:
+									value = value == null ? "" : value.ToString();
+									break;
+								case RegisterAttributeType.DATE:
+									value = value.ParseToDateTimeNullable();
+									break;
+							}
+							registerObject.SetAttributeValue((int)column.AttributrId, value, referenceItemId);
+						}
+						RegisterStorage.Save(registerObject);
+						mainWorkSheet.Rows[row.Index].Cells[maxColumns].SetValue("Успешно");
+					}
+				}
+				catch (Exception ex)
+				{
+					long errorId = ErrorManager.LogError(ex);
+					mainWorkSheet.Rows[row.Index].Cells[maxColumns].SetValue($"{ex.Message} (подробно в журнале №{errorId})");
+				}
+			});
+		}		
+	}
 }
