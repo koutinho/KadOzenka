0ca1a26cceee90a56ce1177fccaa62c7610f2eaf
Силанов Ярослав Олегович <INECAS\silanov>
2020-04-23 00:07:46 +0000

    -Удаление из сборки проекта GISStandProject; -Добавление в сборку проекта StartProject;
    git-tfs-id: [http://192.168.3.86:8080/tfs/defaultcollection]$/CIPJS/KadOzenka;C58174

---
+++ Source/StartProject/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Configuration;
+
+using KadOzenka.Dal.AddressChecker;
+using KadOzenka.Dal.KadNumberChecker;
+using KadOzenka.Dal.RestAppParser;
+
+namespace StartProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //string[] regionIDs = ConfigurationManager.AppSettings["restAppRegionIDsCIAN"].Split(',');
+            //string[] dealTypes = ConfigurationManager.AppSettings["restAppDealTypeCIAN"].Split(',');
+            //int delta = int.Parse(ConfigurationManager.AppSettings["restAppMinuteLimits"]);
+            //string login = ConfigurationManager.AppSettings["restAppLogin001"];
+            //string token = ConfigurationManager.AppSettings["restAppToken001"];
+            //string metaInfoLink = ConfigurationManager.AppSettings["restAppMetaLink"];
+            //string restAppCIANLink = ConfigurationManager.AppSettings["restAppCIANLink"];
+            //string restAppAvitoLink = ConfigurationManager.AppSettings["restAppAvitoLink"];
+            //string restAppTimeTemplate = ConfigurationManager.AppSettings["restAppTimeTmp"];
+
+
+            Console.WriteLine("===Старт парсинга ЦИАНа===");
+            Console.WriteLine("1. Получение данных с RestApp");
+            new Data().Detect();
+            Console.WriteLine("2. Присвоение формализованных адресов");
+            new Addresses().Detect();
+            Console.WriteLine("3. Присвоение кадастровых номеров по формализованным адресам");
+            new KadNumbers().Detect();
+            Console.WriteLine("4. Парсинг объявлений со снятием скриншотов");
+
+
+        }
+    }
+}
