be708e0f4fd2164fe848f3868395c65fbf42a9c3
Короткова Екатерина Владимировна <INECAS\cp_korotkova>
2020-04-29 12:58:14 +0000

    моделирование: рефакторинг
    git-tfs-id: [http://192.168.3.86:8080/tfs/defaultcollection]$/CIPJS/KadOzenka;C58463

---
+++ Source/KadOzenka.Web/wwwroot/js/common-modals.js
@@ -0,0 +1,12 @@
+﻿function ShowModal(modal, width, height) {
+	if (!modal.data("kendoWindow")) {
+		modal.kendoWindow({
+			visible: false,
+			resizable: true,
+			modal: true,
+			width: width,
+			height: height
+		}).data("kendoWindow").center();
+	}
+	modal.data("kendoWindow").open();
+}
