bd621ae4b941f65b52395ed21955ea9c5c77bd6a
Терешечкин Михаил Юрьевич <tereshechkin@genix.pro>
2020-05-12 16:46:14 +0000

    Оптимизация получения данных по пользователю
    git-tfs-id: [http://192.168.3.86:8080/tfs/defaultcollection]$/CIPJS/KadOzenka;C58849

--- Source/KadOzenka.Dal/DataImport/DataImporterService.cs
+++ Source/KadOzenka.Dal/DataImport/DataImporterService.cs
@@ -9,19 +9,19 @@ namespace KadOzenka.Dal.DataImport
     {
         public List<ImportDataLogDto> GetCommonDataLog(long registerId, long objectId)
         {
-            var importDataLogs = OMImportDataLog.Where(x => x.RegisterId == registerId && x.ObjectId == objectId).SelectAll()
+            var importDataLogs = OMImportDataLog.Where(x => x.RegisterId == registerId && x.ObjectId == objectId)
+                .SelectAll()
+                .Select(x => x.ParentUser.FullName)
                 .Execute();
 
             var result = new List<ImportDataLogDto>();
             importDataLogs.ForEach(x =>
             {
-                var user = OMUser.Where(u => u.Id == x.UserId).SelectAll().ExecuteFirstOrDefault();
-
                 result.Add(new ImportDataLogDto
                 {
                     Id = x.Id,
                     CreationDate = x.DateCreated,
-                    Author = user?.FullName,
+                    Author = x.ParentUser?.FullName,
                     FileName = x.DataFileName,
                     NumberOfImportedObjects = x.NumberOfImportedObjects,
                     TotalNumberOfObjects = x.TotalNumberOfObjects
