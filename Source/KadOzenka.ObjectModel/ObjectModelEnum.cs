d496e7c40cf77d80d0825a8ce5191edbdccc1a95
Короткова Екатерина Владимировна <INECAS\cp_korotkova>
2020-06-01 06:50:16 +0000

    CIPJSKO-305: отчет 14
    git-tfs-id: [http://192.168.3.86:8080/tfs/defaultcollection]$/CIPJS/KadOzenka;C59778

--- Source/KadOzenka.ObjectModel/ObjectModelEnum.cs
+++ Source/KadOzenka.ObjectModel/ObjectModelEnum.cs
@@ -3654,87 +3654,87 @@ namespace ObjectModel.Directory
         [ShortTitle("")]
         None = 0,
 
-        /// <summary>
-        /// ЦАО (903)
-        /// </summary>
-        [Description("ЦАО")]
+		/// <summary>
+		/// ЦАО (903)
+		/// </summary>
+		[Description("ЦАО")]
         [EnumCode("1")]
         [ShortTitle("Центральный")]
         CAO = 903,
-        /// <summary>
-        /// САО (904)
-        /// </summary>
-        [Description("САО")]
+		/// <summary>
+		/// САО (904)
+		/// </summary>
+		[Description("САО")]
         [EnumCode("2")]
         [ShortTitle("Северный")]
         SAO = 904,
-        /// <summary>
-        /// СВАО (905)
-        /// </summary>
-        [Description("СВАО")]
+		/// <summary>
+		/// СВАО (905)
+		/// </summary>
+		[Description("СВАО")]
         [EnumCode("3")]
         [ShortTitle("Северо-Восточный")]
         SVAO = 905,
-        /// <summary>
-        /// ВАО (906)
-        /// </summary>
-        [Description("ВАО")]
+		/// <summary>
+		/// ВАО (906)
+		/// </summary>
+		[Description("ВАО")]
         [EnumCode("4")]
         [ShortTitle("Восточный")]
         VAO = 906,
-        /// <summary>
-        /// ЮВАО (907)
-        /// </summary>
-        [Description("ЮВАО")]
+		/// <summary>
+		/// ЮВАО (907)
+		/// </summary>
+		[Description("ЮВАО")]
         [EnumCode("5")]
         [ShortTitle("Юго-Восточный")]
         YVAO = 907,
-        /// <summary>
-        /// ЮАО (908)
-        /// </summary>
-        [Description("ЮАО")]
+		/// <summary>
+		/// ЮАО (908)
+		/// </summary>
+		[Description("ЮАО")]
         [EnumCode("6")]
         [ShortTitle("Южный")]
         YAO = 908,
-        /// <summary>
-        /// ЮЗАО (909)
-        /// </summary>
-        [Description("ЮЗАО")]
+		/// <summary>
+		/// ЮЗАО (909)
+		/// </summary>
+		[Description("ЮЗАО")]
         [EnumCode("7")]
         [ShortTitle("Юго-Западный")]
         YZAO = 909,
-        /// <summary>
-        /// ЗАО (910)
-        /// </summary>
-        [Description("ЗАО")]
+		/// <summary>
+		/// ЗАО (910)
+		/// </summary>
+		[Description("ЗАО")]
         [EnumCode("8")]
         [ShortTitle("Западный")]
         ZAO = 910,
-        /// <summary>
-        /// СЗАО (911)
-        /// </summary>
-        [Description("СЗАО")]
+		/// <summary>
+		/// СЗАО (911)
+		/// </summary>
+		[Description("СЗАО")]
         [EnumCode("9")]
         [ShortTitle("Северо-Западный")]
         SZAO = 911,
-        /// <summary>
-        /// ЗелАО (912)
-        /// </summary>
-        [Description("ЗелАО")]
+		/// <summary>
+		/// ЗелАО (912)
+		/// </summary>
+		[Description("ЗелАО")]
         [EnumCode("10")]
         [ShortTitle("Зеленоградский")]
         ZelAO = 912,
-        /// <summary>
-        /// НАО (913)
-        /// </summary>
-        [Description("НАО")]
+		/// <summary>
+		/// НАО (913)
+		/// </summary>
+		[Description("НАО")]
         [EnumCode("11")]
         [ShortTitle("НАО")]
         NAO = 913,
-        /// <summary>
-        /// ТАО (914)
-        /// </summary>
-        [Description("ТАО")]
+		/// <summary>
+		/// ТАО (914)
+		/// </summary>
+		[Description("ТАО")]
         [EnumCode("12")]
         [ShortTitle("ТАО")]
         TAO = 914,
@@ -5530,6 +5530,13 @@ namespace ObjectModel.Directory
         [EnumCode("1067")]
         [ShortTitle("")]
         PastResultsIndexing = 1067,
+		/// <summary>
+		/// Индивидуальный расчет (1511)
+		/// </summary>
+		[Description("Индивидуальный расчет")]
+        [EnumCode("1511")]
+        [ShortTitle("")]
+        IndividualCalculation = 1511,
     }
 }
 
@@ -5705,6 +5712,39 @@ namespace ObjectModel.Directory
         [EnumCode("4")]
         [ShortTitle("")]
         No = 1510,
+    }
+}
+
+namespace ObjectModel.Directory
+{
+    /// <summary>
+    /// Способ моделирования (218)
+    ///</summary>
+    [ReferenceInfo(ReferenceId = 218)]
+    public enum KoModelingWay : long
+    {
+        /// <summary>
+        /// Значение отсутствует
+        /// </summary>
+        [Description("Значение отсутствует")]
+        [EnumCode("0")]
+        [ShortTitle("")]
+        None = 0,
+
+		/// <summary>
+		/// Индивидуальная оценка (1512)
+		/// </summary>
+		[Description("Индивидуальная оценка")]
+        [EnumCode("1512")]
+        [ShortTitle("")]
+        IndividualCalculation = 1512,
+		/// <summary>
+		/// Массовая оценка (1513)
+		/// </summary>
+		[Description("Массовая оценка")]
+        [EnumCode("1513")]
+        [ShortTitle("")]
+        MassCalculation = 1513,
     }
 }
 
