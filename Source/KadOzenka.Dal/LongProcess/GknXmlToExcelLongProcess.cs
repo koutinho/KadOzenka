ea20628944b2a823b3950fba0429d1bd6a87eade
Коротков Александр <INECAS\simbirsoft_akorotkov>
2020-06-05 07:40:05 +0000

    CIPJSKO-243 Выгрузка данных задания на оценку
    git-tfs-id: [http://192.168.3.86:8080/tfs/defaultcollection]$/CIPJS/KadOzenka;C59987

---
+++ Source/KadOzenka.Dal/LongProcess/GknXmlToExcelLongProcess.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Threading;
+using Core.Main.FileStorages;
+using Core.Messages;
+using Core.Register.LongProcessManagment;
+using Core.Shared.Extensions;
+using GemBox.Spreadsheet;
+using KadOzenka.Dal.DataImport;
+using KadOzenka.Dal.XmlParser;
+using ObjectModel.Common;
+using ObjectModel.Core.LongProcess;
+using ObjectModel.KO;
+
+namespace KadOzenka.Dal.LongProcess
+{
+    public abstract class GknXmlToExcelLongProcess : LongProcess
+    {
+        public const string LongProcessName = nameof(GknXmlToExcelLongProcess);
+
+        protected GknXmlToExcelLongProcess()
+        {
+            NotificationSender = new NotificationSender();
+        }
+
+        public static void AddProcessToQueue(GknXmlConvertParams convertParams)
+        {
+            LongProcessManager.AddTaskToQueue(LongProcessName, parameters: convertParams.SerializeToXml());
+        }
+
+        public override void StartProcess(OMProcessType processType, OMQueue processQueue,
+            CancellationToken cancellationToken)
+        {
+            WorkerCommon.SetProgress(processQueue, 0);
+            var convertParams = processQueue.Parameters.DeserializeFromXml<GknXmlConvertParams>();
+
+            var id = convertParams.KOTaskId;
+            var config = convertParams.config;
+            var userId = convertParams.UserId;
+
+            var attachments = OMImportDataLog.Where(x => x.RegisterId == OMTask.GetRegisterId() && x.ObjectId == id)
+                .SelectAll().Execute();
+            var omImportDataLogs = attachments.Where(a => a.DataFileName.EndsWith(".xml")).ToList();
+            xmlImportGkn.FillDictionary(FileStorageManager.GetPathForStorage("SchemaPath"));
+
+            var allObjects = new xmlObjectList();
+            foreach (var log in omImportDataLogs)
+            {
+                xmlObjectList doc;
+                try
+                {
+                    var fileName = DataImporterCommon.GetTemplateName(log.Id);
+                    Console.WriteLine($"Log name: {fileName}");
+                    var fs = FileStorageManager.GetFileStream(DataImporterCommon.FileStorageName, log.DateCreated,
+                        fileName);
+                    doc = xmlImportGkn.GetXmlObject(fs);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    continue;
+                }
+
+                allObjects.Buildings.AddRange(doc.Buildings);
+                allObjects.Flats.AddRange(doc.Flats);
+                allObjects.CarPlaces.AddRange(doc.CarPlaces);
+                allObjects.Constructions.AddRange(doc.Constructions);
+                allObjects.Uncompliteds.AddRange(doc.Uncompliteds);
+                allObjects.Parcels.AddRange(doc.Parcels);
+            }
+
+            WorkerCommon.SetProgress(processQueue, 50);
+
+            var exporter = new XmlToExcelExport(allObjects, config);
+
+            var result = exporter.GetResult();
+
+            var memoryStream = new MemoryStream();
+
+            using (var archive = new ZipArchive(memoryStream, ZipArchiveMode.Create, true))
+            {
+                foreach (var (name, file) in result)
+                {
+                    var archiveFile = archive.CreateEntry(name + ".xlsx");
+
+                    using (var entryStream = archiveFile.Open())
+                    {
+                        file.Save(entryStream, SaveOptions.XlsxDefault);
+                    }
+                }
+            }
+
+            memoryStream.Seek(0, SeekOrigin.Begin);
+
+
+            var fsName = "DataExporterByTemplate";
+            var dt = DateTime.Today;
+            FileStorageManager.Save(memoryStream, fsName, dt, $"{id}_ExcelConversion.zip");
+
+            if (userId != null)
+                new MessageService().SendMessages(new MessageDto
+                {
+                    Addressers = new MessageAddressersDto {UserIds = new long[] {userId.Value}},
+                    Subject = $"Конвертация xml в xlsx по задаче с идентификатором {id}",
+                    Message =
+                        $@"Процесс конвертации завершен. <a href=""/GknXmlToExcel/DownloadResult?dt={dt}&id={id}"">Скачать результаты</a>",
+                    IsUrgent = true,
+                    IsEmail = false
+                });
+            WorkerCommon.SetProgress(processQueue, 100);
+        }
+
+        public class GknXmlConvertParams
+        {
+            public int[] config;
+            public long KOTaskId;
+            public int? UserId;
+        }
+    }
+}
---
+++ Source/KadOzenka.Web/Controllers/GknXmlToExcelController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Core.Main.FileStorages;
+using Core.SRD;
+using KadOzenka.Dal.LongProcess;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KadOzenka.Web.Controllers
+{
+    public class GknXmlToExcelController : Controller
+    {
+        private readonly string FileStorage = "DataExporterByTemplate";
+
+        [HttpGet]
+        public IActionResult Configurator()
+        {
+            return View("/Views/DataExport/GknXmlToExcel.cshtml");
+        }
+
+        public FileResult DownloadResult(DateTime dt, int id)
+        {
+            var st = FileStorageManager.GetFileStream(FileStorage, dt, $"{id}_ExcelConversion.zip");
+            return File(st, "application/zip",
+                $"{id}_{dt:ddMMyyyy}_ExcelConversion.zip");
+        }
+
+        public IActionResult Convert(long id, int[] config)
+        {
+            try
+            {
+                GknXmlToExcelLongProcess.AddProcessToQueue(
+                    new GknXmlToExcelLongProcess.GknXmlConvertParams
+                    {
+                        KOTaskId = id,
+                        config = config,
+                        UserId = SRDSession.GetCurrentUserId()
+                    });
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, "Возникла ошибка при постановке задачи в очередь");
+            }
+        }
+    }
+}
