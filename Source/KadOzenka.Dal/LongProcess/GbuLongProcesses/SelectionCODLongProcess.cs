c78b19d531da52d34c3fd71579f0df50a9ae48cd
Божко Александр Сергеевич(CorePartners) <INECAS\cp_bozhko>
2020-06-08 12:59:29 +0000

    CIPJSKO-317 долгий процесс для справочников цод
    git-tfs-id: [http://192.168.3.86:8080/tfs/defaultcollection]$/CIPJS/KadOzenka;C60097

---
+++ Source/KadOzenka.Dal/LongProcess/GbuLongProcesses/SelectionCODLongProcess.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Core.Register.LongProcessManagment;
+using Core.Shared.Extensions;
+using KadOzenka.Dal.GbuObject;
+using ObjectModel.Core.LongProcess;
+using ObjectModel.Gbu.CodSelection;
+using EntryPointNotFoundException = System.EntryPointNotFoundException;
+
+namespace KadOzenka.Dal.LongProcess.GbuLongProcesses
+{
+	public class SelectionCODLongProcess : LongProcess
+	{
+		public const string LongProcessName = "HarmonizationCodProcess";
+
+		public static long AddProcessToQueue(CodSelectionSettings settings)
+		{
+			return LongProcessManager.AddTaskToQueue(LongProcessName, null, null, settings.SerializeToXml());
+		}
+
+		public override void StartProcess(OMProcessType processType, OMQueue processQueue,
+			CancellationToken cancellationToken)
+		{
+			var cancelSource = new CancellationTokenSource();
+			var cancelToken = cancelSource.Token;
+			try
+			{
+				WorkerCommon.SetProgress(processQueue, 0);
+
+				var settings = processQueue.Parameters.DeserializeFromXml<CodSelectionSettings>();
+				var t = Task.Run(() =>
+				{
+					while (true)
+					{
+						if (cancelToken.IsCancellationRequested)
+						{
+							break;
+						}
+
+						if (SelectionCOD.MaxCount > 0 && SelectionCOD.CurrentCount > 0)
+						{
+							var newProgress =
+								(long) Math.Round(((double) SelectionCOD.CurrentCount / SelectionCOD.MaxCount) * 100);
+							if (newProgress != processQueue.Progress)
+							{
+								WorkerCommon.SetProgress(processQueue, newProgress);
+							}
+						}
+					}
+				}, cancelToken);
+			}
+			catch (Exception ex)
+			{
+
+			}
+		}
+	}
+}
