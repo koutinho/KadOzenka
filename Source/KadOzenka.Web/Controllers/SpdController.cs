72871960f55713803ab1c149e6b910ef855819b9
Терешечкин Михаил Юрьевич <tereshechkin@genix.pro>
2020-06-09 08:53:12 +0000

    Мелкие правки по интеграции с СПД
    git-tfs-id: [http://192.168.3.86:8080/tfs/defaultcollection]$/CIPJS/KadOzenka;C60136

---
+++ Source/KadOzenka.Web/Controllers/SpdController.cs
@@ -0,0 +1,19 @@
+﻿using Core.UI.Registers.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using SpdIntegration;
+
+
+namespace RsmCloudService.Web.Controllers
+{
+    public class SpdController : BaseController
+	{
+		public IActionResult OpenSpdRequest(long appId)
+		{
+			if (appId > 0)
+			{
+				return Redirect(SpdSettings.Current.SpdRequestUrl + appId);
+			}
+			return Ok("Отсутствует связанная заявка в СПД");
+		}
+	}
+}
