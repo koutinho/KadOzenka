e74681adaba0a2f78e070b706749e93bf4e14340
Короткова Екатерина Владимировна <INECAS\cp_korotkova>
2020-06-16 13:56:55 +0000

    CIPJSKO-305: правки общего UI
    git-tfs-id: [http://192.168.3.86:8080/tfs/defaultcollection]$/CIPJS/KadOzenka;C60386

--- Source/KadOzenka.Web/Models/ManagementDecisionSupport/StatisticalDataModel.cs
+++ Source/KadOzenka.Web/Models/ManagementDecisionSupport/StatisticalDataModel.cs
@@ -1,9 +1,11 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using KadOzenka.Dal.ManagementDecisionSupport.Enums;
 
 namespace KadOzenka.Web.Models.ManagementDecisionSupport
 {
-	public class StatisticalDataModel
-	{
+	public class StatisticalDataModel : IValidatableObject
+    {
 		/// <summary>
 		/// Тур
 		/// </summary>
@@ -13,13 +15,28 @@ namespace KadOzenka.Web.Models.ManagementDecisionSupport
 		/// <summary>
 		/// Список заданий на оценку
 		/// </summary>
-		[Required(ErrorMessage = "Выберете задания на оценку")]
-		public long[] TaskFilter { get; set; }
+        public long[] TaskFilter { get; set; }
 
 		/// <summary>
 		/// Тип отчета
 		/// </summary>
 		[Required(ErrorMessage = "Выберете тип отчета")]
 		public long? ReportType { get; set; }
-	}
+
+
+        private readonly List<long?> _reportsEnabledWithoutTasks = 
+            new List<long?> { (long)StatisticalDataType.PricingFactorsCompositionForPreviousTours };
+
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            var errors = new List<ValidationResult>();
+            if (!_reportsEnabledWithoutTasks.Contains(ReportType) && (TaskFilter == null || TaskFilter.Length == 0))
+            {
+                errors.Add(new ValidationResult("Выберете задания на оценку"));
+            }
+
+            return errors;
+        }
+    }
 }
