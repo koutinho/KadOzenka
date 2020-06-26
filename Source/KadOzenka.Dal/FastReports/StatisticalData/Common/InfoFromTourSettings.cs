e2f365c344f80835d4a37b55e06adb7fe5c891a9
Короткова Екатерина Владимировна <INECAS\cp_korotkova>
2020-06-26 06:25:21 +0000

    CIPJSKO-305: удален файл
    git-tfs-id: [http://192.168.3.86:8080/tfs/defaultcollection]$/CIPJS/KadOzenka;C60739

--- Source/KadOzenka.Dal/FastReports/StatisticalData/Common/InfoFromTourSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace KadOzenka.Dal.FastReports.StatisticalData.Common
-{
-    public class InfoFromTourSettings
-    {
-        public string ObjectType { get; set; }
-        public string CadastralQuartal { get; set; }
-        public string SubGroupNumber { get; set; }
-    }
-}
