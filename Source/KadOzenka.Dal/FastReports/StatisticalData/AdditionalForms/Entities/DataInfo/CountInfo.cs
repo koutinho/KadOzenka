4574e248848f87571987ffba6b186fb1d763d4b1
Митрюшина Наталья Николаевна <INECAS\cp_mitryushina>
2020-07-03 06:18:11 +0000

    CIPJSKO-305 убран из ui отчет 18 ( Статистика по количеству/средних значениях/перцентилях/суммарных значениях в разрезе произвольных параметров Поддержка программы чтения с экрана включена. ), реализация отчета 19.2 (Дополнительные формы.Анализ расчетов)
    git-tfs-id: [http://192.168.3.86:8080/tfs/defaultcollection]$/CIPJS/KadOzenka;C61041

---
+++ Source/KadOzenka.Dal/FastReports/StatisticalData/AdditionalForms/Entities/DataInfo/CountInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace KadOzenka.Dal.FastReports.StatisticalData.AdditionalForms.Entities.DataInfo
+{
+	public class CountInfo
+	{
+		public string CadastralNumber { get; set; }
+		public long Count { get; set; }
+	}
+}
---
+++ Source/KadOzenka.Dal/FastReports/StatisticalData/AdditionalForms/Entities/DataInfo/Gko2018Info.cs
@@ -0,0 +1,11 @@
+﻿namespace KadOzenka.Dal.FastReports.StatisticalData.AdditionalForms.Entities.DataInfo
+{
+	public class Gko2018Info
+	{
+		public string CadastralNumber { get; set; }
+		public string EvaluationSubgroup2018 { get; set; }
+		public decimal? Upks2018 { get; set; }
+		public decimal? CadastralCost2018 { get; set; }
+		public string CadastralQuartal2018 { get; set; }
+	}
+}
