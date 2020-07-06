f05bf62af56466dc4369cc38623c7a3dafc14fe8
Короткова Екатерина Владимировна <INECAS\cp_korotkova>
2020-07-06 06:35:14 +0000

    CIPJSKO-358: динамические уровни добавлены в шаблоны
    git-tfs-id: [http://192.168.3.86:8080/tfs/defaultcollection]$/CIPJS/KadOzenka;C61097

--- Source/KadOzenka.Web/wwwroot/js/harmonization.js
+++ Source/KadOzenka.Web/wwwroot/js/harmonization.js
@@ -1,29 +1,52 @@
-﻿var rowNumber = 1;
-function addNewRow(url) {
+﻿
+var globalRowNumber;
+
+function clearCustomLevels() {
+    var existedCustomLevels = $("div[class^='customLevelBlock-']");
+    existedCustomLevels.each(function () {
+        this.remove();
+    });
+}
+
+
+function addNewCustomLevel(url, rowNumber, attributeId) {
     kendo.ui.progress($('body'), true);
-    var data = [{ rowNumber }];
+    var data = [{ rowNumber: rowNumber }];
     var templateContent = $("#templateRow").html();
     var template = kendo.template(templateContent);
     var result = kendo.render(template, data);
     $("#levelsList").append(result);
-    var classWrapper = ".wrapper-" + rowNumber;
+    var classWrapper = ".customLevelBlock-" + rowNumber;
     $(classWrapper).load(url, { rowNumber: rowNumber }, function (response) {
-        $(classWrapper).show(500);
-        rowNumber++;
+        $(classWrapper).show(250);
+        if (attributeId) {
+            fillLevelFromTemplate(rowNumber, attributeId);
+        } 
         kendo.ui.progress($('body'), false);
     });
 }
 
 
-function addCustomLevels(formObject) {
+function fillLevelFromTemplate(localRowNumber, attributeId) {
+    var dropDownTree = $("#AttributeId_" + localRowNumber).data('kendoDropDownTree');
+    if (dropDownTree) {
+        dropDownTree.value(attributeId || '');
+        dropDownTree.trigger('change');
+    }
+}
+
+
+function addCustomLevelsToForm(formObject) {
     if (!formObject) {
         return formObject;
     }
     var customLevels = [];
-    for (var i = 1; i < rowNumber; i++) {
+    for (var i = 1; i < globalRowNumber; i++) {
         var attributeName = 'AttributeId_' + i;
         var levelName = 'LevelNumber_' + i;
+        var rowName = 'RowNumber_' + i;
         customLevels.push({
+            RowNumber: formObject[rowName],
             LevelNumber: formObject[levelName],
             AttributeId: formObject[attributeName]
         });
