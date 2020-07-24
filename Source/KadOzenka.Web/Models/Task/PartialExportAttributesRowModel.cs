1f76499d64bab38a4566eb9be3dcace5d4e1b28f
Короткова Екатерина Владимировна <INECAS\cp_korotkova>
2020-07-24 12:13:36 +0000

    CIPJSKO-410: динамические уровни добавлены в шаблоны для переноса атрибутов
    git-tfs-id: [http://192.168.3.86:8080/tfs/defaultcollection]$/CIPJS/KadOzenka;C62165

--- Source/KadOzenka.Web/Models/Task/PartialExportAttributesRowModel.cs
+++ Source/KadOzenka.Web/Models/Task/PartialExportAttributesRowModel.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Dynamic;
-using System.Linq.Expressions;
-
-namespace KadOzenka.Web.Models.Task
+﻿namespace KadOzenka.Web.Models.Task
 {
 	public class PartialExportAttributesRowModel
 	{
