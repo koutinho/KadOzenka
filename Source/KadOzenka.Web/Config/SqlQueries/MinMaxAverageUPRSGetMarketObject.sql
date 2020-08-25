5c27f4b33b5eae28471e76319ea995a3b566cc56
Короткова Екатерина Владимировна <INECAS\cp_korotkova>
2020-08-25 07:04:18 +0000

    CIPJSKO-370: скрипт для получения цены объекта-аналога для УПРС переделан на функцию
    git-tfs-id: [http://192.168.3.86:8080/tfs/defaultcollection]$/CIPJS/KadOzenka;C63771

--- Source/KadOzenka.Web/Config/SqlQueries/MinMaxAverageUPRSGetMarketObject.sql
+++ Source/KadOzenka.Web/Config/SqlQueries/MinMaxAverageUPRSGetMarketObject.sql
@@ -1,10 +1,19 @@
-select
+CREATE OR REPLACE FUNCTION public.get_market_object_price_for_uprs (
+  cadastral_number varchar
+)
+RETURNS numeric AS
+$body$
+ declare
+	price numeric;
+        
+BEGIN
+  select
   case when exists 
   		(SELECT 1
           FROM MARKET_CORE_OBJECT L1_R100
           WHERE 
           (
-            (L1_R100.CADASTRAL_NUMBER = '50:21:0120309:81' AND L1_R100.PROCESS_TYPE_CODE = 742)
+            (L1_R100.CADASTRAL_NUMBER = get_market_object_price_for_uprs.cadastral_number AND L1_R100.PROCESS_TYPE_CODE = 742)
             AND (L1_R100.DEAL_TYPE_CODE = 734 OR L1_R100.DEAL_TYPE_CODE = 733)
             and L1_R100.market_code = 737
           )
@@ -16,7 +25,7 @@ select
           FROM MARKET_CORE_OBJECT L1_R100
           WHERE 
           (
-            (L1_R100.CADASTRAL_NUMBER = '50:21:0120309:81' AND L1_R100.PROCESS_TYPE_CODE = 742)
+            (L1_R100.CADASTRAL_NUMBER = get_market_object_price_for_uprs.cadastral_number AND L1_R100.PROCESS_TYPE_CODE = 742)
             AND (L1_R100.DEAL_TYPE_CODE = 734 OR L1_R100.DEAL_TYPE_CODE = 733)
             and L1_R100.market_code = 737
           )
@@ -30,10 +39,25 @@ select
           FROM MARKET_CORE_OBJECT L1_R100
           WHERE 
           (
-            (L1_R100.CADASTRAL_NUMBER = '50:21:0120309:81' AND L1_R100.PROCESS_TYPE_CODE = 742)
+            (L1_R100.CADASTRAL_NUMBER = get_market_object_price_for_uprs.cadastral_number AND L1_R100.PROCESS_TYPE_CODE = 742)
             AND (L1_R100.DEAL_TYPE_CODE = 734 OR L1_R100.DEAL_TYPE_CODE = 733)
           )
           ORDER BY COALESCE(L1_R100.last_date_update, L1_R100.parser_time) desc
           limit 1
     )
-  end;
+  end
+  into price;
+  
+  RETURN price;
+  
+END;
+$body$
+LANGUAGE 'plpgsql'
+VOLATILE
+CALLED ON NULL INPUT
+SECURITY INVOKER
+PARALLEL UNSAFE
+COST 100;
+
+ALTER FUNCTION public.get_market_object_price_for_uprs (cadastral_number varchar)
+  OWNER TO cipjs_kad_ozenka;
