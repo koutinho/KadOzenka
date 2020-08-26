b0d75288ec44d32f04fdf40c06e9b63144c91234
Митрюшина Наталья Николаевна <INECAS\cp_mitryushina>
2020-08-26 12:05:14 +0000

    CIPJSKO-375 оптимизиация отчетов из блока "Результаты на утверждение"
    git-tfs-id: [http://192.168.3.86:8080/tfs/defaultcollection]$/CIPJS/KadOzenka;C63865

---
+++ Source/KadOzenka.Web/Config/SqlQueries/ResultsForApprovalUpksAverage.sql
@@ -0,0 +1,54 @@
+﻿with districtsDictionary as(
+	select itemId as DistrictCode,
+	short_title as ShortTitle
+	from core_reference_item
+	where referenceid=119
+),
+
+initialData as(
+	SELECT 
+		case 
+			when '{0}'='Districts' then districtsDict.ShortTitle
+			when '{0}'='RegionNumbers' then left(marketDict.CADASTRAL_QUARTAL, 5)
+			when '{0}'='Quarters' then marketDict.CADASTRAL_QUARTAL
+		end as Name,
+		(SELECT parentGroup.GROUP_NAME
+			FROM KO_GROUP parentGroup
+			WHERE parentGroup.ID = subgroup.PARENT_ID
+		) AS ParentGroup,
+		u.CADASTRAL_COST AS ObjectCost,
+		u.SQUARE AS ObjectSquare
+	FROM ko_unit u
+		JOIN MARKET_REGION_DICTIONATY marketDict on u.CADASTRAL_BLOCK = marketDict.CADASTRAL_QUARTAL
+		JOIN districtsDictionary districtsDict on districtsDict.DistrictCode=marketDict.DISTRICT_CODE
+		LEFT JOIN KO_GROUP subgroup ON (u.GROUP_ID = subgroup.ID)
+	WHERE u.TASK_ID IN ({1})
+	AND case when '{2}'= 'True' then u.PROPERTY_TYPE_CODE<>4
+			else u.PROPERTY_TYPE_CODE=4
+		end
+)
+
+select * from (
+	(select d.Name as Name, 
+	 0 as IsTotal,
+	COALESCE(d.ParentGroup, 'Без группы') as GroupName,
+	case when d.ParentGroup is null then 1 else 0 end as HasGroup,
+	sum(d.ObjectCost)/ nullif(sum(d.ObjectSquare), 0) as UpksAverageWeight
+	from initialData d
+	group by Name, GroupName, HasGroup)
+union all
+	(select 'Итого' as Name, 
+	 1 as IsTotal,
+	 dg.GroupName,
+	 dg.HasGroup,
+	 dg.UpksAverageWeight
+	 from ( select
+		COALESCE(d.ParentGroup, 'Без группы') as GroupName,
+		case when d.ParentGroup is null then 1 else 0 end as HasGroup,
+		sum(d.ObjectCost)/ nullif(sum(d.ObjectSquare), 0) as UpksAverageWeight
+		from initialData d
+		group by GroupName, HasGroup
+	 ) dg)
+) res
+	order by HasGroup desc, IsTotal, Name, GroupName
+	
