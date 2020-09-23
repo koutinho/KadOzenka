af1f2116af4db543516e450657dfd89fae2b4744
Mitryushina Natalya <mitryushina@corepartners.ru>
2020-09-23 16:53:40 +0300

    CIPJSKO-482 реализация процедуры по добавлению недостающих данных для объектов-аналогов из гбу части

--- Source/KadOzenka.Dal/Selenium/FillingAdditionalFields/YandexFilling.cs
+++ Source/KadOzenka.Dal/Selenium/FillingAdditionalFields/YandexFilling.cs
@@ -100,7 +100,6 @@ namespace KadOzenka.Dal.Selenium.FillingAdditionalFields
 				int totalCount = objects.Count, currentCount = 0, correctCount = 0, updateCount = 0, errorCount = 0;
 				foreach (OMCoreObject marketObject in objects)
 				{
-					DateTime currentTime = DateTime.Now;
 					try
 					{
 						driver.Navigate().GoToUrl(marketObject.Url);
@@ -119,7 +118,6 @@ namespace KadOzenka.Dal.Selenium.FillingAdditionalFields
 							var deserializedObject = (JObject)JsonConvert.DeserializeObject(jsObjectData.ToString());
 							if (YandexParserUtils.FillAdditionalData(deserializedObject, marketObject))
 							{
-								marketObject.LastDateUpdate = currentTime;
 								marketObject.Save();
 								updateCount++;
 							}
