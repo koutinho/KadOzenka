25fd9479654ca3b03db95ce40044d4c409d867cd
Mitryushina Natalya <mitryushina@corepartners.ru>
2020-10-02 13:45:56 +0300

    CIPJSKO-498 изменен формат отображения задания на оценку

--- Source/KadOzenka.Dal/Tasks/Dto/TaskDocumentInfoDto.cs
+++ Source/KadOzenka.Dal/Tasks/Dto/TaskDocumentInfoDto.cs
@@ -6,6 +6,7 @@ namespace KadOzenka.Dal.Tasks.Dto
 	{
 		public long TaskId { get; set; }
 		public long? TourId { get; set; }
+		public DateTime? EstimationDate { get; set; }
 		public DateTime? DocumentCreateDate { get; set; }
 		public string DocumentRegNumber { get; set; }
 		public string KoNoteType { get; set; }
