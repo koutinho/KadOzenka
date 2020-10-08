95b67ff7e3035bbf54d05d0871371334afbb3b48
Mitryushina Natalya <mitryushina@corepartners.ru>
2020-10-08 09:31:55 +0300

    CIPJSKO-512 изменение получения кадастрового квартала и исключения типа ОН "Кадастровый квартал" для отчета "Статистика по минимальным, максимальным и средним (арифметическое и средневзвешенное) УПКС/УПРС в разрезе кадастровых кварталов"

--- Source/KadOzenka.Web/Config/SqlQueries/MinMaxAverageUPKSByCadastralQuarters.sql
+++ Source/KadOzenka.Web/Config/SqlQueries/MinMaxAverageUPKSByCadastralQuarters.sql
@@ -2,20 +2,23 @@
 with initialData as (
 	SELECT 
 		L1_R201.ID,
-		L1_R107.CADASTRAL_QUARTAL,
+		L1_R201.OBJECT_ID as ObjectId,
+		L1_R201.CADASTRAL_BLOCK AS CadastralQuartal,
 		L1_R201.PROPERTY_TYPE,
 		L1_R201.UPKS,
 		L1_R201.CADASTRAL_COST,
 		L1_R201.SQUARE
 	FROM KO_UNIT L1_R201
-	 JOIN MARKET_REGION_DICTIONATY L1_R107
-	 ON (L1_R201.CADASTRAL_BLOCK = L1_R107.CADASTRAL_QUARTAL)
-	WHERE L1_R201.TASK_ID IN ({0})
+	WHERE L1_R201.TASK_ID IN ({0}) AND L1_R201.PROPERTY_TYPE_CODE<>2190
+),
+
+cadastralQuartalAttrValues as (
+	select * from  gbu_get_allpri_attribute_values( ARRAY(select ObjectId from initialData), {1})
 ),
 
 dataGroupedByQuartalAndPropertyType as (
 select 
-	d.CADASTRAL_QUARTAL,
+	L1_R107.CADASTRAL_QUARTAL,
 	d.PROPERTY_TYPE,
 	count(d.ID) as PROPERTY_TYPE_OBJECTS_COUNT,
 	min(d.UPKS) as MIN_UPKS, 
@@ -24,7 +27,10 @@ select
 	sum(d.CADASTRAL_COST) as SUM_CADASTRAL_COST,
 	sum(d.SQUARE) as SUM_SQUARE
 from initialData d
-group by d.CADASTRAL_QUARTAL, d.PROPERTY_TYPE
+	left outer join cadastralQuartalAttrValues cadastralQuartalGbu on d.ObjectId=cadastralQuartalGbu.objectId
+	JOIN MARKET_REGION_DICTIONATY L1_R107 
+		ON COALESCE(cadastralQuartalGbu.attributeValue, d.CadastralQuartal)=L1_R107.CADASTRAL_QUARTAL
+group by L1_R107.CADASTRAL_QUARTAL, d.PROPERTY_TYPE
 ),
 
 obj_count as (
