9b3e23d13846a2b5ec6bb6736c1a143dd25215e3
Mitryushina Natalya <mitryushina@corepartners.ru>
2020-10-26 11:34:41 +0300

    CIPJSKO-539 коммит файла

---
+++ Source/KadOzenka.Web/Models/MarketObject/OutliersCheckingModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace KadOzenka.Web.Models.MarketObject
+{
+	public class OutliersCheckingModel
+	{
+		public bool HasHistory { get; set; }
+		public DateTime? LastDateCreated { get; set; }
+		public DateTime? LastDateStarted { get; set; }
+		public DateTime? LastDateFinished { get; set; }
+		public string LastStatus { get; set; }
+		public string LastMarketSegment { get; set; }
+		public long? LastTotalObjectsCount { get; set; }
+		public long? LastExcludedObjectsCount { get; set; }
+		public string LastReportDownloadLink { get; set; }
+
+
+		public bool HasCurrentAddedProcess { get; set; }
+		public bool HasCurrentRunningProcess { get; set; }
+		public long? CurrentProgress { get; set; }
+	}
+}
