493b730d833c9dcd13a3dcd850e7d043b635209e
Mitryushina Natalya <mitryushina@corepartners.ru>
2020-10-27 10:24:05 +0300

    CIPJSKO-547 фиск для Количество объектов по КР/МР/АО в разрезе групп и видов объектов недвижимости

--- Source/KadOzenka.Web/Config/SqlQueries/NumberOfObjectsByAdministrativeDistrictsByGroupsAndTypes_Regions.sql
+++ Source/KadOzenka.Web/Config/SqlQueries/NumberOfObjectsByAdministrativeDistrictsByGroupsAndTypes_Regions.sql
@@ -6,7 +6,9 @@ SELECT
         L1_R201.PROPERTY_TYPE_CODE AS PropertyTypeCode,
 		(SELECT case when L2_R205.NUMBER is null then L2_R205.GROUP_NAME 
                 else CONCAT(L2_R205.NUMBER, '. ', L2_R205.GROUP_NAME) end
-        FROM KO_GROUP L2_R205 WHERE(L2_R205.ID = L1_R205.PARENT_ID)) AS ParentGroup
+        FROM KO_GROUP L2_R205 WHERE(L2_R205.ID = L1_R205.PARENT_ID)) AS ParentGroup,
+		(SELECT L2_R205.NUMBER
+            FROM KO_GROUP L2_R205 WHERE(L2_R205.ID = L1_R205.PARENT_ID)) AS ParentGroupNumber
 FROM KO_UNIT L1_R201
 LEFT JOIN KO_GROUP L1_R205 ON (L1_R201.GROUP_ID = L1_R205.ID) 
 WHERE (L1_R201.TASK_ID IN ({0}) AND L1_R201.OBJECT_ID IS NOT NULL
@@ -24,11 +26,13 @@ data as(
 	L1_R107.DISTRICT_CODE AS DistrictCode,
 	L1_R107.REGION_CODE as RegionCode,
 	u.PropertyTypeCode,
-    u.ParentGroup
+    u.ParentGroup,
+	u.ParentGroupNumber
 	from unit_data u
     left outer join cadastralQuartalAttrValues cadastralQuartalGbu on u.ObjectId=cadastralQuartalGbu.objectId
 	JOIN MARKET_REGION_DICTIONATY L1_R107 
 		ON COALESCE(cadastralQuartalGbu.attributeValue, u.CadastralQuartal)=L1_R107.CADASTRAL_QUARTAL
 )
 SELECT DistrictCode, RegionCode, PropertyTypeCode, count(id) AS objectsCount, ParentGroup FROM data
-GROUP BY (DistrictCode, RegionCode, PropertyTypeCode, ParentGroup);
+GROUP BY (DistrictCode, RegionCode, PropertyTypeCode, ParentGroup)
+order by min(ParentGroupNumber::int);
