10ba0263d6ea2bbd7856022bc3618f58b566477f
Korotkova Ekaterina <korotkova@corepartners.ru>
2020-10-28 15:29:19 +0300

    CIPJSKO-545: добавлено логирование и исправлена ошибка для Выборки из справочника ЦОД

--- Source/KadOzenka.Dal/GbuObject/GbuObjectSelectionCOD.cs
+++ Source/KadOzenka.Dal/GbuObject/GbuObjectSelectionCOD.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Core.SRD;
+using KadOzenka.Dal.DataImport;
+using Newtonsoft.Json;
 using ObjectModel.Gbu.CodSelection;
 using ObjectModel.Core.TD;
+using Serilog;
 
 namespace KadOzenka.Dal.GbuObject
 {
@@ -13,6 +16,8 @@ namespace KadOzenka.Dal.GbuObject
     /// </summary>
     public class SelectionCOD
     {
+	    private static readonly ILogger _log = Log.ForContext<SelectionCOD>();
+
         /// <summary>
         /// Объект для блокировки счетчика в многопоточке
         /// </summary>
@@ -51,6 +56,8 @@ namespace KadOzenka.Dal.GbuObject
 		/// </summary>
 		public static long Run(CodSelectionSettings setting)
         {
+	        _log.ForContext("InputParameters", JsonConvert.SerializeObject(setting)).Debug("Входные данные для Выборки из справочника ЦОД");
+
             _reportService = new GbuReportService();
 
             _reportService.AddHeaders(new List<string> { "КН", "Поле в которое производилась запись", "Внесенное значение", "Источник внесенного значения", "Ошибка" });
@@ -66,6 +73,8 @@ namespace KadOzenka.Dal.GbuObject
             List<ObjectModel.KO.OMCodDictionary> DictionaryItem = new List<ObjectModel.KO.OMCodDictionary>();
             if (setting.IdCodJob != null)
                 DictionaryItem = ObjectModel.KO.OMCodDictionary.Where(x => x.IdCodjob == setting.IdCodJob).SelectAll().Execute();
+            _log.Debug("Найдено {CodItemsCount} значений из справочника ЦОС с ИД {CodId}", DictionaryItem.Count, setting.IdCodJob);
+
             List<ObjectModel.Gbu.OMMainObject> Objs = new List<ObjectModel.Gbu.OMMainObject>();
             foreach (ObjectModel.KO.OMCodDictionary item in DictionaryItem)
             {
@@ -77,8 +86,10 @@ namespace KadOzenka.Dal.GbuObject
             OMInstance doc = OMInstance.Where(x => x.Id == idDoc).SelectAll().ExecuteFirstOrDefault();
 
             DateTime dt = (doc == null) ? DateTime.Now : doc.CreateDate;
+            _log.ForContext("DateForNewAttributes", dt).Debug("Дата, с которой будут сохранены новые атрибуты");
 
             MaxCount = Objs.Count;
+            _log.Debug("Найдено {Count} Единиц оценки", MaxCount);
             CurrentCount = 0;
             Parallel.ForEach(Objs, options, item => { RunOneGbu(item, setting, DictionaryItem, dt); });
             CurrentCount = 0;
@@ -90,11 +101,13 @@ namespace KadOzenka.Dal.GbuObject
             _reportService.SetIndividualWidth(valueColumn, 3);
             _reportService.SetIndividualWidth(outputFieldColumn, 6);
             _reportService.SetIndividualWidth(errorColumn, 5);
+
             long reportId = _reportService.SaveReport("Отчет выборки из справочника ЦОД по кадастровому номеру");
             _reportService = null;
 
-            return reportId;
+            _log.Debug("Закончена операция Выборки из справочника ЦОД");
 
+            return reportId;
         }
 
         public static void RunOneGbu(ObjectModel.Gbu.OMMainObject obj, CodSelectionSettings setting, List<ObjectModel.KO.OMCodDictionary> dictionaryItem, DateTime dt)
@@ -130,8 +143,7 @@ namespace KadOzenka.Dal.GbuObject
                 Ot = dt,
                 StringValue = value
             };
-            attributeValue.Save();
-
+            DataImporterGkn.SaveAttributeValueWithCheck(attributeValue);
         }
 
         public static void AddRowToReport(int rowNumber, string kn, string value, long resultAttribute, string errorMessage = "", string sourceName = "Справочник ЦОД")
