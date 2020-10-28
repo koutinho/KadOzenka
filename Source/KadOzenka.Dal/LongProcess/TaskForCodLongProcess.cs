eb497db0f1c0bf4e82efad0b8cb2f36b454d445d
Korotkova Ekaterina <korotkova@corepartners.ru>
2020-10-28 16:27:43 +0300

    CIPJSKO-545: реализована пагинация для отчета Выгрузка заданий для ЦОД

--- Source/KadOzenka.Dal/LongProcess/TaskForCodLongProcess.cs
+++ Source/KadOzenka.Dal/LongProcess/TaskForCodLongProcess.cs
@@ -100,13 +100,13 @@ namespace KadOzenka.Dal.LongProcess
 		        Header = "Статус после обновления",
 		        Width = 8
 	        };
-	        reportService.AddHeaders(new List<string> { cadastralNumberColumn.Header, statusNumberColumn.Header });
+	        reportService.AddHeadersNew(new List<string> { cadastralNumberColumn.Header, statusNumberColumn.Header });
 	        reportService.SetIndividualWidth(cadastralNumberColumn.Index, cadastralNumberColumn.Width);
 	        reportService.SetIndividualWidth(statusNumberColumn.Index, statusNumberColumn.Width);
 
 	        units.ForEach(x =>
 	        {
-		        var row = reportService.GetCurrentRow();
+		        var row = reportService.GetCurrentRowNew();
 		        reportService.AddValue(x.CadastralNumber, cadastralNumberColumn.Index, row);
 		        reportService.AddValue(x.UpdateStatus_Code.GetEnumDescription(), statusNumberColumn.Index, row);
 	        });
