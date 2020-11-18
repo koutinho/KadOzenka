5112bec51ea0b0e4077d94ff8f4b4816d8f0c3cb
yarsilanov <45903303+yarsilanov@users.noreply.github.com>
2020-11-18 10:50:56 +0300

    -Функция GetCIANDataByMultipleValues переписана так, что теперь реализует "соглашение". Теперь функция в любом случае возвращает строку, даже если в процессе её выполнения происходит ошибка. Смысл соглашения состоит в том, что функция, возвращающая какое-либо значение всегда долна его возвращать, даже если в процессе её выполнения происходит ошибка. К примеру, в данном случае, если в процессе выполнения функции будет сгенерированно исключение, то в вызывающую область будет возвращена пустая строка. Далее необходимо обработать либо пустую строку, либо результат выполнения функции;

--- Source/KadOzenka.Dal/WebRequest/RestApp.cs
+++ Source/KadOzenka.Dal/WebRequest/RestApp.cs
@@ -22,15 +22,25 @@ namespace KadOzenka.Dal.WebRequest
 
         public string GetCIANDataByMultipleValues(string regionId, string dealId, DateTime date1, DateTime date2, string login, string token)
         {
-                _log
-                    .ForContext("Method", "GetCIANDataByMultipleValues")
-                    .Debug("Попытка получения данных от RestApp");
-                return 
-                    new StreamReader(
+            _log
+                .ForContext("Method", "GetCIANDataByMultipleValues")
+                .Debug("Попытка получения данных от RestApp");
+            string result = string.Empty;
+            try
+            {
+                result = new StreamReader(
                         System.Net.WebRequest.Create(string.Format(restAppCIANLink, login, token, dealId.ToString(), regionId.ToString(), date1.ToString(restAppTimeTemplate), date2.ToString(restAppTimeTemplate)))
                         .GetResponse()
                         .GetResponseStream(), Encoding.UTF8)
                     .ReadToEnd();
+            }
+            catch (Exception ex) 
+            {
+                _log
+                    .ForContext("Method", "GetCIANDataByMultipleValues")
+                    .Error(ex, "Ошибка получения ответа от RestApp");
+            }
+            return result;
         }
 
         public string GetAvitoDataByMultipleValues(string regionId, string categoryId, DateTime date1, DateTime date2, string login, string token) => 
