9c17163bf90f1d4585ee08ccb5bcda924732e1ce
Дьячков Дмитрий Романович <diachkov@genix.pro>
2020-11-19 11:06:11 +0300

    CIPJSKO-609-Внёс правки в скрипты публикации

--- Source/Utils/PublishScripts/publish_CIPJS_KO_PPR_8084.ps1
+++ Source/Utils/PublishScripts/publish_CIPJS_KO_PPR_8084.ps1
@@ -1,11 +1,9 @@
 ﻿
 $df =  (Get-Date).ToString("yyyy-MM-dd HH-mm")
 
-$site = "CIPJS_KO_PPR"
-$IIS_siteName = "CIPJS_KO_PPR"
-$IIS_siteName2 = "CIPJS_KO_PPR_AD"
-$IIS_WebAppPoolName = "CIPJS_KO_PPR"
-$IIS_WebAppPoolName2 = "CIPJS_KO_PPR_AD"
+$site = $IIS_siteName = $IIS_WebAppPoolName = "CIPJS_KO_PPR"
+$site2 = $IIS_siteName2 = $IIS_WebAppPoolName2 = "CIPJS_KO_PPR_AD"
+$service = $IIS_serviceName = $IIS_servicePoolName = "CIPJS_KO_PPR_LongProcessService"
 
 $backup = $true
 $backup_path = "D:\Backup_Site\$site_"
@@ -32,6 +30,18 @@ $scripts_path = "$release_path\scripts"
 if (Test-Path -Path $service_dir -IsValid){ 
     New-Item -ItemType Directory -Force -Path $service_dir 
 }
+Write-Host 1. Бэкап текущей версии
+
+    Write-Host (Get-Date).ToString("yyyy-MM-dd HH:mm:ss") Делаем бэкап config-файлов
+        foreach ($item in $config_array){
+            cpi -Path $site_dir\$item $backup_path\config\ -Recurse -Force -Verbose
+            cpi -Path $site_dir\$item $config_path\ -Recurse -Force -Verbose
+        }
+    if ($backup) {
+        cpi -Path $site_dir "$backup_path\backup\" -Recurse -Force
+        Write-Host (Get-Date).ToString("yyyy-MM-dd HH:mm:ss") Делаем бэкап всего сайта
+        compress-archive "$backup_path\backup\" $backup_path"backup_$site_$df.zip" 
+    }
 
 Write-Host 2. Публикация релиза 
     
@@ -48,31 +58,31 @@ Write-Host 2. Публикация релиза
         $status = Get-WebAppPoolState -name $IIS_WebAppPoolName2
         Write-Host $status.Value
         if ($status.Value -eq "Stopped"){
-     
-                Write-Host (Get-Date).ToString("yyyy-MM-dd HH:mm:ss") Делаем бэкап
-                foreach ($item in $config_array){
-                    cpi -Path $site_dir\$item $backup_path\config -Recurse -Force -Verbose
-                    cpi -Path $site_dir\$item $config_path -Recurse -Force -Verbose
-                }
-                if ($backup) {
-                    compress-archive $site_dir $backup_path"$df.zip" 
-                }
+                Write-Host (Get-Date).ToString("yyyy-MM-dd HH:mm:ss") Обновляем sql-скрипты
+                cpi -Path $scripts_path\* $sql_scripts_path -Force -Verbose
+                
                 Write-Host (Get-Date).ToString("yyyy-MM-dd HH:mm:ss") Публикуем релиз сайта
                 cpi -Path $site_path\* $site_dir\ -Recurse -Force
+
                 Write-Host (Get-Date).ToString("yyyy-MM-dd HH:mm:ss") Заменяем конфиги
-                cpi -Path $config_path\* $site_dir -Force -Verbose 
-                Start-WebSite -Name $IIS_siteName 
-                Start-WebSite -Name $IIS_siteName2
-                Start-WebAppPool -Name $IIS_WebAppPoolName
-                Start-WebAppPool -Name $IIS_WebAppPoolName2
-                $success = $true;
-                
+                cpi -Path $backup_path\config\* $site_dir -Force -Verbose 
+
                 Write-Host (Get-Date).ToString("yyyy-MM-dd HH:mm:ss") Обновляем файлы службы фоновых процессов
                 foreach ($item in $service_array){
                      cpi -Path $service_path\$item $service_dir -Force -Verbose
                 }
-                Write-Host (Get-Date).ToString("yyyy-MM-dd HH:mm:ss") Обновляем sql-скрипты
-                cpi -Path $scripts_path\* $sql_scripts_path -Force -Verbose
+                $success = $true;
+
+                Write-Host (Get-Date).ToString("yyyy-MM-dd HH:mm:ss") Запускаем сайты и приложения
+                Start-WebAppPool -Name $IIS_servicePoolName
+                Start-WebAppPool -Name $IIS_WebAppPoolName
+                Start-WebAppPool -Name $IIS_WebAppPoolName2
+                Start-WebSite    -Name $IIS_serviceName
+                Start-WebSite    -Name $IIS_siteName 
+                Start-WebSite    -Name $IIS_siteName2
+                
+                Invoke-WebRequest -Uri "http://localhost:8086/"
+                Invoke-WebRequest -Uri "http://localhost:8084/"
                 Write-Host (Get-Date).ToString("yyyy-MM-dd HH:mm:ss") Релиз опубликован. Нужно выполнить запуск C:\! Выполнение скриптов БД\Кадастровая оценка - TEST\ExecuteSqlFiles2.exe с правами администратора
             }
             Start-Sleep -s 5
