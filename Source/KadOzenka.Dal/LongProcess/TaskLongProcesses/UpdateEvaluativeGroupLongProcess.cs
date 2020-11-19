3fbfdfa970bcead50bb6b09e685bccb4fb2d253c
Korotkova Ekaterina <korotkova@corepartners.ru>
2020-11-19 16:00:23 +0300

    CIPJSKO-600: рефакторинг (вынос общей логики для работы с группами тура в сервис)

--- Source/KadOzenka.Dal/LongProcess/TaskLongProcesses/UpdateEvaluativeGroupLongProcess.cs
+++ Source/KadOzenka.Dal/LongProcess/TaskLongProcesses/UpdateEvaluativeGroupLongProcess.cs
@@ -9,6 +9,7 @@ using Core.Register.LongProcessManagment;
 using Core.Register.RegisterEntities;
 using Core.Shared.Extensions;
 using KadOzenka.Dal.GbuObject;
+using KadOzenka.Dal.GbuObject.Dto;
 using KadOzenka.Dal.Groups;
 using KadOzenka.Dal.Groups.Dto;
 using KadOzenka.Dal.Tasks;
@@ -224,7 +225,8 @@ namespace KadOzenka.Dal.LongProcess.TaskLongProcesses
 				DateTime.Now.GetEndOfTheDay(), isLight: true);
 			_log.Debug("Найдено {GbuEvaluativeGroupValuesCount} значений ГБУ-атрибутов", gbuEvaluativeGroupValues.Count);
 
-			var tourGroupsInfo = GetGroups(task.TourId);
+			var tourGroupsInfo = GroupService.GetTourGroupsInfo(task.TourId, ObjectTypeExtended.Both);
+			_log.Debug("Найдены группы для Тура");
 
 			_locked = new object();
 			var cancelTokenSource = new CancellationTokenSource();
@@ -255,28 +257,6 @@ namespace KadOzenka.Dal.LongProcess.TaskLongProcesses
 			_log.Debug("Найдено {UnitsCount} единиц оценки с непроставленной группой", units.Count);
 
 			return units;
-		}
-
-		private TourGroupsInfo GetGroups(long tourId)
-		{
-			var allGroupsInTour = GroupService.GetGroupsTreeForTour(tourId);
-			_log.Debug("Найдено {GroupsCount} групп для Тура {TourId}", allGroupsInTour.Count, tourId);
-
-			var oksGroups = GetGroups(allGroupsInTour, KoGroupAlgoritm.MainOKS);
-			var zuGroups = GetGroups(allGroupsInTour, KoGroupAlgoritm.MainParcel);
-
-			return new TourGroupsInfo
-			{
-				OksGroups = oksGroups,
-				OksSubGroups = oksGroups.SelectMany(x => x.Items).ToList(),
-				ZuGroups = zuGroups,
-				ZuSubGroups = zuGroups.SelectMany(x => x.Items).ToList()
-			};
-		}
-
-		private List<GroupTreeDto> GetGroups(List<GroupTreeDto> allGroupsInTour, KoGroupAlgoritm type)
-		{
-			return allGroupsInTour.Where(x => x.Id == (int)type).SelectMany(x => x.Items).ToList();
 		}
 
 		private void UpdateUnitGroup(List<GbuObjectAttribute> gbuEvaluativeGroupValues, OMUnit unit, TourGroupsInfo allGroupsInTour)
@@ -368,15 +348,6 @@ namespace KadOzenka.Dal.LongProcess.TaskLongProcesses
 			public long Id { get; set; }
 			public string Name { get; set; }
 			public long TourId { get; set; }
-		}
-
-		private class TourGroupsInfo
-		{
-			public List<GroupTreeDto> OksGroups { get; set; }
-			public List<GroupTreeDto> OksSubGroups { get; set; }
-			public List<GroupTreeDto> ZuGroups { get; set; }
-			public List<GroupTreeDto> ZuSubGroups { get; set; }
-
 		}
 
 		#endregion
