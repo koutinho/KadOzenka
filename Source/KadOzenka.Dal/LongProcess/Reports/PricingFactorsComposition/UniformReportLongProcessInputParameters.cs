749c3762b7932b96825a89901a6f6ada7cc2c15e
Korotkova Ekaterina <korotkova@corepartners.ru>
2020-12-15 09:19:16 +0300

    CIPJSKO-645: добавлен файл с классом для входных параметров процесса

---
+++ Source/KadOzenka.Dal/LongProcess/Reports/PricingFactorsComposition/UniformReportLongProcessInputParameters.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace KadOzenka.Dal.LongProcess.Reports.PricingFactorsComposition
+{
+	public class UniformReportLongProcessInputParameters
+	{
+		public List<long> TaskIds { get; set; }
+		public int? PackageSize { get; set; }
+	}
+}
