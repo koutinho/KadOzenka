4925207e16f708bfbcbda0a973e2db685f811c47
Korotkova Ekaterina <korotkova@corepartners.ru>
2021-01-11 16:34:31 +0300

    CIPJSKO-645: реализовано создание триггеров при добавлении новой ГБУ-таблицы

--- Source/Utils/KadOzenka.BlFrontEnd/GbuTest/TriggerCreationForDataCompositionReports.cs
+++ Source/Utils/KadOzenka.BlFrontEnd/GbuTest/TriggerCreationForDataCompositionReports.cs
@@ -1,8 +1,5 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using KadOzenka.Dal.ManagementDecisionSupport.StatisticalData.PricingFactorsComposition;
+﻿using KadOzenka.Dal.ManagementDecisionSupport.StatisticalData.PricingFactorsComposition;
 using Microsoft.Practices.EnterpriseLibrary.Data;
-using Platform.Register;
 
 namespace KadOzenka.BlFrontEnd.GbuTest
 {
@@ -12,63 +9,34 @@ namespace KadOzenka.BlFrontEnd.GbuTest
 		{
 			CreateTriggerForGbuMainObject();
 
-			var postfixes = new List<string> { "TXT", "NUM", "DT" };
 			DataCompositionByCharacteristicsService.CachedRegisters.ForEach(register =>
 			{
-				if (register.AllpriPartitioning == AllpriPartitioningType.DataType)
-				{
-					foreach (var postfix in postfixes)
-					{
-						var gbuTableName = $"{register.AllpriTable}_{postfix}";
-						var triggerName = $"trigger_for_{gbuTableName}";
-
-						CreateTriggerForRegistersWithAttributes(triggerName, gbuTableName, null);
-					}
-				}
-				else
-				{
-					var attributes = DataCompositionByCharacteristicsService.CachedAttributes.Where(x => x.RegisterId == register.Id).ToList();
-					foreach (var attribute in attributes)
-					{
-						if (attribute.IsPrimaryKey)
-							continue;
-
-						var gbuTableName = $"{register.AllpriTable}_{attribute.Id}";
-						var triggerName = $"trigger_for_{gbuTableName}";
-
-						CreateTriggerForRegistersWithAttributes(triggerName, gbuTableName, attribute.Id);
-					}
-				}
+				DataCompositionByCharacteristicsService.CreateTriggerForRegister(register.Id);
 			});
 		}
 
-		private static void CreateTriggerForRegistersWithAttributes(string triggerName, string gbuTableName, long? functionInputParameter)
-		{
-			var triggerCreationSql = GetTriggerCreationSql(triggerName, gbuTableName,
-				$"update_cache_table_for_data_composition_reports_for_registers({functionInputParameter})");
+		//реализовано в коде при создании нового ГБУ-источника (ObjectsCharacteristicsService)
+		//private static void CreateTriggerForNewRegistersWithPartitionByDataType(string triggerName, string gbuTableName, long? functionInputParameter)
+		//{
+		//	var triggerCreationSql = @"DROP TRIGGER IF EXISTS test_trigger ON core_register;
+		//					CREATE TRIGGER core_register_trigger_for_data_composition_report
+		//					AFTER INSERT 
+		//					ON core_register
+		//					FOR EACH ROW
+		//					WHEN (NEW.quant_table = 'GBU_MAIN_OBJECT')
+		//					EXECUTE FUNCTION create_trigger_for_new_gbu_source_table_with_partition_by_data();";
 
-			var createTriggerCommand = DBMngr.Main.GetSqlStringCommand(triggerCreationSql);
-			DBMngr.Main.ExecuteNonQuery(createTriggerCommand);
-		}
+		//	var createTriggerCommand = DBMngr.Main.GetSqlStringCommand(triggerCreationSql);
+		//	DBMngr.Main.ExecuteNonQuery(createTriggerCommand);
+		//}
 
 		private static void CreateTriggerForGbuMainObject()
 		{
-			var triggerCreationSql = GetTriggerCreationSql("trigger_for_gbu_main_object", "gbu_main_object",
+			var triggerCreationSql = DataCompositionByCharacteristicsService.GetTriggerCreationSql("gbu_main_object",
 				"update_cache_table_for_data_composition_reports_for_main_object()");
 
 			var createTriggerCommand = DBMngr.Main.GetSqlStringCommand(triggerCreationSql);
 			DBMngr.Main.ExecuteNonQuery(createTriggerCommand);
-		}
-
-		private static string GetTriggerCreationSql(string triggerName, string gbuTableName, string functionName)
-		{
-			return $@"DROP TRIGGER IF EXISTS {triggerName} ON {gbuTableName};
-							
-						CREATE TRIGGER {triggerName}
-						AFTER INSERT 
-						ON {gbuTableName}
-						FOR EACH ROW
-						EXECUTE FUNCTION {functionName};";
 		}
 	}
 }
