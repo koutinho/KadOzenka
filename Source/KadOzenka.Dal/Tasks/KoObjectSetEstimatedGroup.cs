db818f4d45573b7833b53008bf593fe876dc8ae5
Mitryushina Natalya <mitryushina@corepartners.ru>
2021-01-13 11:21:44 +0300

    CIPJSKO-691 оптимизация (изменение работы алгоритма с порционной обработкой юнитов для снижения запросов к базе)

--- Source/KadOzenka.Dal/Tasks/KoObjectSetEstimatedGroup.cs
+++ Source/KadOzenka.Dal/Tasks/KoObjectSetEstimatedGroup.cs
@@ -64,11 +64,13 @@ namespace KadOzenka.Dal.KoObject
 	{
 		private static readonly ILogger Logger = Log.ForContext<KoObjectSetEstimatedGroup>();
 
-		public static object locked;
-		public static int SuccessCount;
+		private object locked;
+
+		public int CountAllUnits { get; private set; }
+		public int CurrentCount { get; private set; }
 
 
-		public static long Run(EstimatedGroupModel param)
+		public long Run(EstimatedGroupModel param)
 		{ 
 			Logger.ForContext("InputParameters", JsonConvert.SerializeObject(param)).Debug("Входные данные для Присвоения оценочной группы");
 
@@ -87,6 +89,7 @@ namespace KadOzenka.Dal.KoObject
 
 			var units = unitsGetter.GetItems();
 			Logger.Debug($"Найдено {units.Count} ЕО");
+			CountAllUnits = units.Count;
 
 			CancellationTokenSource cancelTokenSource = new CancellationTokenSource();
 			ParallelOptions options = new ParallelOptions
@@ -97,80 +100,133 @@ namespace KadOzenka.Dal.KoObject
 
 			var estimatedSubGroupAttribute = RegisterCache.GetAttributeData((int) param.IdEstimatedSubGroup);
 			var codeGroupAttribute = RegisterCache.GetAttributeData((int)param.IdCodeGroup);
+			var attributeQuarter = RegisterCache.GetAttributeData((int)param.IdCodeQuarter);
+			var attributeTerritoryType = RegisterCache.GetAttributeData((int)param.IdTerritoryType);
 
-			var gbuObjects = new List<OMMainObject>();
-			var gbuObjectIds = units.Select(x => x.ObjectId).ToList();
-			if(gbuObjectIds.IsNotEmpty())
-				gbuObjects.AddRange(OMMainObject.Where(x => gbuObjectIds.Contains(x.Id)).SelectAll().Execute());
+			// обрабатываем юниты порциями по 1000 для уменьшения числа запросов к бд
+			var partitionSize = 1000;
+			var partitionCount = units.Count / partitionSize + 1;
 
-			Parallel.ForEach(units, options, item =>
+			for (var i = 0; i < partitionCount; i++)
 			{
-				var gbuObject = gbuObjects.FirstOrDefault(x => x.Id == item.ObjectId);
+				var currentUnitsPartition = units.Skip(i * partitionSize).Take(partitionSize).ToList();
 
-				// берем код группы (значение из справочника цод)
-				var codeGroup = GetValueFactor(gbuObject, codeGroupAttribute.RegisterId, codeGroupAttribute.Id);
-				if (string.IsNullOrEmpty(codeGroup.Value))
+				Logger.ForContext("CurrentHandledCount", CurrentCount)
+					.ForContext("UnitPartitionCount", currentUnitsPartition.Count)
+					.ForContext("CountAllUnits", CountAllUnits)
+					.Debug("Получение необходимых данных из базы для текущей порции юнитов");
+
+				var gbuObjects = new List<OMMainObject>();
+				var gbuObjectIds = currentUnitsPartition.Select(x => x.ObjectId).ToList();
+				if (gbuObjectIds.IsNotEmpty())
+					gbuObjects = OMMainObject.Where(x => gbuObjectIds.Contains(x.Id)).Select(x => x.CadastralNumber).Execute();
+				Logger.ForContext("UnitPartitionCount", currentUnitsPartition.Count)
+					.ForContext("GbuObjectsCount", gbuObjects.Count)
+					.Verbose("Получение связанных с юнитами ГБУ объектов");
+
+				var codeGroups = GetValueFactors(gbuObjects, codeGroupAttribute.RegisterId, codeGroupAttribute.Id);
+				Logger.Verbose("Получение значений атрибута кода группы ГБУ объектов");
+
+				var allComplianceGuides = new List<OMComplianceGuide>();
+				if (currentUnitsPartition.IsNotEmpty() && codeGroups.Values.Any(x => !string.IsNullOrEmpty(x.Value)))
 				{
-					AddErrorRow(item.CadastralNumber,$"Не найдено значение справочника ЦОД у объекта {gbuObject.CadastralNumber}", reportService);
-					return;
+					var propertyTypeValues = currentUnitsPartition.Select(x => x.PropertyType).Distinct().ToList();
+					var codeGroupValues = codeGroups.Values.Where(x => !string.IsNullOrEmpty(x.Value)).Select(x => x.Value).Distinct().ToList();
+					allComplianceGuides = OMComplianceGuide.Where(x => codeGroupValues.Contains(x.Code) && propertyTypeValues.Contains(x.TypeProperty))
+						.Select(x => new{x.SubGroup, x.TypeProperty, x.Code}).Execute();
 				}
+				Logger.Verbose("Получение значений из таблицы соответствий кода и группы");
 
-				var complianceGuides = GetComplianceGuides(OMComplianceGuide.Where(x => x.Code == codeGroup.Value && x.TypeProperty == item.PropertyType).SelectAll().Execute());
+				var codeQuarters = GetValueFactors(gbuObjects, attributeQuarter.RegisterId, attributeQuarter.Id);
+				Logger.Verbose("Получение значений атрибута кадастровый квартал ГБУ объектов");
 
-				if (complianceGuides.Count == 1)
+				var gbuQuarterObjects = new List<OMMainObject>();
+				if (codeQuarters.Values.Any(x => !string.IsNullOrEmpty(x.Value)))
 				{
-                    var value = complianceGuides[0].Group;
-                    AddValueFactor(gbuObject, estimatedSubGroupAttribute.Id, codeGroup.IdDocument, DateTime.Now, value);
-					AddRowToReport(item.CadastralNumber, estimatedSubGroupAttribute.Id, codeGroupAttribute.Id, value, reportService);
-					return;
+					var codeQuartersValues = codeQuarters.Values.Where(x => !string.IsNullOrEmpty(x.Value)).Select(x => x.Value).Distinct().ToList();
+					gbuQuarterObjects = OMMainObject.Where(x => codeQuartersValues.Contains(x.CadastralNumber)).Select(x => x.CadastralNumber).Execute();
 				}
+				Logger.ForContext("GbuQuarterObjectsCount", gbuQuarterObjects.Count)
+					.ForContext("GbuObjectsCount", gbuObjects.Count)
+					.Verbose("Получение ГБУ объектов кадастровых кварталов");
 
-				if (complianceGuides.Count <= 1)
-				{
-					AddErrorRow(item.CadastralNumber, $"Не найдено значение в таблице сопоставления {gbuObject.CadastralNumber}", reportService);
-					return;
-				}
-				{
-					var attributeQuarter = RegisterCache.GetAttributeData((int)param.IdCodeQuarter);
-					// берем кадастровый квартал
-					ValueItem codeQuarter = GetValueFactor(gbuObject, attributeQuarter.RegisterId, attributeQuarter.Id);
+				var territoryTypes = GetValueFactors(gbuQuarterObjects, attributeTerritoryType.RegisterId, attributeTerritoryType.Id);
+				Logger.Verbose("Получение значений атрибута тип территории ГБУ объектов кадастровых кварталов");
 
-					if (string.IsNullOrEmpty(codeQuarter.Value))
+				Logger.ForContext("CurrentHandledCount", CurrentCount)
+					.ForContext("UnitPartitionCount", currentUnitsPartition.Count)
+					.ForContext("CountAllUnits", CountAllUnits)
+					.Debug("Обработка каждого юнита");
+				Parallel.ForEach(currentUnitsPartition, options, item =>
+				{
+					lock (locked)
 					{
-						AddErrorRow(item.CadastralNumber, $"Не найден кадастровый квартал для объекта {gbuObject.CadastralNumber}.", reportService);
+						CurrentCount++;
+					}
+
+					var gbuObject = gbuObjects.FirstOrDefault(x => x.Id == item.ObjectId);
+					if (gbuObject == null)
+					{
+						AddErrorRow(item.CadastralNumber, $"Не найден объект для единицы оценки {item.CadastralNumber}", reportService);
 						return;
 					}
 
-					var gbuQuarterObject = OMMainObject.Where(x => x.CadastralNumber == codeQuarter.Value).SelectAll().ExecuteFirstOrDefault();
-
-					if (gbuQuarterObject == null)
+					codeGroups.TryGetValue(gbuObject.Id, out var codeGroup);
+					if (string.IsNullOrEmpty(codeGroup.Value))
 					{
-						AddErrorRow(item.CadastralNumber, $"Не найден объект кадастровый квартал {codeQuarter.Value} .", reportService);
-						return;
-					}
-					var attributeTerritoryType = RegisterCache.GetAttributeData((int)param.IdTerritoryType);
-
-					ValueItem territoryType = GetValueFactor(gbuQuarterObject, attributeTerritoryType.RegisterId, attributeTerritoryType.Id);
-
-					if (string.IsNullOrEmpty(territoryType.Value))
-					{
-						AddErrorRow(item.CadastralNumber, $"Не найден тип территории для объекта {gbuObject.CadastralNumber}", reportService);
+						AddErrorRow(item.CadastralNumber, $"Не найдено значение справочника ЦОД у объекта {gbuObject.CadastralNumber}", reportService);
 						return;
 					}
 
-					var complianceGuid = complianceGuides
-						.FirstOrDefault(x => x.SubGroup.ToString() == territoryType.Value);
+					var complianceGuides = GetComplianceGuides(allComplianceGuides.Where(x => x.Code == codeGroup.Value && x.TypeProperty == item.PropertyType).ToList());
 
-					if (string.IsNullOrEmpty(complianceGuid.Code))
+					if (complianceGuides.IsEmpty())
 					{
-						AddErrorRow(item.CadastralNumber, $"Не найдено значение в таблице сопоставления, для кода {codeGroup.Value} и подгруппы {territoryType.Value}", reportService);
-						return;
+						AddErrorRow(item.CadastralNumber, $"Не найдено значение в таблице сопоставления {gbuObject.CadastralNumber}", reportService);
+					} 
+					else if (complianceGuides.Count == 1)
+					{
+						var value = complianceGuides[0].Group;
+						AddValueFactor(gbuObject, estimatedSubGroupAttribute.Id, codeGroup.IdDocument, DateTime.Now, value);
+						AddRowToReport(item.CadastralNumber, estimatedSubGroupAttribute.Id, codeGroupAttribute.Id, value, reportService);
+					} 
+					else
+					{
+						codeQuarters.TryGetValue(gbuObject.Id, out var codeQuarter);
+						if (string.IsNullOrEmpty(codeQuarter.Value))
+						{
+							AddErrorRow(item.CadastralNumber, $"Не найден кадастровый квартал для объекта {gbuObject.CadastralNumber}.", reportService);
+							return;
+						}
+
+						var gbuQuarterObject = gbuQuarterObjects.FirstOrDefault(x => x.CadastralNumber == codeQuarter.Value);
+						if (gbuQuarterObject == null)
+						{
+							AddErrorRow(item.CadastralNumber, $"Не найден объект кадастровый квартал {codeQuarter.Value} .", reportService);
+							return;
+						}
+
+						territoryTypes.TryGetValue(gbuQuarterObject.Id, out var territoryType);
+						if (string.IsNullOrEmpty(territoryType.Value))
+						{
+							AddErrorRow(item.CadastralNumber, $"Не найден тип территории для объекта {gbuObject.CadastralNumber}", reportService);
+							return;
+						}
+
+						var complianceGuid = complianceGuides
+							.FirstOrDefault(x => x.SubGroup.ToString() == territoryType.Value);
+
+						if (string.IsNullOrEmpty(complianceGuid.Code))
+						{
+							AddErrorRow(item.CadastralNumber, $"Не найдено значение в таблице сопоставления, для кода {codeGroup.Value} и подгруппы {territoryType.Value}", reportService);
+							return;
+						}
+						var value = complianceGuid.Group;
+						AddValueFactor(gbuObject, estimatedSubGroupAttribute.Id, codeGroup.IdDocument, DateTime.Now, value);
+						AddRowToReport(item.CadastralNumber, estimatedSubGroupAttribute.Id, codeGroupAttribute.Id, value, reportService);
 					}
-					var value = complianceGuid.Group;
-					AddValueFactor(gbuObject, estimatedSubGroupAttribute.Id, codeGroup.IdDocument, DateTime.Now, value);
-					AddRowToReport(item.CadastralNumber, estimatedSubGroupAttribute.Id, codeGroupAttribute.Id, value, reportService);
-				}
-			});
+				});
+			}
 
 			long reportId = reportService.SaveReport();
 
@@ -180,7 +236,7 @@ namespace KadOzenka.Dal.KoObject
 
 		#region Help Methods
 
-		private static void AddValueFactor(OMMainObject mObject, long? idFactor, long? idDoc, DateTime date, string value)
+		private void AddValueFactor(OMMainObject mObject, long? idFactor, long? idDoc, DateTime date, string value)
 		{
 			var attributeValue = new GbuObjectAttribute
 			{
@@ -194,38 +250,40 @@ namespace KadOzenka.Dal.KoObject
 				Ot = date.Date.Date,
 				StringValue = value,
 			};
-			var id = attributeValue.Save();
-			if (id != 0)
-			{
-				lock(locked)
-				{
-					SuccessCount++;
-				}
-            }
-        }
-
-		private static ValueItem GetValueFactor(OMMainObject obj, long idRegister, long idFactor)
-		{
-			ValueItem res = new ValueItem
-			{
-				Value = string.Empty,
-				IdDocument = null,
-			};
-
-			List<GbuObjectAttribute> attribs = new GbuObjectService().GetAllAttributes(obj.Id, new List<long> { idRegister }, new List<long> { idFactor }, DateTime.Now.Date);
-			if (attribs.Count > 0)
-			{
-				if (attribs[0].GetValueInString() != string.Empty && attribs[0].GetValueInString() != null)
-				{
-					res.Value = attribs[0].GetValueInString();
-					res.IdDocument = attribs[0].ChangeDocId;
-				}
-			}
-
-			return res;
+			attributeValue.Save();
 		}
 
-		private static List<ComplianceGuid> GetComplianceGuides(List<OMComplianceGuide> complianceGuides)
+		private Dictionary<long, ValueItem> GetValueFactors(List<OMMainObject> objs, long idRegister, long idFactor)
+		{
+			var result = new Dictionary<long, ValueItem>();
+
+			List<GbuObjectAttribute> attribs = new GbuObjectService().GetAllAttributes(objs.Select(x => x.Id).ToList(), new List<long> { idRegister }, new List<long> { idFactor }, DateTime.Now.Date);
+			foreach (var mainObject in objs)
+			{
+				ValueItem res = new ValueItem
+				{
+					Value = string.Empty,
+					IdDocument = null,
+				};
+
+				var objAttr = attribs.Where(x => x.ObjectId == mainObject.Id).ToList();
+				if (objAttr.Count > 0)
+				{
+					var valueInString = attribs[0].GetValueInString();
+					if (!string.IsNullOrEmpty(valueInString))
+					{
+						res.Value = valueInString;
+						res.IdDocument = attribs[0].ChangeDocId;
+					}
+				}
+
+				result.Add(mainObject.Id, res);
+			}
+
+			return result;
+		}
+
+		private List<ComplianceGuid> GetComplianceGuides(List<OMComplianceGuide> complianceGuides)
 		{
 			var res = new List<ComplianceGuid>();
 
@@ -239,7 +297,7 @@ namespace KadOzenka.Dal.KoObject
 			return res;
 		}
 
-		public static void AddErrorRow(string kn, string value, GbuReportService reportService)
+		private void AddErrorRow(string kn, string value, GbuReportService reportService)
 		{
 			lock (locked)
 			{
@@ -249,7 +307,7 @@ namespace KadOzenka.Dal.KoObject
 			}
 		}
 
-		public static void AddRowToReport(string kn, long inputAttributeId, long sourceAttributeId, string value, GbuReportService reportService)
+		private void AddRowToReport(string kn, long inputAttributeId, long sourceAttributeId, string value, GbuReportService reportService)
 		{
 			lock (locked)
 			{
