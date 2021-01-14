2260cd8b83327245ffba4bb872dec80047ed552d
bozhko <bozhko@corepartners.ru>
2021-01-14 13:00:56 +0300

    Статистика по минимальным, максимальным и средним (арифметическое и средневзвешенное) УПКС/УПРС в разрезе кадастровых кварталов

--- Source/KadOzenka.Dal/ManagementDecisionSupport/StatisticalData/MinMaxAverageUPKSByCadastralQuartersService.cs
+++ Source/KadOzenka.Dal/ManagementDecisionSupport/StatisticalData/MinMaxAverageUPKSByCadastralQuartersService.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.IO;
 using Core.Register.QuerySubsystem;
+using KadOzenka.Dal.CancellationQueryManager;
 using KadOzenka.Dal.ManagementDecisionSupport.Dto.StatisticalData;
 using KadOzenka.Dal.Registers.GbuRegistersServices;
 
@@ -8,12 +9,14 @@ namespace KadOzenka.Dal.ManagementDecisionSupport.StatisticalData
 {
 	public class MinMaxAverageUPKSByCadastralQuartersService
 	{
+		public CancellationManager CancellationManager;
 		private readonly StatisticalDataService _statisticalDataService;
 		private readonly GbuCodRegisterService _gbuCodRegisterService;
 		private readonly string _sqlFileName = "MinMaxAverageUPKSByCadastralQuarters";
 
         public MinMaxAverageUPKSByCadastralQuartersService(StatisticalDataService statisticalDataService, GbuCodRegisterService gbuCodRegisterService)
 		{
+			CancellationManager = new CancellationManager();
 			_statisticalDataService = statisticalDataService;
 			_gbuCodRegisterService = gbuCodRegisterService;
 		}
@@ -26,7 +29,7 @@ namespace KadOzenka.Dal.ManagementDecisionSupport.StatisticalData
 		        contents = sr.ReadToEnd();
             }
 
-	        var result = QSQuery.ExecuteSql<MinMaxAverageUPKSByCadastralQuartersDto>(string.Format(contents, string.Join(", ", taskList), _gbuCodRegisterService.GetCadastralQuarterFinalAttribute().Id));
+	        var result = CancellationManager.ExecuteSql<MinMaxAverageUPKSByCadastralQuartersDto>(string.Format(contents, string.Join(", ", taskList), _gbuCodRegisterService.GetCadastralQuarterFinalAttribute().Id));
 
 	        return result;
         }
