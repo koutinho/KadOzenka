884d299061932875d6628b7d938e0ff53d27142d
Korotkova Ekaterina <korotkova@corepartners.ru>
2021-01-28 14:15:47 +0300

    CIPJSKO-723: добавлен тест на partial OMModel

---
+++ Source/KadOzenka.Dal.Tests/Modeling/OmModelTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using ObjectModel.Directory;
+using ObjectModel.KO;
+
+namespace KadOzenka.Dal.Tests.Modeling
+{
+	[TestFixture]
+	public class OmModelTests
+	{
+		[Test]
+		[TestCase(KoAlgoritmType.Line, 1, 2, 3, 1)]
+		[TestCase(KoAlgoritmType.Exp, 1, 2, 3, 2)]
+		[TestCase(KoAlgoritmType.Multi, 1, 2, 3, 3)]
+		public void Check_A0_For_Model(KoAlgoritmType type, decimal line, decimal exp, decimal mult,
+			decimal expected)
+		{
+			var model = new OMModel
+			{
+				AlgoritmType_Code = type,
+				A0 = line,
+				A0ForExponential = exp,
+				A0ForMultiplicative = mult
+			};
+
+			Assert.That(model.GetA0(), Is.EqualTo(expected));
+		}
+	}
+}
