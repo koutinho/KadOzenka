0b024fc54fbcdec4633014151f658306f4afc074
Korotkova Ekaterina <korotkova@corepartners.ru>
2021-01-28 09:51:51 +0300

    CIPJSKO-723: добавлен проект для тестов

---
+++ Source/KadOzenka.Dal.Tests/Test.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace KadOzenka.Dal.Tests
+{
+	[TestFixture]
+	public class Test
+	{
+		[Test]
+		[TestCase(0, 0, 0)]
+		public void AddNumbersTest(int a, int b, int expected)
+		{
+			var result = a + b;
+
+			Assert.That(result, Is.EqualTo(expected));
+		}
+    }
+}
