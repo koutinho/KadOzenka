a4d157ed40fbc8da5d8a86a41aa634b3663ef35f
Korotkova Ekaterina <korotkova@corepartners.ru>
2021-02-24 16:42:14 +0300

    CIPJSKO-747: js метод вынесен в общий файл

---
+++ Source/KadOzenka.Web/wwwroot/js/common-reports.js
@@ -0,0 +1,26 @@
+﻿function processReport() {
+	kendo.ui.progress($('#getReportForm'), true);
+	var form = $('#getReportForm');
+	var formObject = Common.Functions.FormToObject(form);
+	$.ajax({
+		type: form.attr('method'),
+		url: form.attr('action'),
+		data: formObject,
+		traditional: true,
+		success: function (response) {
+			if (response.Errors) {
+				var errors = getErrors(response.Errors);
+				Common.ShowError(errors);
+				return;
+			} else {
+				Common.ShowMessage("Процесс добавлен в очередь. Результат будет отправлен на почту.");
+			}
+		},
+		error: function (response) {
+			Common.ShowError(response.responseText);
+		},
+		complete: function () {
+			kendo.ui.progress($('#getReportForm'), false);
+		}
+	});
+}
