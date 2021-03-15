e1ef92ccd4a8f69944bc24378bfcf054f4039f75
Aleksandr Korotkov <aleksandr.korotkov@simbirsoft.com>
2021-03-15 12:00:55 +0400

    CIPJSKO-790 Реестровые вьюхи

---
+++ Source/KadOzenka.Web/Controllers/UnloadResultsController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Core.Shared.Extensions;
+using Core.SRD;
+using KadOzenka.Dal.DataExport;
+using KadOzenka.Dal.GbuObject;
+using Microsoft.AspNetCore.Mvc;
+using KadOzenka.Web.Attributes;
+using KadOzenka.Web.Models.GeneralReports;
+using KadOzenka.Web.Models.UnloadResults;
+
+namespace KadOzenka.Web.Controllers
+{
+	public class UnloadResultsController : KoBaseController
+	{
+		public UnloadResultsController()
+		{
+		}
+
+
+		[SRDFunction(Tag = "")]
+		public FileResult DownloadResult(long resultFileId)
+		{
+			var fileName = UnloadResultStorageManager.GetUnloadResultFileName(resultFileId);
+			var content = UnloadResultStorageManager.GetUnloadResultFileById(resultFileId);
+			var contentType = UnloadResultStorageManager.GetUnloadResultContentType(resultFileId);
+			return File(content, contentType, fileName);
+		}
+
+		[HttpGet]
+		[SRDFunction(Tag = "")]
+		public IActionResult UnloadResultsModal(long? resultFileId)
+		{
+			var model = new UnloadResultsDownloadModel();
+			if (!resultFileId.HasValue)
+			{
+				model.FileExists = false;
+			}
+			else
+			{
+				var id = resultFileId.Value;
+				var exists = UnloadResultStorageManager.CheckIfFileExists(id);
+				var fileName = UnloadResultStorageManager.GetUnloadResultFileName(id);
+				model = new UnloadResultsDownloadModel {FileExists = exists, FileName = fileName, Id = id};
+			}
+
+			return View(model);
+		}
+	}
+}
