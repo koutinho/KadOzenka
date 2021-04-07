df984a8942094df44098e9e847b1b87c85f47a0e
Mitryushina Natalya <mitryushina@corepartners.ru>
2021-04-07 11:08:54 +0300

    CIPJSKO-830 фикс ошибки с логическим удалением для реестров 930,931,986,988

---
+++ Source/DbMigrations/3029/down.sql
@@ -0,0 +1,4 @@
+﻿DELETE FROM common_registers_with_soft_deletion WHERE register_id=930 AND main_table_name='CORE_REGISTER';	
+DELETE FROM common_registers_with_soft_deletion WHERE register_id=931 AND main_table_name='CORE_REGISTER_ATTRIBUTE';	
+DELETE FROM common_registers_with_soft_deletion WHERE register_id=986 AND main_table_name='CORE_ATTACHMENT';	
+DELETE FROM common_registers_with_soft_deletion WHERE register_id=988 AND main_table_name='CORE_ATTACHMENT_OBJECT';
