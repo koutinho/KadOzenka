e38180112e7a2623240cf8db322fbed37fbec7ba
Aleksandr Korotkov <aleksandr.korotkov@simbirsoft.com>
2021-04-08 14:22:44 +0400

    CIPJSKO-829 Вынос формирования кнопки справки для тулбаров в отдельный скрипт

---
+++ Source/KadOzenka.Web/wwwroot/js/toolbars.js
@@ -0,0 +1,16 @@
+﻿function helperButtonForToolbar(){
+    let path = window.location.pathname;
+    let splitPath = path.split('/');
+    let controller = splitPath[1];
+    let action = splitPath[2] || '';
+    let url = `/Help/Help?CurrentLocation=/${controller}`;
+    if (action!=='') url = url+ `/${action}`;
+    return {
+        type: 'button',
+        id: 'helpButton',
+        text: '',
+        icon: 'question',
+        url: url,
+        attributes: {'target': '_blank'}
+    };
+}
