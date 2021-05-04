0fe95f9c858700c401d2b78d4ea3ab6f77bbe17e
Korotkova Ekaterina <korotkova@corepartners.ru>
2021-05-04 11:47:51 +0300

    KOMO-29: правки сбора данных для моделирования

--- Source/MarketPlaceBusiness/BaseService.cs
+++ Source/MarketPlaceBusiness/BaseService.cs
@@ -1,9 +1,11 @@
-﻿using ObjectModel.Market;
+﻿using Core.Register.QuerySubsystem;
+using ObjectModel.Market;
 
 namespace MarketPlaceBusiness
 {
 	public class BaseService
 	{
 		public static readonly long RegisterId = OMCoreObject.GetRegisterId();
+		public static readonly QSColumn PrimaryKeyColumn = OMCoreObject.GetColumn(x => x.Id);
 	}
 }
