8d3ebddd45a28c6746320c86d9f840650ac71cfb
Korotkova Ekaterina <korotkova@corepartners.ru>
2021-05-04 11:39:18 +0300

    KOMO-29: правки для моделирования

--- Source/KadOzenka.Dal/LongProcess/Modeling/BaseObjectFormationForModelingProcess.cs
+++ Source/KadOzenka.Dal/LongProcess/Modeling/BaseObjectFormationForModelingProcess.cs
@@ -9,6 +9,7 @@ using Core.Shared.Extensions;
 using ExCSS.Model.Extensions;
 using KadOzenka.Dal.LongProcess.Modeling.Entities;
 using KadOzenka.Dal.Modeling;
+using MarketPlaceBusiness;
 using Microsoft.Practices.EnterpriseLibrary.Data;
 using Microsoft.Practices.ObjectBuilder2;
 using ObjectModel.Core.LongProcess;
@@ -57,7 +58,7 @@ namespace KadOzenka.Dal.LongProcess.Modeling
 			if (modelAttributes == null || modelAttributes.Count == 0 || objectIds == null || objectIds.Count == 0)
 				return new Dictionary<long, List<CoefficientForObject>>();
 
-			return GetCoefficients(OMCoreObject.GetRegisterId(), OMCoreObject.GetColumn(x => x.Id), objectIds, dictionaries, modelAttributes);
+			return GetCoefficients(MarketObjectsForModelingService.RegisterId, OMCoreObject.GetColumn(x => x.Id), objectIds, dictionaries, modelAttributes);
 		}
 
 		protected Dictionary<long, List<CoefficientForObject>> GetCoefficientsFromTourFactors(List<long> unitIds, List<OMModelingDictionary> dictionaries,
--- Source/KadOzenka.Dal/LongProcess/Modeling/FactorAdditionToModelObjectsLongProcess.cs
+++ Source/KadOzenka.Dal/LongProcess/Modeling/FactorAdditionToModelObjectsLongProcess.cs
@@ -8,6 +8,7 @@ using Core.Shared.Extensions;
 using KadOzenka.Dal.LongProcess.Modeling.Entities;
 using KadOzenka.Dal.Modeling;
 using KadOzenka.Dal.Modeling.Dto;
+using MarketPlaceBusiness;
 using ObjectModel.Core.LongProcess;
 using ObjectModel.KO;
 using ObjectModel.Market;
@@ -70,7 +71,7 @@ namespace KadOzenka.Dal.LongProcess.Modeling
 				AddLog(Queue, $"Найдено {objects.Count} объектов Модели.", logger: Logger);
 
 				AddLog(Queue, "Начат сбор коэффициентов.", logger: Logger);
-				var coefficients = Attribute.RegisterId == OMCoreObject.GetRegisterId()
+				var coefficients = Attribute.RegisterId == MarketObjectsForModelingService.RegisterId
 					? GetCoefficientsFromMarketObject(objects.Select(x => x.MarketObjectId.GetValueOrDefault()).ToList(), dictionaries, attributes)
 					: GetCoefficientsFromTourFactors(objects.Where(x => x.UnitId != null).Select(x => x.UnitId.Value).Distinct().ToList(), dictionaries, attributes);
 				AddLog(Queue, "Закончен сбор коэффициентов.", logger: Logger);
@@ -105,7 +106,7 @@ namespace KadOzenka.Dal.LongProcess.Modeling
 
 			for (var i = 0; i < objects.Count; i++)
 			{
-				var objectId = Attribute.RegisterId == OMCoreObject.GetRegisterId() ? objects[i].MarketObjectId : objects[i].UnitId;
+				var objectId = Attribute.RegisterId == MarketObjectsForModelingService.RegisterId ? objects[i].MarketObjectId : objects[i].UnitId;
 				coefficientsToObject.TryGetValue(objectId.GetValueOrDefault(), out var coefficients);
 				var coefficient = coefficients?.FirstOrDefault();
 
