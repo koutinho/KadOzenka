48c8b9d382059d78d093f069fd5ab224e61444e5
Korotkova Ekaterina <korotkova@corepartners.ru>
2021-05-06 11:10:20 +0300

    KOMO-33: убраны неискользуемые юзинги

--- Source/MarketPlaceBusiness/MarketObjectsForMapService.cs
+++ Source/MarketPlaceBusiness/MarketObjectsForMapService.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Core.Register.QuerySubsystem;
+﻿using Core.Register.QuerySubsystem;
 using MarketPlaceBusiness.Interfaces;
 using ObjectModel.Directory;
 using ObjectModel.Market;
