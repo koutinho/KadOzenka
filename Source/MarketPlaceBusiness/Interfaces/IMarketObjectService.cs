d9141c387c36651069a712aa4cf91214d09a96ee
Korotkova Ekaterina <korotkova@corepartners.ru>
2021-05-12 16:45:28 +0300

    KOMO-41: сервисы для удаленных процедур перемещены в отдельную папку

--- Source/MarketPlaceBusiness/Interfaces/IMarketObjectService.cs
+++ Source/MarketPlaceBusiness/Interfaces/IMarketObjectService.cs
@@ -6,9 +6,9 @@ namespace MarketPlaceBusiness.Interfaces
 {
 	public interface IMarketObjectService : IAMarketObjectBaseService
 	{
-		List<OMCoreObject> GetObjectsForDuplicatesChecking();
-		List<List<OMCoreObject>> SplitListByPersentForDuplicates(List<OMCoreObject> list, double areaDelta, double priceDelta,
-			int selectedMarket);
+		//List<OMCoreObject> GetObjectsForDuplicatesChecking();
+		//List<List<OMCoreObject>> SplitListByPersentForDuplicates(List<OMCoreObject> list, double areaDelta, double priceDelta,
+		//	int selectedMarket);
 
 		List<OMCoreObject> GetObjectsToAssignDistrictsRegionsAndZones();
 		List<OMCoreObject> GetObjectsToAssignCoordinates();
