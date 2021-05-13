8fca1a4fd263a3870e6b0119e4bb9e3b8fc5b7cc
Korotkova Ekaterina <korotkova@corepartners.ru>
2021-05-13 14:12:48 +0300

    KOMO-42: удалены колонки: Вид передаваемых прав, Вид права, Год постройки

--- Source/DbMigrations/KOMO_42/up.sql
+++ Source/DbMigrations/KOMO_42/up.sql
@@ -12,3 +12,13 @@ ALTER TABLE market_core_object DROP COLUMN DISTRICT;
 
 delete from core_register_attribute where id = 10009001;
 delete from core_register_attribute where id = 10008700;
+
+delete from core_register_attribute where id = 10007800;
+ALTER TABLE market_core_object DROP COLUMN PROPERTY_LAW_TYPE;
+
+delete from core_register_attribute where id = 10009002;
+ALTER TABLE market_core_object DROP COLUMN OWNERSHIP_TYPE;
+
+delete from core_register_attribute where id = 10004700;
+ALTER TABLE market_core_object DROP COLUMN BUILDING_YEAR;
+
