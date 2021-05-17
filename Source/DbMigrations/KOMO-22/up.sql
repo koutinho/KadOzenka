5d6bb92b029289c5985fa91ef78b0677aaf4a213
Aleksandr Korotkov <aleksandr.korotkov@simbirsoft.com>
2021-05-17 15:25:52 +0400

    KOMO-22 Скрипты для добавления записи о втором этапе нормализации в перечисление

---
+++ Source/DbMigrations/KOMO-22/up.sql
@@ -0,0 +1,9 @@
+update core_reference
+set simple_values = (
+    with sel as (select jsonb_array_elements(regexp_replace(simple_values, ',\n\]', ']')::jsonb
+        || '{"Id": 10,"Value":"Финализация нормализации":"NormalisationFinal" }') val
+                 from core_reference
+                 where referenceid = 801
+    )
+    select jsonb_pretty(jsonb_agg(val) ) from sel)
+where referenceid = 801;
