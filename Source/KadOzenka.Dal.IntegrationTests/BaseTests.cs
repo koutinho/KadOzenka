4333c46d6c5f10fcff2edc71b372db31c3844790
Korotkova Ekaterina <korotkova@corepartners.ru>
2021-05-26 10:26:59 +0300

    KOMO-64: добавлен тест на обновление модели

---
+++ Source/KadOzenka.Dal.IntegrationTests/BaseTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace KadOzenka.Dal.IntegrationTests
+{
+	public class BaseTests
+	{
+		[OneTimeSetUp]
+		public void OneTimeSetUp()
+		{
+			//TODO создание БД
+		}
+	}
+}
