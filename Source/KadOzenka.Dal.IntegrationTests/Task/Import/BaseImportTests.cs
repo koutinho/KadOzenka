702c4ac5f1335bc3e5cf4c2826ea0fc635a8130b
Korotkova Ekaterina <korotkova@corepartners.ru>
2021-05-27 09:20:36 +0300

    KOMO-60: добавлены тесты на импорт из Excel

--- Source/KadOzenka.Dal.IntegrationTests/Task/Import/BaseImportTests.cs
+++ Source/KadOzenka.Dal.IntegrationTests/Task/Import/BaseImportTests.cs
@@ -1,9 +1,10 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading;
 using Core.SRD;
 using KadOzenka.Dal.DataImport;
-using KadOzenka.Dal.IntegrationTests.Task._Builders;
+using KadOzenka.Dal.DataImport.DataImporterGknNew.Attributes;
 using NUnit.Framework;
 using ObjectModel.Common;
 using ObjectModel.Core.LongProcess;
@@ -13,40 +14,13 @@ using ObjectModel.KO;
 
 namespace KadOzenka.Dal.IntegrationTests.Task.Import
 {
-	public class ImportViaXmlTests : BaseTaskTests
+	public class BaseImportTests : BaseTaskTests
 	{
-		[Test]
-		public void Can_Import_Task_Document_Via_Xml()
-		{
-			var fileName = "one_building_success.xml";
-			var task = new TaskBuilder().Document(Document.Id).Build();
-
-			StartImport(task.Id, fileName);
-
-			CheckDataLog(task.Id, 1, 1);
-			CheckCreatedUnits(task.Id, 1);
-		}
-
-		[Test]
-		public void If_CanNot_Parse_Data_From_File_So_Include_Object_In_Total_Counter()
-		{
-			var fileName = "two_buildings_error.xml";
-			var task = new TaskBuilder().Document(Document.Id).Build();
-
-			StartImport(task.Id, fileName);
-
-			CheckDataLog(task.Id, 2, 1);
-			CheckCreatedUnits(task.Id, 1);
-		}
-
-		
-		#region Support Methods
-
-		private void StartImport(long taskId, string fileName)
+		protected void StartImport(long taskId, string fileName, List<ColumnToAttributeMapping> columnsMapping = null)
 		{
 			using (var stream = File.OpenRead($"{PathToFileFolder}{fileName}"))
 			{
-				var import = DataImporterGknLongProcess.SaveImportDataLog(fileName, stream, taskId);
+				var import = DataImporterGknLongProcess.SaveImportDataLog(fileName, stream, taskId, columnsMapping);
 
 				new DataImporterGknLongProcess().StartProcess(new OMProcessType(),
 					new OMQueue
@@ -58,7 +32,7 @@ namespace KadOzenka.Dal.IntegrationTests.Task.Import
 			}
 		}
 
-		private void CheckDataLog(long taskId, int totalObjectsCount, int importedObjectsCount)
+		protected void CheckDataLog(long taskId, int totalObjectsCount, int importedObjectsCount)
 		{
 			var dataLog = OMImportDataLog.Where(x => x.ObjectId == taskId).SelectAll().ExecuteFirstOrDefault();
 			Assert.That(dataLog.Status_Code, Is.EqualTo(ObjectModel.Directory.Common.ImportStatus.Completed), dataLog.ResultMessage);
@@ -66,13 +40,11 @@ namespace KadOzenka.Dal.IntegrationTests.Task.Import
 			Assert.That(dataLog.NumberOfImportedObjects, Is.EqualTo(importedObjectsCount));
 		}
 
-		private void CheckCreatedUnits(long taskId, int numberOfUnits)
+		protected void CheckCreatedUnits(long taskId, int numberOfUnits, PropertyTypes firstUnitType)
 		{
 			var units = OMUnit.Where(x => x.TaskId == taskId).SelectAll().Execute();
 			Assert.That(units.Count, Is.EqualTo(numberOfUnits));
-			Assert.That(units.First().PropertyType_Code, Is.EqualTo(PropertyTypes.Building));
+			Assert.That(units.First().PropertyType_Code, Is.EqualTo(firstUnitType));
 		}
-
-		#endregion
 	}
 }
---
+++ Source/KadOzenka.Dal.IntegrationTests/Task/Import/ExcelTests.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using KadOzenka.Dal.DataImport.DataImporterGknNew;
+using KadOzenka.Dal.DataImport.DataImporterGknNew.Attributes;
+using KadOzenka.Dal.IntegrationTests.Task._Builders;
+using NUnit.Framework;
+using ObjectModel.Directory;
+
+namespace KadOzenka.Dal.IntegrationTests.Task.Import
+{
+	public class ExcelTests : BaseImportTests
+	{
+		[Test]
+		public void Can_Import_Task_Document()
+		{
+			var fileName = "one_parcel_success.xlsx";
+			var columnsMapping = GetColumnsMapping();
+			var task = new TaskBuilder().Document(Document.Id).Build();
+
+			StartImport(task.Id, fileName, columnsMapping);
+
+			CheckDataLog(task.Id, 1, 1);
+			CheckCreatedUnits(task.Id, 1, PropertyTypes.Stead);
+		}
+
+		[Test]
+		public void If_CanNot_Parse_Data_From_File_So_Include_Object_In_Total_Counter()
+		{
+			var fileName = "two_parcels_error.xlsx";
+			var columnsMapping = GetColumnsMapping();
+			var task = new TaskBuilder().Document(Document.Id).Build();
+
+			StartImport(task.Id, fileName, columnsMapping);
+
+			CheckDataLog(task.Id, 2, 1);
+			CheckCreatedUnits(task.Id, 1, PropertyTypes.Stead);
+		}
+
+		
+		#region Support Methods
+
+		private List<ColumnToAttributeMapping> GetColumnsMapping()
+		{
+			return new List<ColumnToAttributeMapping>
+			{
+				new ColumnToAttributeMapping
+					{AttributeId = RequiredFieldsForExcelMapping.CadastralNumberAttributeId, ColumnIndex = 0},
+				new ColumnToAttributeMapping
+					{AttributeId = RequiredFieldsForExcelMapping.ObjectTypeAttributeId, ColumnIndex = 1},
+				new ColumnToAttributeMapping
+					{AttributeId = RequiredFieldsForExcelMapping.AssessmentDateAttributeId, ColumnIndex = 2},
+				new ColumnToAttributeMapping
+					{AttributeId = RequiredFieldsForExcelMapping.SquareAttributeId, ColumnIndex = 3},
+				new ColumnToAttributeMapping {AttributeId = Consts.CadastralCostDocDateAttributeId, ColumnIndex = 5}
+			};
+		}
+
+		#endregion
+	}
+}
