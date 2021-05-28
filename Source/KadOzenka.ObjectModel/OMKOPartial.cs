5b6869874fe6169809ac4f7bb53939da67747ae1
Korotkova Ekaterina <korotkova@corepartners.ru>
2021-05-28 14:54:28 +0300

    KOMO-54: убрана проверка совпадения КК для операции "Актуализация кадастровых данных".

--- Source/KadOzenka.ObjectModel/OMKOPartial.cs
+++ Source/KadOzenka.ObjectModel/OMKOPartial.cs
@@ -597,7 +597,7 @@ namespace ObjectModel.KO
 		            .ForContext("PrevUnitId", lastUnit.Id)
 		            .Verbose("Найдена предыдущая единица оценки для {UnitCadastralNumber}", this.CadastralNumber);
                 //if (lastUnit.CadastralBlock == this.CadastralBlock)
-                {
+                //{
                     List<ObjectModel.KO.OMFactorSettings> oldfactors = ObjectModel.KO.OMFactorSettings.Where(x => x.Inheritance_Code == ObjectModel.Directory.KO.FactorInheritance.ftKvartal).SelectAll().Execute();
                     log.ForContext("UnitId", this.Id)
 	                    .ForContext("FactorIds", oldfactors.Select(x => x.FactorId).ToArray())
@@ -608,15 +608,17 @@ namespace ObjectModel.KO
                     {
                         InheritedKOFactor(oldfactor.FactorId.ParseToLong(), lastUnit, log);
                     }
-                }
-                else
-                {
-	                log.ForContext("UnitId", this.Id)
-		                .ForContext("UnitCadastralQuarter", this.CadastralBlock)
-                        .ForContext("PrevUnitId", lastUnit.Id)
-                        .ForContext("PrevUnitCadastralQuarter", lastUnit.CadastralBlock)
-		                .Verbose("Кадастровый квартал у предыдущей единицы оценки не совпадает с текущим для {UnitCadastralNumber}", this.CadastralNumber);
-				}
+                //}
+                //else
+                //{
+	               // log.ForContext("UnitId", this.Id)
+		              //  .ForContext("UnitCadastralQuarter", this.CadastralBlock)
+		              //  .ForContext("PrevUnitId", lastUnit.Id)
+		              //  .ForContext("PrevUnitCadastralQuarter", lastUnit.CadastralBlock)
+		              //  .Verbose(
+			             //   "Кадастровый квартал у предыдущей единицы оценки не совпадает с текущим для {UnitCadastralNumber}",
+			             //   this.CadastralNumber);
+                //}
             }
             else
             {
