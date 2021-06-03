d6c2685ea04d26e7fd4a3977f0fffac7f01d54ed
Korotkova Ekaterina <korotkova@corepartners.ru>
2021-06-03 14:14:33 +0300

    KOMO-75: Обрезать значение для атрибута 1540

---
+++ Source/KadOzenka.Dal/ConfigurationManagers/KadOzenkaConfigManager/Models/ImportDocumentForTaskConfig.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace KadOzenka.Dal.ConfigurationManagers.KadOzenkaConfigManager.Models
+{
+	public class ImportDocumentForTaskConfig
+	{
+		public List<long> AttributesToTrimLength { get; set; }
+	}
+}
