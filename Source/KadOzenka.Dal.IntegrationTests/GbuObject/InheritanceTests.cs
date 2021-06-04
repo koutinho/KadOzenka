b7588889466bbfcb1e7e4c4226951ffe39b28b07
Korotkova Ekaterina <korotkova@corepartners.ru>
2021-06-05 01:29:38 +0300

    KOMO-53: рефакторинг тестов

--- Source/KadOzenka.Dal.IntegrationTests/GbuObject/InheritanceTests.cs
+++ Source/KadOzenka.Dal.IntegrationTests/GbuObject/InheritanceTests.cs
@@ -9,55 +9,65 @@ using KadOzenka.Dal.Integration._Builders.Task;
 using KadOzenka.Dal.Integration.GbuObject;
 using NUnit.Framework;
 using ObjectModel.Directory;
+using ObjectModel.Gbu;
+using ObjectModel.KO;
 
 namespace KadOzenka.Dal.IntegrationTests.GbuObject
 {
 	public class InheritanceTests : BaseGbuObjectTests
 	{
+		private OMTask _firstTask;
+		private OMMainObject _firstChildObject;
+		private OMMainObject _parentObject;
+		private OMUnit _unit;
+
+		[SetUp]
+		public void InheritanceSetUp()
+		{
+			var childType = PropertyTypes.Pllacement;
+			_firstTask = new TaskBuilder().Tour(Tour.Id).Document(FirstDocument.Id).Build();
+			_firstChildObject = new GbuObjectBuilder().Type(childType).Build();
+			_parentObject = new GbuObjectBuilder().Type(PropertyTypes.Building).Build();
+			_unit = new UnitBuilder().Task(_firstTask).Object(_firstChildObject).Type(childType).Build();
+		}
+
+
 		[Test]
 		public void Can_Inherit_BuildToFlat_From_One_Task_With_One_Unit()
 		{
-			var childType = PropertyTypes.Pllacement;
-			var task = new TaskBuilder().Tour(Tour.Id).Document(FirstDocument.Id).Build();
-			var childObject = new GbuObjectBuilder().Type(childType).Build();
-			var parentObject = new GbuObjectBuilder().Type(PropertyTypes.Building).Build();
-			var unit = new UnitBuilder().Task(task).Object(childObject).Type(childType).Build();
-
-			var dateForAttributes = unit.CreationDate.GetValueOrDefault().AddDays(-1);
+			var dateForAttributes = _unit.CreationDate.GetValueOrDefault().AddDays(-1);
 			var parentCadastralNumberAttribute = new GbuObjectAttributeBuilder()
 				.Attribute(EgrnAttributes.CadastralNumber.Id)
-				.Object(childObject.Id)
+				.Object(_firstChildObject.Id)
 				.OtAndSDates(dateForAttributes)
-				.Value(parentObject.CadastralNumber).Build();
-			var copyAttributeToBuilder = new GbuObjectAttributeBuilder().Object(parentObject.Id).OtAndSDates(dateForAttributes);
+				.Value(_parentObject.CadastralNumber).Build();
+			var copyAttributeToBuilder = new GbuObjectAttributeBuilder().Object(_parentObject.Id).OtAndSDates(dateForAttributes);
 			var firstAttributeCopyFrom = copyAttributeToBuilder.ShallowCopy().Attribute(EgrnAttributes.Address.Id).Build();
 			var secondAttributeCopyFrom = copyAttributeToBuilder.ShallowCopy().Attribute(EgrnAttributes.Square.Id).Build();
 			var firstAttributeCopyTo = EgrnAttributes.AddressOrLocation;
 			var secondAttributeCopyTo = EgrnAttributes.Square;
 
 
-			RunInheritance(new List<long> {task.Id}, parentCadastralNumberAttribute.AttributeId,
+			RunInheritance(new List<long> { _firstTask.Id}, parentCadastralNumberAttribute.AttributeId,
 				new List<long> { firstAttributeCopyFrom.AttributeId, secondAttributeCopyFrom.AttributeId },
 				new List<long> { firstAttributeCopyTo.Id, secondAttributeCopyTo.Id });
 
 
-			CheckCopiedAttribute(firstAttributeCopyTo, childObject.Id, firstAttributeCopyFrom);
-			CheckCopiedAttribute(secondAttributeCopyTo, childObject.Id, secondAttributeCopyFrom);
+			CheckCopiedAttribute(firstAttributeCopyTo, _firstChildObject.Id, firstAttributeCopyFrom);
+			CheckCopiedAttribute(secondAttributeCopyTo, _firstChildObject.Id, secondAttributeCopyFrom);
 		}
 
 		[Test]
 		public void Can_Inherit_BuildToFlat_From_One_Task_With_Several_Units()
 		{
 			var childType = PropertyTypes.Pllacement;
-			var task = new TaskBuilder().Tour(Tour.Id).Document(FirstDocument.Id).Build();
 			
 			var objectBuilder = new GbuObjectBuilder().Type(childType);
 			var firstChildObject = objectBuilder.ShallowCopy().Build();
 			var secondChildObject = objectBuilder.ShallowCopy().Build();
-			var parentObject = new GbuObjectBuilder().Type(PropertyTypes.Building).Build();
-			
+
 			var unitsCreationDate = DateTime.Now;
-			var unitBuilder = new UnitBuilder().Task(task).Type(childType).CreationDate(unitsCreationDate);
+			var unitBuilder = new UnitBuilder().Task(_firstTask).Type(childType).CreationDate(unitsCreationDate);
 			unitBuilder.Object(firstChildObject).ShallowCopy().Build();
 			unitBuilder.Object(secondChildObject).ShallowCopy().Build();
 
@@ -65,18 +75,18 @@ namespace KadOzenka.Dal.IntegrationTests.GbuObject
 			var parentCadastralNumberAttributeBuilder = new GbuObjectAttributeBuilder()
 				.Attribute(EgrnAttributes.CadastralNumber.Id)
 				.OtAndSDates(dateForAttributes)
-				.Value(parentObject.CadastralNumber);
+				.Value(_parentObject.CadastralNumber);
 			parentCadastralNumberAttributeBuilder.Object(firstChildObject.Id).Build();
 			parentCadastralNumberAttributeBuilder.Object(secondChildObject.Id).Build();
 			var attributeCopyFrom = new GbuObjectAttributeBuilder()
 				.Attribute(EgrnAttributes.Address.Id)
-				.Object(parentObject.Id)
+				.Object(_parentObject.Id)
 				.OtAndSDates(dateForAttributes)
 				.Build();
 			var attributeCopyTo = EgrnAttributes.AddressOrLocation;
 
 
-			RunInheritance(new List<long> {task.Id}, parentCadastralNumberAttributeBuilder.AttributeId,
+			RunInheritance(new List<long> { _firstTask.Id}, parentCadastralNumberAttributeBuilder.AttributeId,
 				attributeCopyFrom.AttributeId, attributeCopyTo.Id);
 
 
@@ -88,34 +98,32 @@ namespace KadOzenka.Dal.IntegrationTests.GbuObject
 		public void Can_Inherit_BuildToFlat_From_Several_Tasks()
 		{
 			var childType = PropertyTypes.Pllacement;
-			var firstTask = new TaskBuilder().Tour(Tour.Id).Document(FirstDocument.Id).Build();
 			var secondTask = new TaskBuilder().Tour(Tour.Id).Document(SecondDocument.Id).Build();
 
 			var objectBuilder = new GbuObjectBuilder().Type(childType);
 			var firstChildObject = objectBuilder.ShallowCopy().Build();
 			var secondChildObject = objectBuilder.ShallowCopy().Build();
-			var parentObject = new GbuObjectBuilder().Type(PropertyTypes.Building).Build();
 
 			var firstUnitCreationDate = DateTime.Now.AddDays(1);
 			var secondUnitCreationDate = DateTime.Now.AddDays(2);
-			new UnitBuilder().Type(childType).Task(firstTask).CreationDate(firstUnitCreationDate).Object(firstChildObject).Build();
+			new UnitBuilder().Type(childType).Task(_firstTask).CreationDate(firstUnitCreationDate).Object(firstChildObject).Build();
 			new UnitBuilder().Type(childType).Task(secondTask).CreationDate(secondUnitCreationDate).Object(secondChildObject).Build();
 
 			var parentCadastralNumberAttributeBuilder = new GbuObjectAttributeBuilder()
 				.Attribute(EgrnAttributes.CadastralNumber.Id)
-				.Value(parentObject.CadastralNumber);
+				.Value(_parentObject.CadastralNumber);
 			parentCadastralNumberAttributeBuilder.Object(firstChildObject.Id).OtAndSDates(firstUnitCreationDate).Build();
 			parentCadastralNumberAttributeBuilder.Object(secondChildObject.Id).OtAndSDates(secondUnitCreationDate).Build();
 			
 			var attributeCopyFrom = new GbuObjectAttributeBuilder()
 				.Attribute(EgrnAttributes.Address.Id)
-				.Object(parentObject.Id);
+				.Object(_parentObject.Id);
 			var firstParentAttribute = attributeCopyFrom.ShallowCopy().OtAndSDates(firstUnitCreationDate).Build();
 			var secondParentAttribute = attributeCopyFrom.ShallowCopy().OtAndSDates(secondUnitCreationDate).Build();
 			var attributeCopyTo = EgrnAttributes.AddressOrLocation;
 
 
-			RunInheritance(new List<long> {firstTask.Id, secondTask.Id},
+			RunInheritance(new List<long> { _firstTask.Id, secondTask.Id},
 				parentCadastralNumberAttributeBuilder.AttributeId, attributeCopyFrom.AttributeId, attributeCopyTo.Id);
 
 
@@ -126,26 +134,20 @@ namespace KadOzenka.Dal.IntegrationTests.GbuObject
 		[Test]
 		public void CanNot_Inherit_BuildToFlat_If_There_Are_No_Info_About_Parent_Cadastral_Number()
 		{
-			var childType = PropertyTypes.Pllacement;
-			var task = new TaskBuilder().Tour(Tour.Id).Document(FirstDocument.Id).Build();
-			var childObject = new GbuObjectBuilder().Type(childType).Build();
-			var parentObject = new GbuObjectBuilder().Type(PropertyTypes.Building).Build();
-			var unit = new UnitBuilder().Task(task).Object(childObject).Type(childType).Build();
-
-			var dateForAttributes = unit.CreationDate.GetValueOrDefault().AddDays(-1);
+			var dateForAttributes = _unit.CreationDate.GetValueOrDefault().AddDays(-1);
 			var attributeCopyFrom = new GbuObjectAttributeBuilder()
 				.Attribute(EgrnAttributes.Address.Id)
-				.Object(parentObject.Id)
+				.Object(_parentObject.Id)
 				.OtAndSDates(dateForAttributes)
 				.Build();
 			var attributeCopyTo = EgrnAttributes.AddressOrLocation;
 
 
-			RunInheritance(new List<long> { task.Id }, EgrnAttributes.CadastralNumber.Id,
+			RunInheritance(new List<long> { _firstTask.Id }, EgrnAttributes.CadastralNumber.Id,
 				attributeCopyFrom.AttributeId, attributeCopyTo.Id);
 
 
-			var copiedAttributes = GetAttributeValue(attributeCopyTo, childObject.Id, attributeCopyFrom.ChangeDocId);
+			var copiedAttributes = GetAttributeValue(attributeCopyTo, _firstChildObject.Id, attributeCopyFrom.ChangeDocId);
 			Assert.That(copiedAttributes.Count, Is.EqualTo(0));
 		}
 
