ea2c911dde98cf477be9c991f4c4b3adc6737ebe
Korotkova Ekaterina <korotkova@corepartners.ru>
2021-06-28 10:19:39 +0300

    KOMO-86: удалена логика для скриншотов ОА

--- Source/KadOzenka.Web/Models/MarketObject/PriceHistoryChartModel.cs
+++ Source/KadOzenka.Web/Models/MarketObject/PriceHistoryChartModel.cs
@@ -9,22 +9,19 @@ namespace KadOzenka.Web.Models.MarketObject
 	{
 		public DateTime ChangingDate { get; set; }
 		public long PriceValue { get; set; }
-		public long? ScreenshotId { get; set; }
 		public bool IsPriceUp { get; set; }
 		public bool IsPriceDown { get; set; }
 
-		public static List<PriceHistoryChartModel> FromEntities(List<OMPriceHistory> priceHistories, List<OMScreenshots> screenshots)
+		public static List<PriceHistoryChartModel> FromEntities(List<OMPriceHistory> priceHistories)
 		{
 			var priceHistoryChartModels = new List<PriceHistoryChartModel>();
 			var orderedHistories = priceHistories.OrderBy(x => x.ChangingDate).ToList();
 			for (var i = 0; i < orderedHistories.Count(); i++)
 			{
-				var screenshot = screenshots.FirstOrDefault(x => x.CreationDate == orderedHistories[i].ChangingDate);
 				var priceHistoryChartModel = new PriceHistoryChartModel
 				{
 					ChangingDate = orderedHistories[i].ChangingDate,
 					PriceValue = orderedHistories[i].PriceValueTo,
-					ScreenshotId = screenshot?.Id,
 					IsPriceUp = i > 0 && orderedHistories[i - 1].PriceValueTo < orderedHistories[i].PriceValueTo 
 						? true
 						: false,
