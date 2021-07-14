611b46bd8fe2cea87ab2d0bfde3ade6d512bce74
Korotkova Ekaterina <korotkova@corepartners.ru>
2021-07-14 14:01:26 +0300

    4493: рефакторинг теста

--- Source/KadOzenka.Dal.IntegrationTests/Modeling/Objects/ConstructorTests.cs
+++ Source/KadOzenka.Dal.IntegrationTests/Modeling/Objects/ConstructorTests.cs
@@ -15,55 +15,16 @@ namespace KadOzenka.Dal.IntegrationTests.Modeling.Objects
 {
 	public class ConstructorTests : BaseModelingTests
 	{
-		[Test]
-		public void Can_Update_Objects()
+		private long _addressAttributeId;
+		private long _squareAttributeId;
+		private OMModelToMarketObjects _firstObject;
+		private ExcelRow _firstExcelRow;
+
+
+		[OneTimeSetUp]
+		public void OneTimeSetUp()
 		{
-			var model = new ModelBuilder().Build();
-			var addressAttributeId = RandomGenerator.GenerateRandomId();
-			var squareAttributeId = RandomGenerator.GenerateRandomId();
-			var coefficients = new List<CoefficientForObject>
-			{
-				new(addressAttributeId)
-				{
-					Coefficient = RandomGenerator.GenerateRandomDecimal(),
-					Value = RandomGenerator.GetRandomString()
-				},
-				new(squareAttributeId)
-				{
-					Coefficient = RandomGenerator.GenerateRandomDecimal(),
-					Value = RandomGenerator.GetRandomString()
-				}
-			};
-			var firstObject = new ModelObjectBuilder().Id(1).Model(model).Coefficients(coefficients).Build();
-
-			ExcelFile excelFile;
-			var fileName = "objects.xlsx";
-			using (var stream = File.OpenRead($"{PathToFileFolder}{fileName}"))
-			{
-				excelFile = ExcelFile.Load(stream, new XlsxLoadOptions());
-			}
-
-			var config = new ModelObjectsConstructor
-			{
-				IdColumnIndex = 0,
-				ColumnsMapping = new List<ColumnToAttributeMapping>
-				{
-					new(1, GetAttributeId(x => x.IsForTraining)),
-					new(2, GetAttributeId(x => x.IsForControl)),
-					new(3, GetAttributeId(x => x.IsExcluded)),
-					new(4, GetAttributeId(x => x.CadastralNumber)),
-					new(5, GetAttributeId(x => x.UnitPropertyType)),
-					new(6, GetAttributeId(x => x.Price)),
-					new(7, GetAttributeId(x => x.PriceFromModel)),
-					new(9, $"{addressAttributeId}{ModelObjectsService.PrefixForValueInNormalizedColumn}"),
-					new(10, $"{addressAttributeId}{ModelObjectsService.PrefixForCoefficientInNormalizedColumn}"),
-					new(11, $"{squareAttributeId}{ModelObjectsService.PrefixForFactor}")
-				}
-			};
-
-			ModelObjectsService.ChangeModelObjects(excelFile, config);
-
-			var firstExcelRow = new ExcelRow
+			_firstExcelRow = new ExcelRow
 			{
 				Id = 1,
 				IsForTraining = false,
@@ -78,18 +39,83 @@ namespace KadOzenka.Dal.IntegrationTests.Modeling.Objects
 				AddressCoefficient = 12,
 				SquareCoefficient = 13
 			};
-			CheckUpdatedObject(firstObject.Id, addressAttributeId, squareAttributeId, firstExcelRow);
 		}
-		
+
+		[SetUp]
+		public void SetUp()
+		{
+			_addressAttributeId = RandomGenerator.GenerateRandomId();
+			_squareAttributeId = RandomGenerator.GenerateRandomId();
+			
+			var coefficients = new List<CoefficientForObject>
+			{
+				new(_addressAttributeId)
+				{
+					Coefficient = RandomGenerator.GenerateRandomDecimal(),
+					Value = RandomGenerator.GetRandomString()
+				},
+				new(_squareAttributeId)
+				{
+					Coefficient = RandomGenerator.GenerateRandomDecimal(),
+					Value = RandomGenerator.GetRandomString()
+				}
+			};
+
+			var model = new ModelBuilder().Build();
+			_firstObject = new ModelObjectBuilder().Id(1).Model(model).Coefficients(coefficients).Build();
+		}
+
+
+
+		[Test]
+		public void Can_Update_Objects()
+		{
+			var excelFile = GetFile();
+			var config = GetConfig();
+			ModelObjectsService.ChangeModelObjects(excelFile, config);
+
+			CheckUpdatedObject(_firstObject.Id, _firstExcelRow);
+		}
+
 
 		#region Support Methods
+		
+		private ExcelFile GetFile()
+		{
+			var fileName = "objects.xlsx";
+			using (var stream = File.OpenRead($"{PathToFileFolder}{fileName}"))
+			{
+				return ExcelFile.Load(stream, new XlsxLoadOptions());
+			}
+		}
+
+		private ModelObjectsConstructor GetConfig()
+		{
+			return new ModelObjectsConstructor
+			{
+				IdColumnIndex = 0,
+				ColumnsMapping = new List<ColumnToAttributeMapping>
+				{
+					new(1, GetAttributeId(x => x.IsForTraining)),
+					new(2, GetAttributeId(x => x.IsForControl)),
+					new(3, GetAttributeId(x => x.IsExcluded)),
+					new(4, GetAttributeId(x => x.CadastralNumber)),
+					new(5, GetAttributeId(x => x.UnitPropertyType)),
+					new(6, GetAttributeId(x => x.Price)),
+					new(7, GetAttributeId(x => x.PriceFromModel)),
+					new(9, $"{_addressAttributeId}{ModelObjectsService.PrefixForValueInNormalizedColumn}"),
+					new(10, $"{_addressAttributeId}{ModelObjectsService.PrefixForCoefficientInNormalizedColumn}"),
+					new(11, $"{_squareAttributeId}{ModelObjectsService.PrefixForFactor}")
+				}
+			};
+		}
 
 		private string GetAttributeId(Expression<Func<OMModelToMarketObjects, object>> expression)
 		{
 			return OMModelToMarketObjects.GetColumnAttributeId(expression).ToString();
 		}
 
-		private void CheckUpdatedObject(long id, long addressAttributeId, long squareAttributeId, ExcelRow row)
+		private void CheckUpdatedObject(long id, ExcelRow row)
 		{
 			var updatedObject = OMModelToMarketObjects.Where(x => x.Id == id).SelectAll().ExecuteFirstOrDefault();
 			
@@ -105,11 +131,11 @@ namespace KadOzenka.Dal.IntegrationTests.Modeling.Objects
 			var coefficients = updatedObject.DeserializeCoefficient();
 			Assert.That(coefficients.Count, Is.EqualTo(2));
 			
-			var addressAttribute = coefficients.First(x => x.AttributeId == addressAttributeId);
+			var addressAttribute = coefficients.First(x => x.AttributeId == _addressAttributeId);
 			Assert.That(addressAttribute.Value, Is.EqualTo(row.AddressValue));
 			Assert.That(addressAttribute.Coefficient, Is.EqualTo(row.AddressCoefficient));
 
-			var squareAttribute = coefficients.First(x => x.AttributeId == squareAttributeId);
+			var squareAttribute = coefficients.First(x => x.AttributeId == _squareAttributeId);
 			Assert.That(squareAttribute.Value, Is.EqualTo(row.SquareCoefficient.ToString()));
 			Assert.That(squareAttribute.Coefficient, Is.EqualTo(row.SquareCoefficient));
 		}
