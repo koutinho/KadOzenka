ad8a155df2b49411a8555d6d10161240f86b97d9
Korotkova Ekaterina <korotkova@corepartners.ru>
2021-07-23 12:48:43 +0300

    4691: оптимизация (добавлены индексы на БД)

--- Source/DbMigrations/services/postgresql/scripts/KOMO_4691/down.sql
+++ Source/DbMigrations/services/postgresql/scripts/KOMO_4691/down.sql
@@ -1,3 +1,7 @@
 delete from core_long_process_type where id=100;
 
 ALTER TABLE ko_modeling_dictionaries_values DROP CONSTRAINT ko_modeling_dictionaries_values_idx1;
+
+DROP INDEX modeling_model_to_market_objects_idx;
+
+DROP INDEX ko_model_factor_idx;
--- Source/DbMigrations/services/postgresql/scripts/KOMO_4691/up.sql
+++ Source/DbMigrations/services/postgresql/scripts/KOMO_4691/up.sql
@@ -2,3 +2,7 @@ INSERT INTO core_long_process_type (id, process_name, class_name, schedule_type,
 VALUES (100, 'MarksCalculationLongProcess', 'KadOzenka.Dal.LongProcess.Modeling.MarksCalculationLongProcess, KadOzenka.Dal', 0, null, 1, null, null, null, null, null, null, 'Расчет меток для автоматической модели', 1, null);
 
 ALTER TABLE ko_modeling_dictionaries_values ADD CONSTRAINT ko_modeling_dictionaries_values_idx1 UNIQUE (dictionary_id, value);
+
+CREATE INDEX modeling_model_to_market_objects_idx ON modeling_model_to_market_objects USING btree (model_id);
+
+CREATE INDEX ko_model_factor_idx ON ko_model_factor USING btree (model_id);
