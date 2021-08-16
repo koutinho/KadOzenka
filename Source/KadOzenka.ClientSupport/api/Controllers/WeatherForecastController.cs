d2b087a7b7358dd6e2d5987d9bacdda17c9827ff
Васильев Александр Витальевич <vasiliev@genix.pro>
2021-08-16 16:18:53 +0300

    удален лишний код из WeatherForecastController

--- Source/KadOzenka.ClientSupport/api/Controllers/WeatherForecastController.cs
+++ Source/KadOzenka.ClientSupport/api/Controllers/WeatherForecastController.cs
@@ -1,12 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Claims;
-using System.Threading.Tasks;
-using api.Authentication;
-using api.DataLayer;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -24,21 +19,14 @@ namespace api.Controllers
 
         private readonly ILogger<WeatherForecastController> _logger;
 
-        private readonly UserManager<ApplicationUser> _userManager;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger, UserManager<ApplicationUser> userManager)
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
         {
             _logger = logger;
-            _userManager = userManager;
         }
 
         [HttpGet]
-        public async Task<IEnumerable<WeatherForecast>> Get()
+        public IEnumerable<WeatherForecast> Get()
         {
-            string userName = User.Identity.Name;
-
-            var user = await _userManager.FindByNameAsync(userName);
-            
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
